1dadb9c3e50967912a25b6d632a811f78f641389
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-02 17:35:04 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Basic Syntax, Conditional Statements and Loops/04. Back In 30 Minutes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04._Back_In_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            m += 30;
+            if(m > 59)
+            {
+                h++;
+                m -= 60;
+            }
+            if (h > 23)
+            {
+                h = 0;
+            }
+            Console.WriteLine($"{h}:{m:D2}");
+        }
+    }
+}
