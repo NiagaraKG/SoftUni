453a9d92b914e0616b9611e5b5e8bb966c032bb1
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-02 17:39:56 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Basic Syntax, Conditional Statements and Loops/07. Theatre Promotion/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _07._Theatre_Promotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine().ToLower();
+            int age = int.Parse(Console.ReadLine());
+            int price = 0;
+            if(0 <= age && age <= 18)
+            {
+                switch (day)
+                {
+                    case "weekday": price = 12; break;
+                    case "weekend": price = 15; break;
+                    case "holiday": price = 5; break;
+                }
+            }
+            else if (18 < age && age <= 64)
+            {
+                switch (day)
+                {
+                    case "weekday": price = 18; break;
+                    case "weekend": price = 20; break;
+                    case "holiday": price = 12; break;
+                }
+            }
+            else if (64 < age && age <= 122)
+            {
+                switch (day)
+                {
+                    case "weekday": price = 12; break;
+                    case "weekend": price = 15; break;
+                    case "holiday": price = 10; break;
+                }
+            }
+            if(price != 0)
+            {
+                Console.WriteLine(price + "$");
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}
