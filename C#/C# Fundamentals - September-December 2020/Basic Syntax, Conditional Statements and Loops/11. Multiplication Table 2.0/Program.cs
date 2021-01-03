1bc86c33e063bf82b96800886df2766bd4247ced
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-03 21:43:10 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Basic Syntax, Conditional Statements and Loops/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _11._Multiplication_Table_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int times = int.Parse(Console.ReadLine());
+            if (times <= 10)
+            {
+                for (int i = times; i <= 10; i++)
+                {
+                    Console.WriteLine($"{num} X {i} = {num * i}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{num} X {times} = {num * times}");
+            }
+        }
+    }
+}
