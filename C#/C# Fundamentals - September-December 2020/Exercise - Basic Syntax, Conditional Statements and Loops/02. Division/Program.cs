58ddb19b387815242386fd8f95766f139eec70de
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-04 21:23:29 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Basic Syntax, Conditional Statements and Loops/02. Division/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02._Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int d = 0;
+            if(n % 10 == 0) { d = 10; }
+            else if (n % 7 == 0) { d = 7; }
+            else if (n % 6 == 0) { d = 6; }
+            else if (n % 3 == 0) { d = 3; }
+            else if (n % 2 == 0) { d = 2; }
+            if(d == 0) { Console.WriteLine("Not divisible"); }
+            else { Console.WriteLine($"The number is divisible by {d}"); }
+        }
+    }
+}
