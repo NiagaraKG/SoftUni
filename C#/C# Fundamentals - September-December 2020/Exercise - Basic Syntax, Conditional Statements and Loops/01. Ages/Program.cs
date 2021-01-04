c40d1ac78e6522c5d6fe1cd26fdbdbaed199e8f6
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-04 21:22:04 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Basic Syntax, Conditional Statements and Loops/01. Ages/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            string result;
+            if(age < 3) { result = "baby"; }
+            else if (age < 14) { result = "child"; }
+            else if (age < 20) { result = "teenager"; }
+            else if (age < 66) { result = "adult"; }
+            else { result = "elder"; }
+            Console.WriteLine(result);
+        }
+    }
+}
