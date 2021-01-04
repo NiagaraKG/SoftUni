99d70e2014d2eae02cf393f19dc35d6a6417f9db
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-04 21:27:18 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Basic Syntax, Conditional Statements and Loops/4. Print and Sum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _4._Print_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int sum = end;
+            for (int i = start; i < end; i++)
+            {
+                Console.Write(i + " ");
+                sum += i;
+            }
+            Console.WriteLine(end);
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
