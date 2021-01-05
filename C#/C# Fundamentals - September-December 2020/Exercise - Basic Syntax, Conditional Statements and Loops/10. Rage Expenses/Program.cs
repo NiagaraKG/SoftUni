8a83795baf4aa62c7340483deeb3293445c29534
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-05 20:45:42 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Basic Syntax, Conditional Statements and Loops/10. Rage Expenses/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _10._Rage_Expenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int loses = int.Parse(Console.ReadLine());            
+            double headset = double.Parse(Console.ReadLine());
+            double mouse = double.Parse(Console.ReadLine());
+            double keyboard = double.Parse(Console.ReadLine());
+            double display = double.Parse(Console.ReadLine());
+            double total = headset * (loses / 2) + mouse * (loses / 3) + keyboard * (loses / 6) + display * (loses/ 12);
+            Console.WriteLine($"Rage expenses: {total:F2} lv.");
+        }
+    }
+}
