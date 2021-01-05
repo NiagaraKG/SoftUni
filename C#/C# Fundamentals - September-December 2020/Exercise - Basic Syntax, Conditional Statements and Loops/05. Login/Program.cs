9108f0c4710c28c493c6416e24756c8cb5324f25
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-05 20:37:35 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Basic Syntax, Conditional Statements and Loops/05. Login/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05._Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string user = Console.ReadLine();
+            string pass = "";
+            int tries = 0;
+            string input;
+            for (int i = user.Length - 1; i >= 0; i--)
+            {
+                pass += user[i];
+            }
+            while (tries < 4)
+            {
+                input = Console.ReadLine();
+                if (input == pass) { Console.WriteLine($"User {user} logged in."); break; }
+                tries++;
+                if (tries == 4) { Console.WriteLine($"User {user} blocked!"); break; }
+                Console.WriteLine("Incorrect password. Try again.");
+            }
+        }
+    }
+}
