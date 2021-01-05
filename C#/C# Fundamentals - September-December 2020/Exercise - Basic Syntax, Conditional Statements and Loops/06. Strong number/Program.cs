2029424d7146a731617000b7090e15fd598e7e07
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-05 20:40:36 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Basic Syntax, Conditional Statements and Loops/06. Strong number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _06._Strong_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int current = num;
+            int sum = 0;
+            while (current > 0)
+            {
+                int f = 1;
+                for(int i = current % 10; i >= 1 ; i--)
+                { f *= i; }
+                sum += f;
+                current /= 10;
+            }
+            if(sum == num) { Console.WriteLine("yes"); }
+            else { Console.WriteLine("no"); }
+        }
+    }
+}
