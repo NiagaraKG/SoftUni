72056b9d76feccbcd18daef250e11d6f4e6b9a33
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-06 22:01:35 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/More Exercises - Basic Syntax, Conditional Statements and Loops/01. Sort Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01._Sort_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int buf;
+            if (a < b) { buf = a; a = b; b = buf; }
+            if (a < c) { buf = a; a = c; c = buf; }
+            if (b < c) { buf = b; b = c; c = buf; }
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
