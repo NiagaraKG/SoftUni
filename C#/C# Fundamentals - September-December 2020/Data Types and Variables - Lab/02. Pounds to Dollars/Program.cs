99ef78de3494c235e29c5891c97901d00377b6c1
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-07 21:48:48 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Data Types and Variables - Lab/02. Pounds to Dollars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02._Pounds_to_Dollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal BP = decimal.Parse(Console.ReadLine());
+            decimal D = BP * 1.31M;
+            Console.WriteLine($"{D:F3}");
+        }
+    }
+}
