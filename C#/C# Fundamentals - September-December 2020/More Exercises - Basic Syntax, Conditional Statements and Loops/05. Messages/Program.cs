2d8a730bc683d8e4ab7b081e53aac00515b6796b
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-07 21:43:53 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/More Exercises - Basic Syntax, Conditional Statements and Loops/05. Messages/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05._Messages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int n = int.Parse(Console.ReadLine());
+            string result = "";
+            for (int i = 0; i < n; i++)
+            {
+                string letter = Console.ReadLine();
+                string first = "";
+                first += letter[0];
+                if (first == "0") { result += " "; }
+                else
+                {
+                    int num = (int.Parse(first) - 2) * 3;
+                    if (int.Parse(first) == 8 || int.Parse(first) == 9) { num++; }
+                    num += letter.Length - 1;
+                    num += 97;
+                    result += (char)num;
+                }
+            }
+            Console.Write(result);
+        }
+    }
+}
