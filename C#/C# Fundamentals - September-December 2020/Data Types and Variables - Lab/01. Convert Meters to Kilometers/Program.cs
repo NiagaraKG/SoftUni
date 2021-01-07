30f9140751b34b6edc999647c3a34dffac41172f
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-07 21:47:17 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Data Types and Variables - Lab/01. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01._Convert_Meters_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = int.Parse(Console.ReadLine());
+            decimal km = m / 1000.00M;
+            Console.WriteLine($"{km:F2}");
+        }
+    }
+}
