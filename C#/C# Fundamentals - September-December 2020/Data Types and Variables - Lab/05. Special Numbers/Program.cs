39f92557deee8a6e943f0757f97c1890b8acf991
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-08 23:01:53 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Data Types and Variables - Lab/05. Special Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _05._Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= n; i++)
+            {                
+                int sum = 0;
+                int num = i;
+                while(num > 0)
+                {
+                    sum += num % 10;
+                    num /= 10;
+                }
+                bool isSpecial = sum == 5 || sum == 7 || sum == 11;
+                Console.WriteLine($"{i} -> {isSpecial}");
+            }
+        }
+    }
+}
