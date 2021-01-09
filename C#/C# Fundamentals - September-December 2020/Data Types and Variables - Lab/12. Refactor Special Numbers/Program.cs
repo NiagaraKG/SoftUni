b0d317ef84aeffd71449062bfa57fcbfaebd53fa
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-09 21:11:55 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Data Types and Variables - Lab/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _12._Refactor_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());            
+            for (int i = 1; i <= n; i++)
+            {
+                int current = i;
+                int sum = 0;
+                while (current > 0)
+                {
+                    sum += current % 10;
+                    current = current / 10;
+                }
+                bool isSpecial = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine($"{i} -> {isSpecial}");               
+            }
+        }
+    }
+}
