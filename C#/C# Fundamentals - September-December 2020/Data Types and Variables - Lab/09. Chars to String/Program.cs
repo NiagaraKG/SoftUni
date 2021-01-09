56085a6c09b5c7db28cca5c766faa07904b91247
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-09 21:03:52 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Data Types and Variables - Lab/09. Chars to String/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _09._Chars_to_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            string a = Console.ReadLine();
+            a += Console.ReadLine();
+            a += Console.ReadLine();
+            Console.WriteLine(a);
+        }
+    }
+}
