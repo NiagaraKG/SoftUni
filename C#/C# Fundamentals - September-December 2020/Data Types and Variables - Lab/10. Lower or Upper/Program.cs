84e639b4549eda8b0383c14c5b4763356af29fa3
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-09 21:06:25 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Data Types and Variables - Lab/10. Lower or Upper/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _10._Lower_or_Upper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine());
+            if(a == char.ToLower(a))
+            {
+                Console.WriteLine("lower-case");
+            }
+            else { Console.WriteLine("upper-case"); }
+        }
+    }
+}
