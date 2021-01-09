d262f5c7a515b844e20e39edffbbb5cd63477b50
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-09 21:15:10 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Data Types and Variables - Exercise/01. Integer Operations/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01._Integer_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            int fourth = int.Parse(Console.ReadLine());
+            int result = (first + second) / third * fourth;
+            Console.WriteLine(result);
+        }
+    }
+}
