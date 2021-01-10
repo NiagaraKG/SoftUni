e9b7c3570337248454ef4171310b2ba065c3a584
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-10 18:35:01 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Data Types and Variables - Exercise/03. Elevator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int persons = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            int courses = 0;
+            while(persons > 0)
+            {
+                courses++;
+                persons -= capacity;
+            }
+            Console.WriteLine(courses);
+        }
+    }
+}
