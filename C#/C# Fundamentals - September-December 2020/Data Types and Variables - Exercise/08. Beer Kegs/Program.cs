7f544468c4fc9df3ac1ad88eaacf33678da8b281
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-11 19:34:09 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Data Types and Variables - Exercise/08. Beer Kegs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _08._Beer_Kegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string biggest = "";
+            double max = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string model = Console.ReadLine();
+                double r = double.Parse(Console.ReadLine());
+                int h = int.Parse(Console.ReadLine());
+                double V = Math.PI * Math.Pow(r,2) * h;
+                if(V > max) 
+                {
+                    biggest = model;
+                    max = V;
+                }
+            }
+            Console.WriteLine(biggest);
+        }
+    }
+}
