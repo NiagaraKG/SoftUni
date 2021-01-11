09f71cfd9133a3d2066f9a9fe2b5fe5557fa1a34
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-11 19:31:26 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Data Types and Variables - Exercise/06. Triples of Latin Letters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06._Triples_of_Latin_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        char f = Convert.ToChar(i + 97);
+                        char s = Convert.ToChar(j + 97);
+                        char t = Convert.ToChar(k + 97);
+                        Console.WriteLine($"{f}{s}{t}");
+                    }
+                }
+            }
+        }
+    }
+}
