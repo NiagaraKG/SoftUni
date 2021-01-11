d5c5f0a68a8407d0fa824c7afcb753110d4630b7
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-11 19:35:40 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Data Types and Variables - Exercise/09. Spice Must Flow/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _09._Spice_Must_Flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yield = int.Parse(Console.ReadLine());
+            int days = 0, total = 0;
+            while (yield >= 100)
+            {
+                total += yield;
+                yield -= 10;
+                if (total >= 26) { total -= 26; }
+                days++;
+            }
+            if(total >= 26) { total -= 26; }
+            Console.WriteLine(days);
+            Console.WriteLine(total);
+        }
+    }
+}
