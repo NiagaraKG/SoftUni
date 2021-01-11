c38b3ccce48f013eef99889a5189d54595d6c663
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-11 19:33:03 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Data Types and Variables - Exercise/07. Water Overflow/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int liters = 0;
+            for(int i = 0; i < n; i++)
+            {
+                int quantity = int.Parse(Console.ReadLine());
+                liters += quantity;
+                if (liters > 255)
+                { 
+                    Console.WriteLine("Insufficient capacity!");
+                    liters -= quantity;
+                }
+            }
+            Console.WriteLine(liters);
+        }
+    }
+}
