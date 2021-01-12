9c857fca165c2116c6deac3aaf3a3deea5fb0b5c
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-12 18:51:45 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/More Exercises - Data Types and Variables/03. Floating Equality/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _03._Floating_Equality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double first = double.Parse(Console.ReadLine());
+            double second = double.Parse(Console.ReadLine());
+            if(Math.Abs(first - second) <= 0.000001)
+            { Console.WriteLine("True"); }
+            else { Console.WriteLine("False"); }
+        }
+    }
+}
