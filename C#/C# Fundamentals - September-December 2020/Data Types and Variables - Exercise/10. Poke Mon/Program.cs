c2ed2e518c8a152f94b5d94424b7aee5dade3fd0
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-12 18:40:58 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Data Types and Variables - Exercise/10. Poke Mon/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _10._Poke_Mon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int power = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustion = int.Parse(Console.ReadLine());
+            int targets = 0;
+            double half = power / 2.0;
+            while(power >= distance)
+            {
+                power -= distance;
+                targets++;
+                if(power == half && exhaustion != 0)
+                { power /= exhaustion; }
+            }
+            Console.WriteLine(power);
+            Console.WriteLine(targets);
+        }
+    }
+}
