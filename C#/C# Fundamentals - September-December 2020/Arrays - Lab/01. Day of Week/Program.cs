034f9716153895299ca92dd7503cc15fa76b0c94
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-13 19:58:56 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Arrays - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01._Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = { "Invalid day!", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            int i = int.Parse(Console.ReadLine());
+            if(i < 1 || i > 7) { i = 0; }
+            Console.WriteLine(days[i]);
+        }
+    }
+}
