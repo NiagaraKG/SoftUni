6c526ad92b431fbb13784090bcc66c1aaa0ca861
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-13 19:56:03 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/More Exercises - Data Types and Variables/05. Decrypting Message/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05._Decrypting_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            string result = "";
+            for (int i = 0; i < n; i++)
+            {
+                char current = char.Parse(Console.ReadLine());
+                result += (char)((int)current + key);
+            }
+            Console.Write(result);
+        }
+    }
+}
