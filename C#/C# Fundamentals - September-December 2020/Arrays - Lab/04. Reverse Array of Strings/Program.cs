11b85f247c5e5202a055b7c58db22f8602f6568b
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-13 20:03:10 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Arrays - Lab/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+            Console.WriteLine(string.Join(' ', arr.Reverse()));
+        }
+    }
+}
