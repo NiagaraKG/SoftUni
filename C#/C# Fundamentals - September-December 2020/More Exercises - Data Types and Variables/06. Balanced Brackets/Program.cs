47a21db4d8f29eae2f496f4399be0c8047e5309a
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-13 19:56:59 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/More Exercises - Data Types and Variables/06. Balanced Brackets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _06._Balanced_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int balance = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                if (input == "(")
+                {
+                    if (balance == -1) { balance = -10; }
+                    balance++;
+                    if(balance == 2) { balance = -10; }                    
+                }
+                else if (input == ")")
+                {
+                    balance--;                    
+                }
+            }
+            if(balance == 0) { Console.WriteLine("BALANCED"); }
+            else { Console.WriteLine("UNBALANCED"); }
+        }
+    }
+}
