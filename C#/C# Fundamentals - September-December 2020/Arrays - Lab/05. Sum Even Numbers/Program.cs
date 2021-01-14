ac5184d2914f76e766a594f4b206f8060ccab52b
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-14 10:51:30 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Arrays - Lab/05. Sum Even Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Sum_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            foreach (int i in arr) { if(i % 2 == 0) { sum += i; } }
+            Console.WriteLine(sum);
+        }
+    }
+}
