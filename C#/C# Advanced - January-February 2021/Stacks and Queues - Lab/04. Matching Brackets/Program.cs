f7c5a80905921f77271cde3964c0aea123673bd6
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-14 10:49:55 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Stacks and Queues - Lab/04. Matching Brackets/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<int> indexes = new Stack<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(input[i] == '(') { indexes.Push(i); }
+                else if(input[i] == ')')
+                {
+                    int start = indexes.Pop();
+                    Console.WriteLine(input.Substring(start, i-start+1));
+                }
+            }
+        }
+    }
+}
