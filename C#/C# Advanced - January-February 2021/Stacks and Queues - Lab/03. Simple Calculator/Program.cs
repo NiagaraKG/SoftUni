31d01e46877ef43f995a548340e8fc8405905c17
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-14 10:48:08 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Stacks and Queues - Lab/03. Simple Calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+            Stack<string> expression = new Stack<string>(input.Reverse());
+            int result = 0;
+            while(expression.Count > 1)
+            {
+                int operand1 = int.Parse(expression.Pop());
+                string Operator = expression.Pop();
+                int operand2 = int.Parse(expression.Pop());
+                if(Operator == "+") { result = operand1 + operand2; }
+                else { result = operand1 - operand2; }
+                expression.Push(result.ToString());
+            }
+            Console.WriteLine(expression.Pop());
+        }
+    }
+}
