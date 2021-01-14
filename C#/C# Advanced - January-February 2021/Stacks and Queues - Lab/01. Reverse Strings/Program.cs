d56e91ce384418d09ca2459192be9db2c00c0ee7
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-14 10:45:04 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Stacks and Queues - Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Join("", Console.ReadLine().ToArray()); ;
+            Stack<char> text = new Stack<char>(input);
+            Console.WriteLine(string.Join("", text.ToArray()));
+        }
+    }
+}
