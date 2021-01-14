f8f8c85bd57724b7c22e6e7a6a8a13103c95c908
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-14 10:46:23 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Stacks and Queues - Lab/02. Stack Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Stack_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Stack<int> numbers = new Stack<int>();
+            for (int i = 0; i < input.Length; i++) { numbers.Push(int.Parse(input[i])); }
+            string[] commannd = Console.ReadLine().Split();
+            while(commannd[0].ToLower() != "end")
+            {
+                if(commannd[0].ToLower() == "add")
+                { 
+                    numbers.Push(int.Parse(commannd[1]));
+                    numbers.Push(int.Parse(commannd[2])); 
+                }
+                else
+                {
+                    int n = int.Parse(commannd[1]);
+                    if(n<=numbers.Count)
+                    {
+                        for (int i = 0; i < n; i++)
+                        {
+                            numbers.Pop();
+                        }
+                    }
+                }
+                commannd = Console.ReadLine().Split();
+            }
+            Console.WriteLine("Sum: " + numbers.Sum());
+        }
+    }
+}
