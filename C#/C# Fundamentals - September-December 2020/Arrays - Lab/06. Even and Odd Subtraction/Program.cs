eb7d3ed8fa0f5f90653319df2415a7c2eb812127
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-14 10:52:54 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Arrays - Lab/06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Even_and_Odd_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum1 = 0, sum2 = 0;
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            foreach (int i in arr) 
+            {
+                if (i % 2 == 0) { sum1 += i; }
+                else { sum2 += i; }
+            }            
+            Console.WriteLine(sum1 - sum2);
+        }
+    }
+}
