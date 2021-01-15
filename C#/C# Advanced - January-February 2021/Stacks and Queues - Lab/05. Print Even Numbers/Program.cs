d644f86de362c1e45f8adac9efa32b3d4028b045
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-15 17:50:01 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Stacks and Queues - Lab/05. Print Even Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).Where(x => x % 2 == 0).ToArray();
+            Queue<int> result = new Queue<int>(input);
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
