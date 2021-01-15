59be2a2c7984edb49f4b611f910f3cff16742ee0
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-15 17:52:29 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Stacks and Queues - Lab/07. Hot Potato/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Queue<string> names = new Queue<string>(input);
+            int n = int.Parse(Console.ReadLine()), br = 0;
+            while (names.Count > 1)
+            {
+                string child = names.Dequeue();
+                br++;
+                if(br%n==0) { Console.WriteLine("Removed " + child); }
+                else { names.Enqueue(child); }
+            }
+            Console.WriteLine("Last is " + names.Dequeue());
+        }
+    }
+}
