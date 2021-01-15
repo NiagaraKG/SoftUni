14c22b81af4cc83e450d04575c4f41490d016efc
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-15 17:53:59 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Stacks and Queues - Lab/08. Traffic Jam/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()), br = 0;
+            string input = Console.ReadLine();
+            Queue<string> vehicles = new Queue<string>();
+            while (input != "end")
+            {
+                if (input == "green")
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (vehicles.Count == 0) { break; }
+                        Console.WriteLine(vehicles.Dequeue() + " passed!"); br++;
+                    }
+                }
+                else { vehicles.Enqueue(input); }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(br + " cars passed the crossroads.");
+        }
+    }
+}
