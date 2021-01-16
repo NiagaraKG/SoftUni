3985b2fbe74388aceecf577d9c29243ec61a8a8c
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-16 19:35:30 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Arrays/01. Train/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] train = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                train[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(string.Join(' ', train));
+            Console.WriteLine(train.Sum());
+        }
+    }
+}
