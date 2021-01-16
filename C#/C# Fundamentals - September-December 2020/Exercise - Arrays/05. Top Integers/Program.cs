2d08373464f0c73f9431d6e81eed000b3f4be644
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-16 19:43:05 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Arrays/05. Top Integers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Top_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string result = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool isBigger = true;
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if(arr[j] >= arr[i]) { isBigger = false; break; }
+                }
+                if (isBigger) { result += arr[i] + " "; }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
