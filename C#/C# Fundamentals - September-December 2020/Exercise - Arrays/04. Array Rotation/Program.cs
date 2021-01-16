35cf8d964bc6cd41946e4024aba7fb166d08cdca
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-16 19:41:20 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Arrays/04. Array Rotation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04._Array_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+            int r = int.Parse(Console.ReadLine());            
+            for (int i = 0; i < r; i++)
+            {
+                string first = arr[0];
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    arr[j] = arr[j + 1];
+                }
+                arr[arr.Length - 1] = first;
+            }
+            Console.WriteLine(string.Join(' ', arr));
+        }
+    }
+}
