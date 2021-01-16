72594de65156aca2f74ad09f3357a8bc85d4798d
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-16 19:38:47 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Arrays/02. Common Elements/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02._Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] first = Console.ReadLine().Split();
+            string[] second = Console.ReadLine().Split();
+            string common = "";
+            for (int i = 0; i < second.Length; i++)
+            {
+                for (int j = 0; j < first.Length; j++)
+                {
+                    if(second[i] == first[j]) { common += second[i] + " "; }
+                }
+            }
+            Console.WriteLine(common);
+        }
+    }
+}
