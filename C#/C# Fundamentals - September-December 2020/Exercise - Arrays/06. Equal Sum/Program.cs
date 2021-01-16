de0b10c85fc8ffd3e058de90550c6181f4f706df
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-16 19:44:29 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Arrays/06. Equal Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Equal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int left = 0;
+                int right = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    left += arr[j];
+                }
+                for (int k = i + 1; k < arr.Length; k++)
+                {
+                    right += arr[k];
+                }
+                if(left == right) { Console.WriteLine(i); return; }                
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
