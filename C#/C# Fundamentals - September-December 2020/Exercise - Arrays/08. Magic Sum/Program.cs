74e00a0825b71cbfbe102753347ffa645597fa44
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-17 19:30:13 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Arrays/08. Magic Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = int.Parse(Console.ReadLine());
+            string result = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if( (arr[i] + arr[j]) == sum)
+                    { result += arr[i] + " " + arr[j] + '\n'; }
+                }
+            }
+            Console.Write(result);
+        }
+    }
+}
