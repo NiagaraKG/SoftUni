8c8447244a191bd0b8646bf37bafae72c45b4d6f
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-17 19:29:00 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Arrays/07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int max = 0;
+            int br = 1;
+            int value = 0;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if(arr[i] == arr[i-1]) { br++; }
+                else
+                {
+                    if(br > max) { max = br; value = arr[i-1]; }
+                    br = 1;
+                }
+            }
+            if (br > max) { max = br; value = arr[arr.Length - 1]; }
+            for (int i = 0; i < max; i++)
+            { Console.Write(value + " "); }
+        }
+    }
+}
