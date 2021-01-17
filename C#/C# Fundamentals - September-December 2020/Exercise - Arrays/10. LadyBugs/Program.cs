16b5552c4a4883caf29a2a7e5efb6093d615e682
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-17 19:35:42 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Arrays/10. LadyBugs/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _10._LadyBugs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[] bugs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] field = new int[size];
+            for (int i = 0; i < bugs.Length; i++)
+            {
+                if(bugs[i] >= 0 && bugs[i] < size)
+                {
+                    field[bugs[i]] = 1;
+                }
+            }
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                string[] flight = command.Split();
+                int from = int.Parse(flight[0]);
+                int steps = int.Parse(flight[2]);
+                if (flight[1] == "left") { steps *= -1; }
+                if (from >= 0 && from < field.Length)
+                {
+                    int to = from + steps;
+                    if (field[from] == 1)
+                    {
+                        field[from] = 0;                        
+                            while (to >= 0 && to < field.Length && field[to] == 1) { to += steps; }
+                            if (to >= 0 && to < field.Length) { field[to] = 1; }                        
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(' ', field));
+        }
+    }
+}
