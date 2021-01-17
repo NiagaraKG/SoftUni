48564fc40f7313928d3b74b069249d965410df11
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-17 19:38:49 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/More Exercises - Arrays/02. Pascal Triangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string printed = "1";
+            Console.WriteLine(printed);
+            for (int row = 1; row < n; row++)
+            {
+                printed += " 0";
+                int[] last = printed.Split().Select(int.Parse).ToArray();
+                int[] current = new int[last.Length];
+                current[0] = 1;
+                for (int col = 1; col < row; col++)
+                {
+                    current[col] = last[col] + last[col - 1];
+                }
+                current[current.Length - 1] = 1;
+                printed = string.Join(' ', current);
+                Console.WriteLine(printed);
+            }
+        }
+    }    
+}
+
