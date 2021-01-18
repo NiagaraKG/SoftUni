cd62afbcd0b0daf067fb8b01e9fdfc147a07f51e
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-18 18:31:37 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/More Exercises - Arrays/04. Fold and Sum/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Fold_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int k = input.Length / 4;
+            int[] first = new int[2*k];
+            int[] second = new int[2*k];
+            int[] begin = new int[k];
+            int[] end = new int[k];
+            int[] sum = new int[2 * k];
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(i < k)
+                {
+                    begin[i] = input[i];
+                }
+                else if (i >= input.Length - k)
+                {
+                    end[i - 3 * k] = input[i];
+                }
+                else
+                {
+                    second[i - k] = input[i];
+                }                
+            }
+            begin = begin.Reverse().ToArray();
+            end = end.Reverse().ToArray();
+            for (int i = 0; i < begin.Length; i++)
+            {
+                first[i] = begin[i];
+            }
+            for (int i = 0; i < end.Length; i++)
+            {
+                first[i+k] = end[i];
+            }
+            for (int i = 0; i < sum.Length; i++)
+            {
+                sum[i] = first[i] + second[i];
+            }
+            Console.WriteLine(string.Join(' ', sum));
+        }
+    }
+}
