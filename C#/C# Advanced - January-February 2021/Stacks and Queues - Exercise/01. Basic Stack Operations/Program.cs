68c2de98d711ba5244b909850de3483980845588
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-18 18:36:18 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Stacks and Queues - Exercise/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] first = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] second = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> s = new Stack<int>();
+            for (int i = 0; i < first[0]; i++)
+            {
+                s.Push(second[i]);
+            }
+            for (int i = 0; i < first[1]; i++)
+            {
+                if(s.Count>0) { s.Pop(); }
+            }
+            if(s.Contains(first[2])) { Console.WriteLine("true"); }
+            else if(s.Count == 0) { Console.WriteLine(0); }
+            else { Console.WriteLine(s.Min()); }
+        }
+    }
+}
