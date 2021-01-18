2356aaf1dfcf99ca9ea91b481d48b8c08d3bae62
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-18 18:30:20 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/More Exercises - Arrays/03. Recursive Fibonacci/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03._Recursive_Fibonacci
+{
+    class Program
+    {
+        static long Get_Fibonacci(long n)
+        {
+            if(n == 2 || n == 1) { return 1; }
+            return Get_Fibonacci(n-1) + Get_Fibonacci(n-2);
+        }
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());         
+            Console.WriteLine(Get_Fibonacci(n));
+        }
+    }
+}
