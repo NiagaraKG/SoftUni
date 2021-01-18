7ba1707b5e623afdc22d2b6fc58e58ce6bc04a57
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-18 18:38:36 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Stacks and Queues - Exercise/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] first = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] second = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> s = new Queue<int>();
+            for (int i = 0; i < first[0]; i++)
+            {
+                s.Enqueue(second[i]);
+            }
+            for (int i = 0; i < first[1]; i++)
+            {
+                if (s.Count > 0) { s.Dequeue(); }
+            }
+            if (s.Contains(first[2])) { Console.WriteLine("true"); }
+            else if (s.Count == 0) { Console.WriteLine(0); }
+            else { Console.WriteLine(s.Min()); }
+        }
+    }
+}
