82827463df70b842a5a0f1f4b39f07ae625ab62c
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-19 11:57:30 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._Balanced_Parenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+            Queue<char> brackets = new Queue<char>(input);
+            bool match = true;
+            while (brackets.Count > 2)
+            {
+                int half = brackets.Count / 2 - 1;
+                for (int i = 0; i < half; i++) { brackets.Enqueue(brackets.Dequeue()); }
+                char first = brackets.Dequeue(), second = brackets.Dequeue();
+                if(first == '(' && second == ')') { match = true; }
+                else if(first == '[' && second == ']') { match = true; }
+                else if(first == '{' && second == '}') { match = true; }
+                else { Console.WriteLine("NO"); return; }
+                for (int i = 0; i < half; i++) { brackets.Enqueue(brackets.Dequeue()); }
+            }
+            Console.WriteLine("YES"); 
+        }
+    }
+}
