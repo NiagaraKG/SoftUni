b6f1217bf4f6358ed37c90fbeeff16ec4f50ff8e
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-19 11:50:39 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Stacks and Queues - Exercise/04. Fast Food/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Fast_Food
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] quantities = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> orders = new Queue<int>(quantities);
+            Console.WriteLine(orders.Max());
+            while (orders.Count > 0)
+            {
+                int current = orders.Peek();
+                if (n >= current) { n -= current; orders.Dequeue(); }
+                else
+                {
+                    Console.WriteLine("Orders left: " + string.Join(" ", orders));
+                    return;
+                }
+            }
+            Console.WriteLine("Orders complete");
+        }
+    }
+}
