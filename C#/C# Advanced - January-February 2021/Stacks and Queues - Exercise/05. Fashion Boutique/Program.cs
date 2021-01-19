f033fa550d7dc82fc0760c5cd7edede56f16a3a8
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-19 11:51:48 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> clothes = new Stack<int>(input);
+            int sum = 0, racks = 1, capacity = int.Parse(Console.ReadLine());
+            while (clothes.Any())
+            {
+                int current = clothes.Pop();
+                if (sum + current <= capacity) { sum += current; }
+                else { sum = current; racks++; }
+            }
+            Console.WriteLine(racks);
+        }
+    }
+}
