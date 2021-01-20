68b72c95c0d0b4eff8beba59d4294d7353687fe3
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-20 19:17:53 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Stacks and Queues - Exercise/10. Crossroads/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10._Crossroads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int green = int.Parse(Console.ReadLine());
+            int window = int.Parse(Console.ReadLine());
+            int passed = 0;
+            bool left = false;
+            Queue<string> cars = new Queue<string>();
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                if (command == "green")
+                {
+                    string current = "";
+                    int time = 0;
+                    while (time < green && cars.Any())
+                    {
+                        current = cars.Dequeue();
+                        time += current.Length;
+                        if (time > green)
+                        {
+                            time = time - green; left = true; break;
+                        }
+                        else { passed++; }
+                    }
+                    if (left == true)
+                    {
+                        if (time > window)
+                        {
+                            Console.WriteLine("A crash happened!");
+                            Console.WriteLine(current + $" was hit at {current[current.Length - time + window]}.");
+                            return;
+                        }
+                        else { passed++; }
+                    }
+                }
+                else { cars.Enqueue(command); }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine(passed + " total cars passed the crossroads.");
+        }
+    }
+}
