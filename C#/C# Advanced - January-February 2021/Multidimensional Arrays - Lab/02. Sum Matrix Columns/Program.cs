4b46aac9d7adc1653851e26bbee71641de6070e5
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-20 19:23:55 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Multidimensional Arrays - Lab/02. Sum Matrix Columns/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Matrix_Columns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimentions = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int[,] matrix = new int[dimentions[0], dimentions[1]];
+            for (int r = 0; r < dimentions[0]; r++)
+            {
+                int[] row = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int c = 0; c < dimentions[1]; c++)
+                {
+                    matrix[r, c] = row[c];
+                }
+            }
+            for (int c = 0; c < dimentions[1]; c++)
+            {
+                int sum = 0;
+                for (int r = 0; r < dimentions[0]; r++)
+                {
+                    sum += matrix[r, c];
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
