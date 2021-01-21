468f3e171819de6d08bbf9f39f9dac8c72f3518c
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-21 19:49:54 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Multidimensional Arrays - Lab/07. Pascal Triangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long[][] matrix = new long[n][];
+            long[] row = new long[1];
+            row[0] = 1;
+            matrix[0] = row;
+            Console.WriteLine(string.Join(" ", row));
+            for (int i = 1; i < n; i++)
+            {
+                row = new long[i + 1];
+                row[0] = 1;
+                row[i] = 1;
+                for (int j = 1; j < i; j++)
+                {
+                    row[j] = matrix[i - 1][j - 1] + matrix[i - 1][j];
+                }
+                matrix[i] = row;
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+    }
+}
