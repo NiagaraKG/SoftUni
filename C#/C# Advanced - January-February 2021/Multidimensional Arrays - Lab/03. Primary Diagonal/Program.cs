c93d5e046092cf191dfd1c913ee5ec070d07c469
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-21 19:45:23 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Multidimensional Arrays - Lab/03. Primary Diagonal/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            int sum = 0;
+            for (int r = 0; r < n; r++)
+            {
+                int[] row = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int c = 0; c < n; c++)
+                {
+                    matrix[r, c] = row[c];
+                }
+                sum += matrix[r, r];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
