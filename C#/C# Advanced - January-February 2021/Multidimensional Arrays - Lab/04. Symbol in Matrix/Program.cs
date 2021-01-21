c4764137819484634095ae1e4f0397c1d6e38266
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-21 19:46:52 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Multidimensional Arrays - Lab/04. Symbol in Matrix/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _04._Symbol_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+            for (int r = 0; r < n; r++)
+            {
+                char[] row = Console.ReadLine().ToCharArray();
+                for (int c = 0; c < n; c++)
+                {
+                    matrix[r, c] = row[c];
+                }
+            }
+            char wanted = char.Parse(Console.ReadLine());
+            for (int r = 0; r < n; r++)
+            {
+                for (int c = 0; c < n; c++)
+                {
+                    if (matrix[r, c] == wanted)
+                    {
+                        Console.WriteLine($"({r}, {c})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine(wanted + " does not occur in the matrix");
+        }
+    }
+}
