8a5e87b8e60e09969b9653cd30b3b9762c62f921
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-22 22:20:06 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Multidimensional Arrays - Exercise/05. Snake Moves/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Snake_Moves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimentions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            string snake = Console.ReadLine();
+            char[,] matrix = new char[dimentions[0], dimentions[1]];
+            int i = 0;
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    if (r % 2 == 0) { matrix[r, c] = snake[i]; }
+                    else { matrix[r, dimentions[1] - 1 - c] = snake[i]; }
+                    i++;
+                    if (i == snake.Length) { i = 0; }
+                }
+            }
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    Console.Write(matrix[r, c]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
