dc38e534865c28ba3814158286d29b99aadb074d
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-23 22:50:05 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Methods - Lab/01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01._Sign_of_Integer_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintSign(n);
+        }
+
+        static void PrintSign (int n)
+        {
+            if(n > 0)
+            {
+                Console.WriteLine($"The number {n} is positive.");
+            }
+            else if (n < 0)
+            {
+                Console.WriteLine($"The number {n} is negative.");
+            }
+            else { Console.WriteLine($"The number {n} is zero."); }
+        }
+    }
+}
