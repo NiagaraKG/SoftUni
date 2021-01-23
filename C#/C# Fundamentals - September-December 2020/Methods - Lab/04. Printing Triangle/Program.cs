f63d90af0cc3a49bfd0f7bb0efa4581ed4149f8b
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-23 22:55:31 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Methods - Lab/04. Printing Triangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _04._Printing_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                PrintLine(i);
+            }
+            for (int i = n-1; i >= 1; i--)
+            {
+                PrintLine(i);
+            }
+        }
+
+        static void PrintLine(int n)
+        {            
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
