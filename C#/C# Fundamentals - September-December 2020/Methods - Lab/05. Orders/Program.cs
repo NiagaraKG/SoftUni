0e0942f36faee76707ee1607eecec31c385eda64
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-23 22:57:08 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Methods - Lab/05. Orders/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _05._Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            CalculatePrice(product, quantity);
+        }
+
+        static void CalculatePrice (string product, int quantity)
+        {
+            double total;
+            if(product == "coffee")
+            {
+                total = quantity * 1.50;
+            }
+            else if(product == "water")
+            {
+                total = quantity * 1.00;
+            }
+            else if(product == "coke")
+            {
+                total = quantity * 1.40;
+            }
+            else
+            {
+                total = quantity * 2.00;
+            }
+            Console.WriteLine(total.ToString("0.00"));
+        }
+
+    }
+}
