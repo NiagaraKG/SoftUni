25e1b2cfdf2bf3f3f9bbc45d6b2a80df99b47893
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-23 22:53:25 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Methods - Lab/03. Calculations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace _03._Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            if(command == "add") { Sum(a, b); }
+            else if (command == "multiply") { Multiply(a, b); }
+            else if (command == "substract") { Substract(a, b); }
+            else { Divide(a, b); }
+        }
+        static void Sum(double a, double b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        static void Multiply(double a, double b)
+        {
+            Console.WriteLine(a * b);
+        }
+
+        static void Substract(double a, double b)
+        {
+            Console.WriteLine(a - b);
+        }
+
+        static void Divide(double a, double b)
+        {
+            Console.WriteLine(a / b);
+        }
+    }
+}
