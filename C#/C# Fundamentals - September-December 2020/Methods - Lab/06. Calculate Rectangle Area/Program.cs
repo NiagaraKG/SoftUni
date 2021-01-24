c61caf54fd0a7cd7fd1ea9bf1411b63670003a68
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-24 22:08:11 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Methods - Lab/06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _06._Calculate_Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine(Area(a, b));
+        }
+
+        static double Area(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
