a35a99c6c09ab0f18d1d8b29b4d8808b62d2dfbd
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-24 22:09:22 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Methods - Lab/07. Repeat String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace _07._Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int times = int.Parse(Console.ReadLine());
+            Console.WriteLine(Repeat(input, times));
+        }
+
+        static string Repeat(string input, int times)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < times; i++)
+            {
+                result.Append(input);
+            }
+            return result.ToString();
+        }
+    }
+}
