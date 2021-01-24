e3639d1b2b321001566f5941c191a05f73a37d7e
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-24 22:10:55 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Methods - Lab/08. Math Power/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            double  pow = double .Parse(Console.ReadLine());
+            Console.WriteLine(Power(num, pow));
+        }
+
+        static double Power(double num, double pow)
+        {
+            return Math.Pow(num, pow);
+        }
+
+    }
+}
