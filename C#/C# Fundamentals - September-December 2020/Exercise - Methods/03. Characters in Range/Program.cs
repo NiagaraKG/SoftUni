94c83b22a1570f829c294effd757b5e73e907ee9
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-25 21:19:34 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Methods/03. Characters in Range/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _03._Characters_in_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char start = char.Parse(Console.ReadLine());
+            char end = char.Parse(Console.ReadLine());
+            PrintASCII(start, end);
+        }
+
+        static void PrintASCII(char s, char e)
+        {
+            if(s > e) { char a = s; s = e; e = a; }
+            string result = "";
+            for (int i = (int)s + 1; i < (int)e; i++)
+            {
+                result += (char)(i) + " ";
+            }
+            Console.WriteLine(result);
+        }
+
+    }
+}
