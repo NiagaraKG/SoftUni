c742f74fca30c06695be6b12c41df5940bc6146a
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-25 21:23:30 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Methods/05. Add and Subtract/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _05._Add_and_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine(Substract(Sum(a, b), c));
+        }
+
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Substract(int a, int b)
+        {
+            return a - b;
+        }
+
+    }
+}
