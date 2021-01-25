b130a3e9d8ae0b0e5b2f51452206aef3b9c53fd2
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-25 21:15:52 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Methods/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01._Smallest_of_Three_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            PrintMIN(a, b, c);
+        }
+
+        static void PrintMIN(int a, int b, int c)
+        {
+            a = Math.Min(a, b);
+            Console.WriteLine(Math.Min(a, c));
+        }
+
+    }
+}
