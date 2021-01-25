f85f261a79fc8f6a24b9cd72603752432af82fc1
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-25 21:24:52 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Methods/06. Middle Characters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _06._Middle_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            PrintMiddle(input);
+        }
+
+        static void PrintMiddle(string input)
+        {
+            if(input.Length % 2 == 0)
+            {
+                Console.WriteLine(input[(input.Length / 2)-1].ToString() + input[input.Length / 2]);
+            }
+            else
+            {
+                Console.WriteLine(input[input.Length / 2]);
+            }
+        }
+
+    }
+}
