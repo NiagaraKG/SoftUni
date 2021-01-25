bf5fea025cf8a614528459bece75859d0d5193d8
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-25 21:18:12 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Methods/02. Vowels Count/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02._Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            CountVowels(input);
+        }
+
+        static void CountVowels(string input)
+        {
+            input = input.ToLowerInvariant();
+            int br = 0;
+            for (int i = 0; i < input.Length; i++)
+            {                
+                if(input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u')
+                { br++; }
+            }
+            Console.WriteLine(br);
+        }
+
+    }
+}
