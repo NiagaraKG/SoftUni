7b0355fa875960870b8561375262e03ed1c71f89
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-26 22:46:18 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Methods/07. NxN Matrix/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07._NxN_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                PrintLine(n);
+            }
+        }
+
+        static void PrintLine(int n)
+        {
+            string result = "";
+            for (int i = 0; i < n; i++)
+            {
+                result += n + " ";
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
