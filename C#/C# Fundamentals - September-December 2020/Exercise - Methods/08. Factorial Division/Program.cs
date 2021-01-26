445e68e13c133212f62135777a7882d4fb13ca03
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-26 22:47:17 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Methods/08. Factorial Division/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _08._Factorial_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            double quotient = Factoriel(a) / Factoriel(b);
+            Console.WriteLine(quotient.ToString("0.00"));
+        }
+
+        static double Factoriel(int a)
+        {
+            double fact = 1;
+            for (int i = a; i > 0; i--)
+            {
+                fact *= i;
+            }
+            return fact;
+        }
+
+    }
+}
