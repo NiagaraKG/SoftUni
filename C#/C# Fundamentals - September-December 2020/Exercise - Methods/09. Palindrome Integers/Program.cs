2fb8ba9ee1987ef0f6f1e76c99d940be2cc20801
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-26 22:48:29 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Methods/09. Palindrome Integers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _09._Palindrome_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();            
+            while (input != "END")
+            {
+                int[] num = new int[input.Length];
+                for (int i = 0; i < input.Length; i++)
+                {
+                    num[i] = input[i];
+                }
+                
+                if(isPalindrome(num)) { Console.WriteLine("true"); }
+                else { Console.WriteLine("false"); }
+                input = Console.ReadLine();
+            }
+        }
+
+        static bool isPalindrome (int[] num)
+        {            
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] != num[num.Length - 1 - i])
+                {
+                    return false;                    
+                }
+            }
+            return true;
+        }
+    }
+}
