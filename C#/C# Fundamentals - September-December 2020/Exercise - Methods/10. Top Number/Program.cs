bdfe4ea4485dd4ba84641ac9681cf5b8d362feb8
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-26 22:49:29 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Methods/10. Top Number/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _10._Top_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if(IsTop(i)) { Console.WriteLine(i); }
+            }
+        }
+
+        static bool IsTop(int n)
+        {
+            if(!DigitSum(n)) { return false; }
+            if(!OddDigit(n)) { return false; }
+            return true;
+        }
+
+        static bool DigitSum (int n)
+        {
+            int sum = 0;
+            while(n > 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+            if(sum % 8 == 0) { return true; }
+            return false;
+        }
+
+        static bool OddDigit(int n)
+        {
+            int digit;
+            while (n > 0)
+            {
+                digit = n % 10;
+                n /= 10;
+                if(digit % 2 == 1) { return true; }
+            }
+            return false;
+        }
+
+    }
+}
