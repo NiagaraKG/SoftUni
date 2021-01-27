21c327f101a3582601c9c7319378ece7ba46ff07
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-27 22:56:41 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/More Exercises - Methods/04. Tribonacci Sequence/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _04._Tribonacci_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] sequence = new int[n];
+            sequence[0] = 1;
+            if (n > 1)
+            {
+                sequence[1] = 1;
+                if (n > 2)
+                {
+                    sequence[2] = 2;
+                    if (n > 3)
+                    {
+                        for (int i = 3; i < n; i++)
+                        {
+                            sequence[i] = GetTribonacci(sequence, i);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(String.Join(" ", sequence));
+        }
+        
+        static int GetTribonacci(int[] sequence, int i)
+        {
+            return (sequence[i - 1] + sequence[i - 2] + sequence[i - 3]) ;
+        }
+
+    }
+}
