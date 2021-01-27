e7ffe853c66bc423321c7a6859a8b688d2e6be5b
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-27 22:53:48 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/More Exercises - Methods/02. Center Point/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02._Center_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double distance1 = GetDistance(x1, y1);
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double distance2 = GetDistance(x2, y2);
+            if(distance1 <= distance2)
+            { Console.WriteLine($"({x1}, {y1})"); }
+            else { Console.WriteLine($"({x2}, {y2})"); }
+        }
+
+        static double GetDistance(double x, double y)
+        {
+            return Math.Sqrt(Math.Pow(x, 2)+ Math.Pow(y, 2));
+        }
+
+    }
+}
