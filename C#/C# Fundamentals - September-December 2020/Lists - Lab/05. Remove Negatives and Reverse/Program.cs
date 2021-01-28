8d285890a21108c614e31c40dffa1c5400f7adb4
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-28 21:29:57 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Lists - Lab/05. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Remove_Negatives_and_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> list = Console.ReadLine().Split().Select(double.Parse).Where(n => n >= 0).ToList();
+            if(list.Count == 0) { Console.WriteLine("empty"); }
+            else
+            {
+                list.Reverse();
+                Console.WriteLine(string.Join(' ', list));
+            }
+        }
+    }
+}
