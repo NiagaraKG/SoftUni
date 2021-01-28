08f518fe3e91d14e9dd8ee3b9a3d1eb096794b97
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-28 21:26:17 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Lists - Lab/03. Merging Lists/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Merging_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> list1 = Console.ReadLine().Split().Select(double.Parse).ToList();
+            List<double> list2 = Console.ReadLine().Split().Select(double.Parse).ToList();
+            List<double> sums = new List<double>();
+            int minSize = Math.Min(list1.Count, list2.Count);
+            int maxList = (list1.Count > list2.Count) ? 1 : 2;
+            int br1 = 0, br2 = 0, size = list1.Count + list2.Count;
+            for (int i = 0; i < size; i++)
+            {
+                if(minSize > 0)
+                {
+                    if(i %2==0)
+                    {
+                        sums.Add(list1[br1]);
+                        br1++;
+                        if (maxList == 2)
+                        {                            
+                            minSize--;
+                        }
+                    }
+                    else 
+                    {
+                        sums.Add(list2[br2]);
+                        br2++;
+                        if (maxList == 1)
+                        {                           
+                            minSize--;
+                        }
+                    }
+                }
+                else if(maxList == 1)
+                {
+                    sums.Add(list1[br1]);
+                    br1++;
+                }
+                else
+                {
+                    sums.Add(list2[br2]);
+                    br2++;
+                }
+            }
+            Console.WriteLine(string.Join(" ", sums));
+        }
+    }
+}
