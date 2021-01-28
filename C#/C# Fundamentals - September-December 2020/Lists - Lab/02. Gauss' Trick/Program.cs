0df329dc5078d1bf6028f68151820824ee8357f0
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-28 21:25:19 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Lists - Lab/02. Gauss' Trick/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Gauss__Trick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> input = Console.ReadLine().Split().Select(double.Parse).ToList();
+            List<double> sums = new List<double>();
+            for (int i = 0; i < (input.Count / 2) + (input.Count %2); i++)
+            {
+                double sum;
+                if (i != input.Count - 1 - i)
+                {
+                    sum = input[i] + input[input.Count - 1 - i];
+                }
+                else { sum = input[i]; }
+                sums.Add(sum);
+            }
+            Console.WriteLine(string.Join(" ", sums));
+        }
+    }
+}
