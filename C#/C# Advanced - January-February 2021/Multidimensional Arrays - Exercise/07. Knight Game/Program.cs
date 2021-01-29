e6f0400ca07ce184af40e335f8da7439aa63e394
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-29 21:46:45 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Multidimensional Arrays - Exercise/07. Knight Game/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _07._Knight_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] board = new char[n, n];
+            int removed = 0;
+
+            for (int r = 0; r < n; r++)
+            {
+                char[] row = Console.ReadLine().ToCharArray();
+                for (int c = 0; c < n; c++) { board[r, c] = row[c]; }
+            }
+            while (true)
+            {
+                int maxAttacks = 0, rowMaxAttacker = -1, colMaxAttacker = -1;
+                for (int r = 0; r < n; r++)
+                {
+
+                    for (int c = 0; c < n; c++)
+                    {
+                        if (board[r, c] == 'K')
+                        {
+                            int attacks = CountAttacks(board, r, c);
+                            if (attacks > maxAttacks) { maxAttacks = attacks; rowMaxAttacker = r; colMaxAttacker = c; }
+                        }
+                    }
+                }
+                if (maxAttacks == 0) { break; }
+                board[rowMaxAttacker, colMaxAttacker] = '0';
+                removed++;
+            }
+            Console.WriteLine(removed);
+        }
+
+        static int CountAttacks(char[,] board, int row, int col)
+        {
+            int br = 0;
+            if (row - 2 >= 0 && col - 1 >= 0 && board[row - 2, col - 1] == 'K') { br++; }
+            if (row - 2 >= 0 && col + 1 < board.GetLength(0) && board[row - 2, col + 1] == 'K') { br++; }
+            if (row - 1 >= 0 && col - 2 >= 0 && board[row - 1, col - 2] == 'K') { br++; }
+            if (row - 1 >= 0 && col + 2 < board.GetLength(0) && board[row - 1, col + 2] == 'K') { br++; }
+
+            if (row + 2 < board.GetLength(0) && col - 1 >= 0 && board[row + 2, col - 1] == 'K') { br++; }
+            if (row + 2 < board.GetLength(0) && col + 1 < board.GetLength(0) && board[row + 2, col + 1] == 'K') { br++; }
+            if (row + 1 < board.GetLength(0) && col - 2 >= 0 && board[row + 1, col - 2] == 'K') { br++; }
+            if (row + 1 < board.GetLength(0) && col + 2 < board.GetLength(0) && board[row + 1, col + 2] == 'K') { br++; }
+            return br;
+        }
+    }
+}
