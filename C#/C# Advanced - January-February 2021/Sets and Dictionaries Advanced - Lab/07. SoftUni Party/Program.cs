62ccc56587e807e7d07a54378eb0455ed83ad036
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-30 21:46:46 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Sets and Dictionaries Advanced - Lab/07. SoftUni Party/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> VIP = new HashSet<string>();
+            HashSet<string> regular = new HashSet<string>();
+            string command = Console.ReadLine();
+            while (command != "PARTY")
+            {
+                if (Char.IsDigit(command[0])) { VIP.Add(command); }
+                else { regular.Add(command); }
+                command = Console.ReadLine();
+            }
+            command = Console.ReadLine();
+            while (command != "END")
+            {
+                if (VIP.Contains(command)) { VIP.Remove(command); }
+                else if (regular.Contains(command)) { regular.Remove(command); }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(VIP.Count + regular.Count);
+            if (VIP.Count > 0) { Console.WriteLine(string.Join(Environment.NewLine, VIP)); }
+            if (regular.Count > 0) { Console.WriteLine(string.Join(Environment.NewLine, regular)); }
+        }
+    }
+}
