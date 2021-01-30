bbe89819b021e5702de7a689a527a51a87ddf1ab
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-30 21:45:54 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Sets and Dictionaries Advanced - Lab/06. Parking Lot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> cars = new HashSet<string>();
+            string[] command = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            while (command[0] != "END")
+            {
+                if(command[0] == "IN") { cars.Add(command[1]); }
+                else { cars.Remove(command[1]); }
+                command = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+            if(cars.Count == 0) { Console.WriteLine("Parking Lot is Empty"); }
+            else { Console.WriteLine(string.Join(Environment.NewLine, cars)); }
+        }
+    }
+}
