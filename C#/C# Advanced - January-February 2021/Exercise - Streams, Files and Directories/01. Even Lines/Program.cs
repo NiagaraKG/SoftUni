691e47a310092126fb97fb721d790c1ed44899d7
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-30 21:51:08 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Exercise - Streams, Files and Directories/01. Even Lines/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01._Even_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../text.txt"))
+            {
+                string line = reader.ReadLine();
+                int br = 0;
+                while (line != null)
+                {
+                    if(br%2 == 0) 
+                    {
+                        Regex pattern = new Regex(@"[-,.!?]");
+                        line = pattern.Replace(line, "@");
+                        string[] words = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                        words = words.Reverse().ToArray();                        
+                        Console.WriteLine(string.Join(" ", words));
+                    }
+                    line = reader.ReadLine();
+                    br++;
+                }
+            }
+        }
+    }
+}
