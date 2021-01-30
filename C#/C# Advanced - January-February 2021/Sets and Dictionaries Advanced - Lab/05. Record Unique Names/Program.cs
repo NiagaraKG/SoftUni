baddfbfebb84a2e230c3b279c7ffa5411b403c05
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-30 21:44:46 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Sets and Dictionaries Advanced - Lab/05. Record Unique Names/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> unique = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                unique.Add(name);
+            }
+            foreach (var item in unique)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
