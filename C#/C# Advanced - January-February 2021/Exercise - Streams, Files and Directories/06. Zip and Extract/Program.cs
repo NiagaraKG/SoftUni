718747b06ef8037a4e5f7774561b9540b53a180c
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-31 22:04:17 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Exercise - Streams, Files and Directories/06. Zip and Extract/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace _06._Zip_and_Extract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipFile.CreateFromDirectory(@"D:\ZipFileFrom", @"D:\ZipFileTo\result.zip");
+        }
+    }
+}
