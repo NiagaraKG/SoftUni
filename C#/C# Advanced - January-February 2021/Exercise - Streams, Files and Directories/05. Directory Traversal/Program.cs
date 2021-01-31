1685239376e878893db6b891101621fd8aa3b240
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-31 22:02:19 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Exercise - Streams, Files and Directories/05. Directory Traversal/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _05._Directory_Traversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> fileInfo = new Dictionary<string, Dictionary<string, double>>();
+            DirectoryInfo directoryInfo = new DirectoryInfo("../../../");
+            FileInfo[] files = directoryInfo.GetFiles();
+            foreach (var file in files)
+            {
+                if (!fileInfo.ContainsKey(file.Extension))
+                { fileInfo.Add(file.Extension, new Dictionary<string, double>()); }
+                fileInfo[file.Extension].Add(file.Name,file.Length/1000.00);
+            }
+            using (StreamWriter writer = new StreamWriter(@$"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\report.txt"))
+            {
+                foreach (var item in fileInfo.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+                {
+                    writer.WriteLine(item.Key);
+                    foreach (var file in item.Value.OrderBy(x => x.Value))
+                    {
+                        writer.WriteLine($"--{file.Key} - {file.Value}kb");
+                    }
+                }
+            }
+        }
+    }
+}
