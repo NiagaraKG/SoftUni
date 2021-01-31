d7afc8e2526e7d4b75ae49301c5f5ef832d508e7
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-01-31 22:00:35 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Exercise - Streams, Files and Directories/03. Word Count/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _03._Word_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] search = File.ReadAllLines(Path.Combine("..","..","..","words.txt"));
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+            foreach (string word in search) { counts.Add(word.ToLower(), 0); }
+            string[] text = File.ReadAllText(Path.Combine("..", "..", "..", "text.txt")).ToLower().Split(new string[] { " ", ",", ".", "!", "?", "-", Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in text) { if (counts.ContainsKey(word)) { counts[word]++; } }
+            counts = counts.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, y => y.Value);
+            List<string> output = counts.Select(x => $"{x.Key} - {x.Value}").ToList();
+            File.WriteAllLines(Path.Combine("..", "..", "..", "actualResult.txt"), output);
+        }
+    }
+}
