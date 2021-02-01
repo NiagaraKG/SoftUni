1346601eb34ee1c9d6bc6bedfccf3e214f617a5c
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-01 23:33:06 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> counts = new SortedDictionary<char, int>();
+            char[] characters = Console.ReadLine().ToCharArray();
+            for (int i = 0; i < characters.Length; i++)
+            {
+                if (counts.ContainsKey(characters[i])) { counts[characters[i]]++; }
+                else { counts.Add(characters[i], 1); }
+            }
+            foreach (var c in counts) { Console.WriteLine($"{c.Key}: {c.Value} time/s"); }
+        }
+    }
+}
