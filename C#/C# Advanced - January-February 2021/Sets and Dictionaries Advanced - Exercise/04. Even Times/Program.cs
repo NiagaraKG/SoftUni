cec5a07bf75236d47b5ba4d38c6dc9239f5a2358
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-01 23:32:32 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> counts = new Dictionary<int, int>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int current = int.Parse(Console.ReadLine());
+                if(counts.ContainsKey(current)) { counts[current]++; }
+                else { counts.Add(current, 1); }
+            }
+            foreach (var item in counts)
+            {
+                if(item.Value % 2 == 0) { Console.WriteLine(item.Key); }
+            }
+        }
+    }
+}
