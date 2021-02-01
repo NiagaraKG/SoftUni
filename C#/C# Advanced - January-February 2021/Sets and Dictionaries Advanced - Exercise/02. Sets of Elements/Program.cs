6d6f577cc80d1f2e226079f88061b53fa1b43027
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-01 23:31:28 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = Console.ReadLine().Split().Select(int.Parse).ToArray();            
+            HashSet<string> first = new HashSet<string>();
+            HashSet<string> second = new HashSet<string>();
+            for (int i = 0; i < n[0]; i++) { first.Add(Console.ReadLine()); }
+            for (int i = 0; i < n[1]; i++) { second.Add(Console.ReadLine()); }
+            foreach (var item in first)
+            {
+                if(second.Contains(item)) { Console.Write(item + " "); }
+            }
+        }
+    }
+}
