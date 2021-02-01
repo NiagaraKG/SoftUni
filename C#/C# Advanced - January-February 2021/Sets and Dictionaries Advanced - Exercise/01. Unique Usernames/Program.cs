0b9ffa9c5f1a6defafa6fb052884535746b65949
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-01 23:30:52 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> unique = new HashSet<string>();
+            for (int i = 0; i < n; i++) { unique.Add(Console.ReadLine()); }
+            Console.WriteLine(string.Join(Environment.NewLine, unique));
+        }
+    }
+}
