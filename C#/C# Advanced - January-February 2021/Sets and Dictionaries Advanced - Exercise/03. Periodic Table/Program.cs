ca89db681622e2732aacc6a576ec2cca6004da10
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-01 23:31:55 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<string> unique = new SortedSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] line = Console.ReadLine().Split();
+                for (int j = 0; j < line.Length; j++) { unique.Add(line[j]); }
+            }
+            Console.WriteLine(string.Join(" ", unique));
+        }
+    }
+}
