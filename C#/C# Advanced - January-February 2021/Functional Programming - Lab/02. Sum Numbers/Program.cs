9e1d94e05b64aa2d886aa9d29bc63bcd90d51f37
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-02 22:07:45 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Functional Programming - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
