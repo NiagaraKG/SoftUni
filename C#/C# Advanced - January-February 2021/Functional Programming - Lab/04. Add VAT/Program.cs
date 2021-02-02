c943b57dd9d8f6be028f4201c434f83af7b095e7
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-02 22:09:29 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Functional Programming - Lab/04. Add VAT/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] prices = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).Select(x => x * 1.2).ToArray();
+            foreach (var item in prices) { Console.WriteLine(item.ToString("0.00")); }
+        }
+    }
+}
