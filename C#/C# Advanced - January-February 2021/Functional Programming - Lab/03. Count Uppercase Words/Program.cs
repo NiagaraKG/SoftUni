1a987ea74af2e81cfa3bdc6f93b942b80f58ef9b
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-02 22:08:30 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(Environment.NewLine, Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(x => char.IsUpper(x[0])).ToArray()));
+        }
+    }
+}
