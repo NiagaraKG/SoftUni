d5f98a719bf112e1b304278fdc055bdb68d0e4db
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-03 23:41:59 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Functional Programming - Exercise/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int[]> add = x => { for (int i = 0; i < x.Length; i++) { x[i]++; } };
+            Action<int[]> muliply = x => { for (int i = 0; i < x.Length; i++) { x[i]*=2; } };
+            Action<int[]> subtract = x => { for (int i = 0; i < x.Length; i++) { x[i]--; } };
+            Action<int[]> print = x => Console.WriteLine(string.Join(" ", x));
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                switch (command)
+                {
+                    case "add": add(numbers); break;
+                    case "muliply": muliply(numbers); break;
+                    case "subtract": subtract(numbers); break;
+                    case "print": print(numbers); break;
+                    default: break;
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
