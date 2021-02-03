1e5a5e27d03f3f7bd7cf5d7e10c11c5c0776e94a
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-03 23:38:59 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Action_Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(Environment.NewLine, Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)));           
+        }
+    }
+}
