bb8bc534169ccf45757efdcb8037a96652d20ff3
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-03 23:39:44 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(Environment.NewLine, Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x=>x = "Sir " + x)));
+        }
+    }
+}
