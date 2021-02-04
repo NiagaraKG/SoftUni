47d61dc1618968c9e53892cca2a6b66ed137f252
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-04 22:48:42 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Functional Programming - Exercise/08. Custom Comparator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Custom_Comparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Array.Sort(numbers, (x, y) =>
+            {
+                if (x % 2 != 0 && y % 2 == 0) { return 1; }
+                else if (x % 2 == 0 && y % 2 != 0) { return -1; }
+                return x.CompareTo(y);
+            });
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
