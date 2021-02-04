7238351248ab191d62377c07557f1349f883a60e
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-04 22:47:56 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Functional Programming - Exercise/07. Predicate For Names/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Predicate_For_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Func<string, bool> filter = x => x.Length <= n;
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(filter).ToArray();
+            Console.WriteLine(String.Join(Environment.NewLine, names));
+        }
+    }
+}
