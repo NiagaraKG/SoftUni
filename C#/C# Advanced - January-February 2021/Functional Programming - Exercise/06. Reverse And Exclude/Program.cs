7adf9b57060ef1bb4c5962d6d0de9a5dbe410082
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-04 22:47:10 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Functional Programming - Exercise/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] collection = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Reverse().ToArray();
+            int n = int.Parse(Console.ReadLine());
+            Func<int, bool> divisible = x => x % n != 0;
+            Console.WriteLine(string.Join(" ", collection.Where(divisible).ToArray()));
+        }
+    }
+}
