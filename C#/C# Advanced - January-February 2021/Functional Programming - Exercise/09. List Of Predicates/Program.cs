8116466cfd3f6c58a278b831c62e3dae0a991c12
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-04 22:49:22 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Functional Programming - Exercise/09. List Of Predicates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._List_Of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; i++) { numbers[i] = i + 1; }
+            int[] dividers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Func<int, bool> filter = x =>
+            {
+                foreach (var d in dividers)
+                {
+                    if (x % d != 0) { return false; }
+                }
+                return true;
+            };
+            Console.WriteLine(string.Join(" ", numbers.Where(filter)));
+        }
+    }
+}
