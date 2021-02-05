126822baf2023d43b6639350aa0ffba2cee4a49b
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-05 22:30:49 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Functional Programming - Exercise/12. TriFunction/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _12._TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            foreach (var name in names)
+            {
+                int current = 0;
+                foreach (var c in name)
+                {
+                    current += c;
+                }
+                if(current>= s) { Console.WriteLine(name); return; }
+            }
+        }
+    }
+}
