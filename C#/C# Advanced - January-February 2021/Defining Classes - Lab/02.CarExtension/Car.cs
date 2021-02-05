2df738cf72cdf4a72be367eed20a8fcf42bc643f
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-05 22:35:38 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Defining Classes - Lab/02.CarExtension/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make, model;
+        private int year;
+        private double fuelQuantity, fuelConsumption;
+
+        public string Make { get { return this.make; } set { this.make = value; } }
+        public string Model { get { return this.model; } set { this.model = value; } }
+        public int Year { get { return this.year; } set { this.year = value; } }
+        public double FuelQuantity { get { return this.fuelQuantity; } set { this.fuelQuantity = value; } }
+        public double FuelConsumption { get { return this.fuelConsumption; } set { this.fuelConsumption = value; } }
+
+        public void Drive(double distance)
+        {
+            double fuelNeeded = fuelConsumption * distance/100;
+            if (fuelQuantity - fuelNeeded > 0) { FuelQuantity -= fuelNeeded; }
+            else { Console.WriteLine("Not enough fuel to perform this trip!"); }
+        }
+        public string WhoAmI() { return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L"; }
+    }
+}
