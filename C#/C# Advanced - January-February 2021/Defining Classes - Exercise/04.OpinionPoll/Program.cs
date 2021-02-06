90de2950d257e29589e8398d97824891489c75b5
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-06 23:05:29 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Defining Classes - Exercise/04.OpinionPoll/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                people.Add(new Person(input[0], int.Parse(input[1])));
+            }
+            people = people.Where(x => x.Age > 30).OrderBy(x => x.Name).ToList();
+            foreach (var p in people) { Console.WriteLine($"{p.Name} - {p.Age}"); }
+        }
+    }
+}
