3e3e2766eda7203570e323662c60c92b3181d0e7
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-06 23:06:00 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Defining Classes - Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05.DateModifier
+{
+    public class DateModifier
+    {
+       public static int Calculate(string first, string second)
+        {
+            int[] date = first.Split().Select(int.Parse).ToArray();
+            DateTime date1 = new DateTime(date[0], date[1], date[2]);
+            date = second.Split().Select(int.Parse).ToArray();
+            DateTime date2 = new DateTime(date[0], date[1], date[2]);
+            return Math.Abs((date1-date2).Days);
+        }
+    }
+}
---
+++ C#/C# Advanced - January-February 2021/Defining Classes - Exercise/05.DateModifier/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _05.DateModifier
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+            Console.WriteLine(DateModifier.Calculate(first, second));
+        }
+    }
+}
