d5b7c35d1659c9265ec72bf56befb38080653d14
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-06 23:04:54 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Defining Classes - Exercise/03.OldestFamilyMember/Family.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> members;
+        public Family() { this.members = new List<Person>(); }
+        public void AddMember(Person member) { this.members.Add(member); }
+        public Person GetOldestMember()
+        {
+            return this.members.OrderByDescending(x=>x.Age).First();
+        }
+    }
+}
---
+++ C#/C# Advanced - January-February 2021/Defining Classes - Exercise/03.OldestFamilyMember/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Family f = new Family();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                f.AddMember(new Person(input[0], int.Parse(input[1])));
+            }
+            Person oldest = f.GetOldestMember();
+            Console.WriteLine(oldest.Name + " " + oldest.Age);
+        }
+    }
+}
