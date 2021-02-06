ef653473d06248b6524525326f8233760005c40c
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-06 23:06:30 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Defining Classes - Exercise/06.SpeedRacing/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.SpeedRacing
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumotionPerKilometer { get; set; }
+        public double DistanceTraveled { get; set; }
+        public Car(string model, double fuel, double consumption)
+        {
+            this.Model = model;
+            this.FuelAmount = fuel;
+            this.FuelConsumotionPerKilometer = consumption;
+            this.DistanceTraveled = 0;
+        }
+        public void Drive(double km)
+        {
+            double FuelNeeded = km * FuelConsumotionPerKilometer;
+            if (FuelNeeded <= FuelAmount)
+            {
+                FuelAmount -= FuelNeeded;
+                DistanceTraveled += km;
+            }
+            else { Console.WriteLine("Insufficient fuel for the drive"); }
+        }
+    }
+}
---
+++ C#/C# Advanced - January-February 2021/Defining Classes - Exercise/06.SpeedRacing/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.SpeedRacing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+            int n = int.Parse(Console.ReadLine());
+            string[] input;
+            for (int i = 0; i < n; i++)
+            {
+                input = Console.ReadLine().Split();
+                cars.Add(new Car(input[0], double.Parse(input[1]),double.Parse(input[2])));
+            }
+            input = Console.ReadLine().Split();
+            while (input[0] != "End")
+            {
+                foreach (var c in cars)
+                {
+                    if(c.Model == input[1]) { c.Drive(double.Parse(input[2])); break; }
+                }
+                input = Console.ReadLine().Split();
+            }
+            foreach (var c in cars)
+            {
+                Console.WriteLine($"{c.Model} {c.FuelAmount:f2} {c.DistanceTraveled}");
+            }
+        }
+    }
+}
