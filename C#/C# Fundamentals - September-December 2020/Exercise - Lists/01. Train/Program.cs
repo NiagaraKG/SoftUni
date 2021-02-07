69c46444d93ec93c914026f0c677a02778ddf5ec
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-07 23:33:41 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Lists/01. Train/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int max = int.Parse(Console.ReadLine());
+            string[] command = Console.ReadLine().Split();
+            while (command[0] != "end")
+            {
+                if(command[0] == "Add")
+                {
+                    wagons.Add(int.Parse(command[1]));
+                }
+                else
+                {
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        int passengers = int.Parse(command[0]);
+                        if (wagons[i]+ passengers <= max)
+                        {
+                            wagons[i] += passengers;
+                            break;
+                        }
+                    }
+                }
+                command = Console.ReadLine().Split();
+            }
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
