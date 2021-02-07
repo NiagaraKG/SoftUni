c5d3ab021438b27efed8fda7e299a58df1f18f2d
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-07 23:34:15 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exercise - Lists/02. Change List/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Change_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string[] command = Console.ReadLine().Split();
+            while (command[0] != "end")
+            {
+                if (command[0] == "Delete")
+                {
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        int n = int.Parse(command[1]);
+                        if (numbers[i] == n)
+                        {
+                            numbers.RemoveAt(i--);
+                        }
+                    }
+                }
+                else
+                {
+                    numbers.Insert(int.Parse(command[2]), int.Parse(command[1]));
+                }
+                command = Console.ReadLine().Split();
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
