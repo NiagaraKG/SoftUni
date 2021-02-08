b80cdd59096cc9d91ef287d7bb2860727e9796e2
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-08 23:58:05 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Text Processing - More Exercise/02. Ascii Sumator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02._Ascii_Sumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char start = char.Parse(Console.ReadLine());
+            char end = char.Parse(Console.ReadLine());
+            char[] text = Console.ReadLine().ToCharArray();
+            int sum = 0;
+            foreach (var c in text)
+            {
+                if(c>start && c<end) { sum += c; }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
