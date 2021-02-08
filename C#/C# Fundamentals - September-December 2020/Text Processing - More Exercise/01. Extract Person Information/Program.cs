52c9405d5a3f39644616433ceee8a6b35d4d15e8
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-08 23:57:23 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Text Processing - More Exercise/01. Extract Person Information/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01._Extract_Person_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string line = Console.ReadLine();
+                int start = line.IndexOf("@") + 1;
+                int end = line.IndexOf("|");
+                string name = line.Substring(start, end - start);
+                start = line.IndexOf("#") + 1;
+                end = line.IndexOf("*");
+                string age = line.Substring(start, end - start);
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
