83530a6fafd6b9376d0fea55b804a9fd0180b5de
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-10 22:03:34 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Text Processing - More Exercise/05. HTML/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _05._HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = Console.ReadLine(); Console.WriteLine("<h1>\n" + title + "\n</h1>");
+            string article = Console.ReadLine(); Console.WriteLine("<article>\n" + article + "\n</article>");
+            string comment = Console.ReadLine();
+            while(comment != "end of comments")
+            {
+                Console.WriteLine("<div>\n" + comment + "\n</div>");
+                comment = Console.ReadLine();
+            }
+        }
+    }
+}
