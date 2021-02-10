dd83a94706079f3ea9ee76de787b97207bbbe484
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-10 22:11:09 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Generics - Lab/01.Box/Box.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> box;
+        public Box() { box = new Stack<T>(); }
+        public int Count => this.box.Count;
+        public void Add(T element) { this.box.Push(element); }
+        public T Remove() { return box.Pop(); }
+    }
+}
