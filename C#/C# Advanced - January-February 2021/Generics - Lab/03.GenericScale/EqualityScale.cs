6e23bf4ffd7e82dc6ce9778aa44757a05ca8a8ee
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-11 19:56:22 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Generics - Lab/03.GenericScale/EqualityScale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T> where T : IComparable
+    {
+        private T Left;
+        private T Right;
+        public EqualityScale(T left, T right) { this.Left = left; this.Right = right; }
+        public bool AreEqual() { return this.Left.CompareTo(this.Right) == 0; }
+    }
+}
