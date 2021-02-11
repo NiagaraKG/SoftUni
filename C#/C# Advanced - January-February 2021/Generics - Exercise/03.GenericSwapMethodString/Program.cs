cd3e5d5c76c23766180c03b0cb1b634aaed9e48a
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-11 20:02:23 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Generics - Exercise/03.GenericSwapMethodString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.GenericSwapMethodString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Box<string>> collection = new List<Box<string>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Box<string> b = new Box<string>(Console.ReadLine());
+                collection.Add(b);
+            }
+            int[] positions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            collection = Swap(collection, positions[0], positions[1]);
+            Console.WriteLine(string.Join(Environment.NewLine, collection));
+        }
+        public static List<T> Swap<T>(List<T> collection, int p1, int p2)
+        {
+            T temp = collection[p1];
+            collection[p1] = collection[p2];
+            collection[p2] = temp;
+            return collection;
+        }
+    }
+}
