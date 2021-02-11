904ac196c33f14121b29e5f519740a31308df007
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-11 19:59:17 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Generics - Exercise/01.GenericBoxOfString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01.GenericBoxOfString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {                
+                Box<string> b = new Box<string>(Console.ReadLine());
+                Console.WriteLine(b.ToString());
+            }
+        }
+    }
+}
