5a5844965641086eb24d316a2e596082a6700dde
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-11 20:03:57 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Generics - Exercise/05.GenericCountMethodString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.GenericCountMethodString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Box<string>> collection = new List<Box<string>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Box<string> b = new Box<string>(Console.ReadLine());
+                collection.Add(b);
+            }
+            Box<string> compateTo = new Box<string>(Console.ReadLine());
+            Console.WriteLine(Count(collection, compateTo));
+        }
+        public static int Count<T>(List<T> collection, T compareTo) where T : IComparable
+        {
+            int br = 0;
+            foreach (var item in collection) { if (item.CompareTo(compareTo) == 1) { br++; } }
+            return br;
+        }
+    }
+}
