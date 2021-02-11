3f3f31f76ed5079de4f9438815bf93c91652ab8d
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-11 20:01:41 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Generics - Exercise/02.GenericBoxOfInteger/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02.GenericBoxOfInteger
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Box<int> b = new Box<int>(int.Parse(Console.ReadLine()));
+                Console.WriteLine(b.ToString());
+            }
+        }
+    }
+}
