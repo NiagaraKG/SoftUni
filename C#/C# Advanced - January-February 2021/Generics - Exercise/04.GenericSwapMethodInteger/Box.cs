e6dbadd09b124d7313fde3f9725c8d2eca86e830
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-11 20:03:12 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Generics - Exercise/04.GenericSwapMethodInteger/Box.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.GenericSwapMethodInteger
+{
+    public class Box<T>
+    {
+        public T Value { get; set; }
+        public Box(T value) { this.Value = value; }
+        public override string ToString()
+        {
+            return $"{Value.GetType()}: {Value}";
+        }
+    }
+}
---
+++ C#/C# Advanced - January-February 2021/Generics - Exercise/04.GenericSwapMethodInteger/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.GenericSwapMethodInteger
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Box<int>> collection = new List<Box<int>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Box<int> b = new Box<int>(int.Parse(Console.ReadLine()));
+                collection.Add(b);
+            }
+            int[] positions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            collection = Swap(collection, positions[0], positions[1]);
+            Console.WriteLine(string.Join(Environment.NewLine, collection));
+        }
+        public static List<T> Swap<T>(List<T> collection, int p1, int p2)
+        {
+            T temp = collection[p1];
+            collection[p1] = collection[p2];
+            collection[p2] = temp;
+            return collection;
+        }
+    }
+}
