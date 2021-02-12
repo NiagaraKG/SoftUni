94e2ca55e0791fd59134cb183472dcf4248310fe
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-12 20:23:36 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Generics - Exercise/08.Threeuple/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.Threeuple
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split().ToList();
+            string name = input[0] + " " + input[1];
+            string adress = input[2];
+            input.RemoveAt(0);input.RemoveAt(0);input.RemoveAt(0);           
+            Console.WriteLine(new Threeuple<string, string, string>(name, adress, string.Join(" ", input)));
+            input = Console.ReadLine().Split().ToList();
+            bool isDrunk = input[2] == "drunk" ? true : false;
+            Console.WriteLine(new Threeuple<string, int, bool>(input[0], int.Parse(input[1]), isDrunk));
+            input = Console.ReadLine().Split().ToList();
+            Console.WriteLine(new Threeuple<string, double, string>(input[0], double.Parse(input[1]), input[2]));
+        }
+    }
+}
---
+++ C#/C# Advanced - January-February 2021/Generics - Exercise/08.Threeuple/Threeuple.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08.Threeuple
+{
+    public class Threeuple<T1, T2, T3>
+    {   
+        public T1 item1 { get; set; }
+        public T2 item2 { get; set; }
+        public T3 item3 { get; set; }
+        public Threeuple(T1 first, T2 second, T3 third) { this.item1 = first; this.item2 = second; this.item3 = third; }
+        public override string ToString() { return $"{this.item1} -> {this.item2} -> {this.item3}"; }
+    }
+}
