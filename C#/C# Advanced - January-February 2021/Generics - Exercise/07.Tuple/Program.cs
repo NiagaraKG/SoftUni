8e67953b9bb80f1d9ade03635e87d217466d5e46
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-12 20:23:07 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Generics - Exercise/07.Tuple/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.Tuple
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            string name = input[0] + " " + input[1];
+            string adress = input[2];
+            Console.WriteLine(new Tuple<string, string>(name, adress));
+            input = Console.ReadLine().Split();
+            Console.WriteLine(new Tuple<string, int>(input[0], int.Parse(input[1])));
+            input = Console.ReadLine().Split();
+            Console.WriteLine(new Tuple<int, double>(int.Parse(input[0]), double.Parse(input[1])));
+        }
+    }
+}
---
+++ C#/C# Advanced - January-February 2021/Generics - Exercise/07.Tuple/Tuple.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.Tuple
+{
+    public class Tuple<T1, T2>
+    {   
+        public T1 item1 { get; set; }
+        public T2 item2 { get; set; }
+        public Tuple(T1 first, T2 second) { this.item1 = first; this.item2 = second; }
+        public override string ToString() { return $"{this.item1} -> {this.item2}"; }
+    }
+}
