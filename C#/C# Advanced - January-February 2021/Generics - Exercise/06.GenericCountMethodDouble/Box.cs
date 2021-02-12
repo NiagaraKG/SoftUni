e056df82a6e9db57f7d313336bbe80c12cbf8f7f
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-12 20:22:03 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Generics - Exercise/06.GenericCountMethodDouble/Box.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.GenericCountMethodDouble
+{
+    public class Box<T> : IComparable
+        where T : IComparable
+    {
+        public T Value { get; set; }
+        public Box(T value) { this.Value = value; }
+        public override string ToString() { return $"{Value.GetType()}: {Value}"; }
+        public int CompareTo(object obj)
+        {
+            Box<T> B = obj as Box<T>;
+            return this.Value.CompareTo(B.Value);
+        }
+    }
+}
---
+++ C#/C# Advanced - January-February 2021/Generics - Exercise/06.GenericCountMethodDouble/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.GenericCountMethodDouble
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Box<double>> collection = new List<Box<double>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Box<double> b = new Box<double>(double.Parse(Console.ReadLine()));
+                collection.Add(b);
+            }
+            Box<double> compateTo = new Box<double>(double.Parse(Console.ReadLine()));
+            Console.WriteLine(Count(collection, compateTo));
+        }
+        public static int Count<T>(List<T> collection, T compareTo) where T : IComparable
+        {
+            int br = 0;
+            foreach (var item in collection) { if (item.CompareTo(compareTo) == 1) { br++; } }
+            return br;
+        }
+    }
+}
