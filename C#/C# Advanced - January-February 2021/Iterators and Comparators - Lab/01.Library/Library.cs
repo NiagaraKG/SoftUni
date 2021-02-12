364e4986c56f2de9deab7de23051c5db73460814
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-12 20:27:03 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Iterators and Comparators - Lab/01.Library/Library.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library
+    {
+        public List<Book> books { get; set; }
+        public Library(params Book[] books) { this.books = new List<Book>(books); }
+    }
+}
