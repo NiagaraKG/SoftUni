ba9a0f48dcaee81559e621708b90572d2315b241
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-13 22:11:51 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/More Exercises - Lists/01. Messaging/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> indexes = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string message = Console.ReadLine();
+            string result = "";
+            for (int i = 0; i < indexes.Count; i++)
+            {
+                int sum = 0;
+                while (indexes[i] > 0)
+                {
+                    sum += indexes[i] % 10;
+                    indexes[i] /= 10;
+                }
+                while(sum >= message.Length)
+                { sum -= message.Length; }
+                result += message[sum];
+                message = message.Remove(sum, 1);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
