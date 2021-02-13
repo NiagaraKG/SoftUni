6420adb02b92870a1be42228780a7e84c0d21c4b
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-13 22:06:56 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Iterators and Comparators - Lab/04.BookComparer/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public string Title { get; private set; }
+        public int Year { get; private set; }
+        public List<string> Authors { get; private set; }
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors.ToList();
+        }
+
+        public int CompareTo(Book other)
+        {
+            int result = this.Title.CompareTo(other.Title);
+            if (result == 0) { result = other.Year.CompareTo(this.Year); }
+            return result;
+        }
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+    }
+}
---
+++ C#/C# Advanced - January-February 2021/Iterators and Comparators - Lab/04.BookComparer/BookComparator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class BookComparator : IComparer<Book>
+    {
+        public int Compare(Book A, Book B)
+        {
+            int result = A.Title.CompareTo(B.Title);
+            if (result == 0) { result = A.CompareTo(B); }
+            return result;
+        }
+    }
+}
