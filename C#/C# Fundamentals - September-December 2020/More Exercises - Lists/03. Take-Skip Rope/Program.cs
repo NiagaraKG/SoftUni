b9516f952b6b4ddae3571c8fd18065d31796da6c
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-13 22:13:32 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/More Exercises - Lists/03. Take-Skip Rope/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Take_Skip_Rope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<int> digits = new List<int>();
+            List<char> symbols = new List<char>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(Char.IsDigit(input[i]))
+                {                    
+                    digits.Add(input[i] - '0'); 
+                }
+                else { symbols.Add(input[i]); }
+            }
+           
+            int textindex = 0, br = 0;
+            string result = "";
+            for (int i = 0; i < digits.Count; i++)
+            {
+                if(i%2==0)
+                {
+                    while (br < digits[i] && textindex < symbols.Count)
+                    {
+                        result += symbols[textindex];
+                        br++;
+                        textindex++;
+                    }
+                    br = 0;
+                }
+                else 
+                {
+                    while (br < digits[i] && textindex < symbols.Count)
+                    {                        
+                        br++;
+                        textindex++;
+                    }
+                    br = 0;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
