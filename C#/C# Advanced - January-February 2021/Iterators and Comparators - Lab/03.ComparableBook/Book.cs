5288c59bd30a1c51ef7229c902f8d32b3e7094fc
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-13 22:05:52 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Iterators and Comparators - Lab/03.ComparableBook/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public string Title { get; private set; }
+        public int Year { get; private set; }
+        public List<string> Authors { get; private set; }
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors.ToList();
+        }
+
+        public int CompareTo(Book other)
+        {
+            int result = this.Year.CompareTo(other.Year);
+            if(result == 0)
+            {
+                result = this.Title.CompareTo(other.Title);
+            }
+            return result;
+        }
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+    }
+}
