925b874b4a7058af3926f042bd4320c8c7269268
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-14 22:05:25 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Iterators and Comparators - Exercise/05.ComparingObjects/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _05.ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+
+        public Person(string[] input)
+        {
+            this.Name = input[0];
+            this.Age = int.Parse(input[1]);
+            this.Town = input[2];
+        }
+        public int CompareTo(Person other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+            if (result == 0) { result = this.Age.CompareTo(other.Age); }
+            if (result == 0) { result = this.Town.CompareTo(other.Town); }
+            return result;
+        }
+    }
+}
---
+++ C#/C# Advanced - January-February 2021/Iterators and Comparators - Exercise/05.ComparingObjects/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.ComparingObjects
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            while (command[0] != "END")
+            {
+                people.Add(new Person(command));
+                command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+            int index = int.Parse(Console.ReadLine()) - 1;
+            int matches = 0, diff = 0;
+            foreach (var p in people)
+            {
+                if (p.CompareTo(people[index]) == 0) { matches++; }
+                else { diff++; }
+            }
+            if (matches == 1) { Console.WriteLine("No matches"); }
+            else { Console.WriteLine($"{matches} {diff} {matches + diff}"); }
+        }
+    }
+}
