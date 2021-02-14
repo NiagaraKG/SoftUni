7d587ee8dd5bf96378666196370fbc299390c338
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-14 22:04:30 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Iterators and Comparators - Exercise/03.Stack/CustomStack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Stack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        public List<T> elements;
+        public CustomStack() { this.elements = new List<T>(); }
+        public void Push(params T[] data)
+        {
+            foreach (var item in data)
+            {
+                elements.Add(item);
+            }
+        }
+        public void Pop() 
+        {
+            if (elements.Count == 0) { Console.WriteLine("No elements"); }
+            else { this.elements.RemoveAt(elements.Count - 1); }
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = elements.Count - 1; i >= 0; i--)
+            {
+                yield return elements[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ C#/C# Advanced - January-February 2021/Iterators and Comparators - Exercise/03.Stack/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Stack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CustomStack<string> collection = new CustomStack<string>();
+            string[] input = Console.ReadLine().Split(new string[] { " ", ", "}, StringSplitOptions.RemoveEmptyEntries);
+            while (input[0] != "END")
+            {
+                switch (input[0])
+                {
+                    case "Push": collection.Push(input.Skip(1).ToArray()); break;
+                    case "Pop": collection.Pop(); break;
+                }
+                input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+            foreach (var item in collection) { Console.WriteLine(item); }
+            foreach (var item in collection) { Console.WriteLine(item); }
+        }
+    }
+}
