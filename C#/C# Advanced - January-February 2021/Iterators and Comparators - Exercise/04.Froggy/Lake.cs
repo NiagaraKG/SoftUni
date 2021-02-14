973b129d2f84e1b6e68d0fee287b39d7d8ab5ab9
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-14 22:04:56 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Iterators and Comparators - Exercise/04.Froggy/Lake.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.Froggy
+{
+    public class Lake<T> : IEnumerable<T>
+    {
+        private List<T> stones;
+        public Lake(params T[] input) { this.stones = new List<T>(input); }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < stones.Count; i+=2)
+            {
+                yield return stones[i];
+            }
+            int index = stones.Count - 1;
+            if(index%2==0) { index--; }
+            for (int i = index; i >=0; i-=2)
+            {
+                yield return stones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() { return this.GetEnumerator(); }
+    }
+}
---
+++ C#/C# Advanced - January-February 2021/Iterators and Comparators - Exercise/04.Froggy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Froggy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Lake<int> stones = new Lake<int>(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+            string result = "";
+            foreach (var item in stones) { result += item + ", "; }
+            result = result.TrimEnd(new char[] { ',', ' '});
+            Console.WriteLine(result);
+        }
+    }
+}
