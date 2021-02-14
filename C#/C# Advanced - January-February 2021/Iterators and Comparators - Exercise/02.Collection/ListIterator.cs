216ebd4a56aabf3d0a384b3313fc09472ce45d24
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-14 22:04:00 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Iterators and Comparators - Exercise/02.Collection/ListIterator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.Collection
+{
+    public class ListIterator<T> : IEnumerable<T>
+    {
+        private List<T> collection { get; set; }
+        private int index;
+        public ListIterator(params T[] elements) { this.collection = new List<T>(elements); this.index = 0; }
+        public bool HasNext() { return this.index < this.collection.Count - 1; }
+        public bool Move()
+        {
+            if (this.HasNext()) { this.index++; return true; }
+            return false;
+        }
+        public void Print()
+        {
+            if (this.index > this.collection.Count || collection.Count == 0)
+            { throw new InvalidOperationException("Invalid Operation!"); }
+            Console.WriteLine(this.collection[this.index]);
+        }
+        public void PrintAll()
+        {
+            if (collection.Count == 0)
+            { throw new InvalidOperationException("Invalid Operation!"); }
+            Console.WriteLine(string.Join(" ", collection));
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in this.collection)
+            {
+                yield return item;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator() { return this.GetEnumerator(); }
+    }
+}
