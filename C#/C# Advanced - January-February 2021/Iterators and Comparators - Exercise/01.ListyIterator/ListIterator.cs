1d49dabe957d0f05310337c89db5a50f683bbe10
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-14 22:03:13 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Iterators and Comparators - Exercise/01.ListyIterator/ListIterator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.ListyIterator
+{
+    public class ListIterator<T>
+    {
+        private List<T> collection { get; set; }
+        private int index;
+        public ListIterator(params T[] elements) { this.collection = new List<T>(elements); this.index = 0; }
+        public bool HasNext() { return this.index < this.collection.Count - 1; }
+        public bool Move()
+        {
+            if (this.HasNext()) { this.index++; return true; }
+            return false;
+        }
+        public void Print()
+        {
+            if (this.index > this.collection.Count || collection.Count == 0) 
+            { throw new InvalidOperationException("Invalid Operation!"); }
+            Console.WriteLine(this.collection[this.index]);
+        }
+    }
+}
---
+++ C#/C# Advanced - January-February 2021/Iterators and Comparators - Exercise/01.ListyIterator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ListyIterator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] items = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1).ToArray();
+            ListIterator<string> collection = new ListIterator<string>(items);
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                switch (command)
+                {
+                    case "Move": Console.WriteLine(collection.Move()); break;
+                    case "HasNext": Console.WriteLine(collection.HasNext()); break;
+                    case "Print":
+                        try { collection.Print(); }
+                        catch (InvalidOperationException ex) { Console.WriteLine(ex.Message); }
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
