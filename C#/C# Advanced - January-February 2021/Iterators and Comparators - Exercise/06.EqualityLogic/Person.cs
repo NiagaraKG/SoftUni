735f45b1fbcb57bc0b1cbaf5d1f432a8a7cf59da
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-14 22:05:57 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Iterators and Comparators - Exercise/06.EqualityLogic/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _06.EqualityLogic
+{
+    public class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person(string[] input) { this.Name = input[0]; this.Age = int.Parse(input[1]); }
+        public int CompareTo(Person other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+            if (result == 0) { result = this.Age.CompareTo(other.Age); }
+            return result;
+        }
+        public override int GetHashCode() { return this.Name.GetHashCode() + this.Age.GetHashCode(); }
+        public override bool Equals(object obj) { return this.GetHashCode() == obj.GetHashCode(); }
+    }
+}
---
+++ C#/C# Advanced - January-February 2021/Iterators and Comparators - Exercise/06.EqualityLogic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.EqualityLogic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<Person> sorted = new SortedSet<Person>();
+            HashSet<Person> hash = new HashSet<Person>();
+            for (int i = 0; i < n; i++)
+            {
+                Person current = new Person(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries));
+                sorted.Add(current);
+                hash.Add(current);
+            }
+            Console.WriteLine(sorted.Count);
+            Console.WriteLine(hash.Count);
+        }
+    }
+}
