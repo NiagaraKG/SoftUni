3a1d1efb412f4048e1a34d41800c0a329367b34c
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-17 22:14:25 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Objects and Classes - Lab/01. Randomize Words/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Randomize_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine().Split().ToList();
+            Random r = new Random();
+            for (int i = 0; i < words.Count; i++)
+            {
+                string word = words[i];
+                int index = r.Next(0, words.Count);
+                words[i] = words[index];
+                words[index] = word;
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
