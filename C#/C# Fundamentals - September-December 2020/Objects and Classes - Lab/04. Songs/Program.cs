31fb14851eb94e7aa4c541167b65eef4c691c859
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-17 22:15:38 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Objects and Classes - Lab/04. Songs/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Songs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Song> all = new List<Song>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] current = Console.ReadLine().Split('_');
+                Song curr = new Song();
+                curr.type = current[0];
+                curr.name = current[1];
+                curr.time = current[2];
+                all.Add(curr);
+            }
+            string type = Console.ReadLine();
+            if (type != "all")
+            {
+                foreach (var item in all)
+                {
+                    if (item.type == type)
+                    {
+                        Console.WriteLine(item.name);
+                    }
+                }
+            }
+            else
+            {
+                foreach (var item in all)
+                {
+                    Console.WriteLine(item.name);
+                }
+            }
+        }
+    }
+
+    class Song
+    {
+        public string type { get; set; }
+        public string name { get; set; }
+        public string time { get; set; }
+    }
+
+}
