2c2181cc55bbce51eff052bb14ade569410eb7d2
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-18 19:22:39 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Basic Algorithms - Exercise/03.SumOfCoins/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.SumOfCoins
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> values = Console.ReadLine().Split(new string[] { ": ", ", " }, StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse).OrderByDescending(c => c).ToList();
+            int sum = Console.ReadLine().Split(": ").Skip(1).Select(int.Parse).First();
+
+            try
+            {
+                var selectedCoins = ChooseCoins(values, sum);
+
+                Console.WriteLine($"Number of coins to take: {selectedCoins.Values.Sum()}");
+                foreach (var selectedCoin in selectedCoins)
+                {
+                    Console.WriteLine($"{selectedCoin.Value} coin(s) with value {selectedCoin.Key}");
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+           
+        }
+        public static Dictionary<int, int> ChooseCoins(List<int> coins, int targetSum)
+        {
+            var chosen = new Dictionary<int, int>();
+            int currentSum = 0, index = 0;
+            while (currentSum != targetSum && index < coins.Count)
+            {
+                int currentCoin = coins[index];
+                int remainingSum = targetSum - currentSum;
+                int numberOfCoins = remainingSum / currentCoin;
+                if (numberOfCoins > 0)
+                {
+                    chosen.Add(currentCoin, numberOfCoins);
+                    currentSum += numberOfCoins * currentCoin;
+                }
+                index++;
+            }
+            if (currentSum == targetSum) { return chosen; }
+            else
+            {
+                throw new InvalidOperationException("Error");
+            }
+        }
+    }
+}
