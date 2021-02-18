7599044fb82daef7a3b438211202f3ae87226e19
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-18 19:22:10 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Basic Algorithms - Exercise/02.RecursiveFactorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SumOfCoins
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(n));
+        }
+        private static long Factorial(int n)
+        {
+            if(n == 0) { return 1; }
+            if (n == 1) { return n; }
+            return n * Factorial(n - 1);
+        }
+    }
+}
