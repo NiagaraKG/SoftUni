0e158d2bc30155a1095c0e637eafd646e077aacb
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-18 19:21:25 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Basic Algorithms - Exercise/01.RecursiveArraySum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.RecursiveArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int s = Sum(arr, 0);
+            Console.WriteLine(s);
+        }
+
+        private static int Sum(int[] arr, int index)
+        {
+            if(index == arr.Length - 1) { return arr[index]; }
+            return arr[index] + Sum(arr, index+1);
+        }
+    }
+}
