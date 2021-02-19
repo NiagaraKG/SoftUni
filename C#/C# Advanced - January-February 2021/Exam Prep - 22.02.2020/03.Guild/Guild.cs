cddaf865471303e716a8697d189c3bead9f45e26
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-19 21:13:37 +0200

    Add files via upload

---
+++ C#/C# Advanced - January-February 2021/Exam Prep - 22.02.2020/03.Guild/Guild.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+        private List<Player> roster;
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count => this.roster.Count();
+        public Guild(string name, int capacity)
+        {
+            this.Name = name; this.Capacity = capacity;
+            this.roster = new List<Player>();
+        }
+        public void AddPlayer(Player p) { if (this.roster.Count < this.Capacity) { this.roster.Add(p); } }
+        public bool RemovePlayer(string name)
+        {
+            if (this.roster.Any(p => p.Name == name))
+            {
+                this.roster.RemoveAll(p => p.Name == name);
+                return true;
+            }
+            return false;
+        }
+        public void PromotePlayer(string name)
+        { this.roster.First(p => p.Name == name).Rank = "Member"; }
+        public void DemotePlayer(string name)
+        { this.roster.First(p => p.Name == name).Rank = "Trial"; }
+        public Player[] KickPlayersByClass(string clas)
+        {
+            Player[] removed = roster.Where(p => p.Class == clas).ToArray();
+            roster = roster.Where(p => p.Class != clas).ToList();
+            return removed;
+        }
+        public string Report()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"Players in the guild: {this.Name}");
+            foreach (var p in this.roster) { result.AppendLine(p.ToString()); }
+            return result.ToString().Trim();
+        }
+    }
+}
---
+++ C#/C# Advanced - January-February 2021/Exam Prep - 22.02.2020/03.Guild/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guild
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Rank { get; set; }
+        public string Description { get; set; }
+        public Player(string name, string clas)
+        {
+            this.Name = name;
+            this.Class = clas;
+            this.Rank = "Trial";
+            this.Description = "n/a";
+        }
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"Player {this.Name}: {this.Class}");
+            result.AppendLine($"Rank: {this.Rank}");
+            result.AppendLine($"Description: {this.Description}");
+            return result.ToString().Trim();
+        }
+    }
+}
