841dcb3d8b9394f823b07e28de5c1ba9c5731a41
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-19 21:18:01 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Objects and Classes - Lab/07. Store Boxes/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _07._Store_Boxes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<Box> all = new List<Box>();
+            while(input != "end")
+            {
+                string[] current = input.Split();
+                int serial = int.Parse(current[0]);
+                string name = current[1];
+                int quantity = int.Parse(current[2]);
+                double price = double.Parse(current[3]);
+                double boxPrice = quantity * price;
+                Box box = new Box();
+                box.item = new Item();
+                box.SerialNum = serial;
+                box.Quantity = quantity;
+                box.PriceForBox = boxPrice;
+                box.item.Name = name;
+                box.item.Price = price;
+                all.Add(box);
+                input = Console.ReadLine();
+            }
+            all = all.OrderByDescending(x => x.PriceForBox).ToList();
+            foreach (var i in all)
+            {
+                Console.WriteLine(i.SerialNum);
+                Console.WriteLine($"-- {i.item.Name} - ${i.item.Price:F2}: {i.Quantity}");
+                Console.WriteLine($"-- ${i.PriceForBox:F2}");
+            }
+        }
+    }
+
+    class Item
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+    class Box
+    {
+        public int SerialNum { get; set; }
+        public Item item { get; set; }
+        public int Quantity { get; set; }
+        public double PriceForBox { get; set; }
+    }
+
+}
