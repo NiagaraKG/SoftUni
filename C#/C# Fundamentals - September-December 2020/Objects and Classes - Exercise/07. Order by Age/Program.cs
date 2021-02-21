2bf656e310c45bb7d183df4d37b355fdd2c3de28
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-21 21:50:15 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Objects and Classes - Exercise/07. Order by Age/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Order_by_Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            List<Person> all = new List<Person>();
+            while(command != "End")
+            {
+                string[] input = command.Split();
+                Person current = new Person(input);
+                all.Add(current);
+                command = Console.ReadLine();
+            }
+            all = all.OrderBy(x => x.Age).ToList();
+            Console.WriteLine(string.Join(Environment.NewLine, all));
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public int Age { get; set; }
+
+        public Person(string[] input)
+        {
+            this.Name = input[0];
+            this.ID = input[1];
+            this.Age = int.Parse(input[2]);
+        }
+
+        public override string ToString()
+        {
+            string result = this.Name + " with ID: " + this.ID + $" is {this.Age} years old.";
+            return result;
+        }
+
+    }
+
+}
