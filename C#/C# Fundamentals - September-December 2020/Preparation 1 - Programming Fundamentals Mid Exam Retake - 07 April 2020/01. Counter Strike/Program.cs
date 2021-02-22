82d71b129ace83880d43b07efc7e24a41d56ff39
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-22 22:38:35 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Preparation 1 - Programming Fundamentals Mid Exam Retake - 07 April 2020/01. Counter Strike/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01._Counter_Strike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            int wins = 0;
+            while (command != "End of battle" && energy >= 0)
+            {
+                int distance = int.Parse(command);
+                if(distance <= energy)
+                {
+                    energy -= distance;
+                    wins++;
+                    if (wins % 3 == 0) { energy += wins; }
+                }
+                else
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {wins} won battles and {energy} energy");
+                    return;
+                }
+                command = Console.ReadLine();
+            }
+            if (command == "End of battle") { Console.WriteLine($"Won battles: {wins}. Energy left: {energy}"); }
+        }
+    }
+}
