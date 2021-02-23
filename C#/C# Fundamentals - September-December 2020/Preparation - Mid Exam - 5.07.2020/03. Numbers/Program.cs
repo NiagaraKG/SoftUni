6aeec476d39168968e94f3a80918396292c6fdf7
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-23 20:33:07 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Preparation - Mid Exam - 5.07.2020/03. Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _03._Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> input = Console.ReadLine().Split().Select(double.Parse).ToList();
+            List<double> above = new List<double>();
+            double av = input.Average();
+            foreach (var item in input)
+            { if (item > av) { above.Add(item); } }
+            if(above.Count == 0) { Console.WriteLine("No"); return; }
+            above.Sort((x, y) => y.CompareTo(x));
+            for (int i = 0; i < above.Count && i < 5; i++)
+            {
+                Console.Write(above[i] + " ");
+            }
+        }
+    }
+}
