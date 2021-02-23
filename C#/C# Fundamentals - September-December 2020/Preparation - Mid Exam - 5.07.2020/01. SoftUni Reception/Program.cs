aadf765a234347624c493e7aa1020fd6780644dc
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-23 20:31:10 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Preparation - Mid Exam - 5.07.2020/01. SoftUni Reception/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01._SoftUni_Reception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+            int t = int.Parse(Console.ReadLine());
+            int all = int.Parse(Console.ReadLine());
+            int h = 0, hour = f + s + t;
+            while (all > 0)
+            {
+                all -= hour;
+                h++;
+                if (h % 4 == 0) { h++; }
+            }
+            Console.WriteLine($"Time needed: {h}h.");
+        }
+    }
+}
