d650cbfb1cf48ad757b180168e420a5a8d8cd940
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-24 22:17:28 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Preparation - Mid Exam Retake - 12.08.2020/01. Computer Store/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01._Computer_Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double sum = 0;
+            while(input != "special" && input != "regular")
+            {
+                double price = double.Parse(input);
+                if (price > 0) { sum += price; }  
+                else { Console.WriteLine("Invalid price!"); }
+                input = Console.ReadLine();
+            }
+            if(sum == 0) { Console.WriteLine("Invalid order!"); return; }
+            double taxes = sum * 20 / 100;
+            double total = sum + taxes;
+            if(input == "special") { total *= 0.9; }
+            Console.WriteLine("Congratulations you've just bought a new computer!");
+            Console.WriteLine($"Price without taxes: {sum:F2}$");
+            Console.WriteLine($"Taxes: {taxes:F2}$");
+            Console.WriteLine("-----------");
+            Console.WriteLine($"Total price: {total:F2}$");
+        }
+    }
+}
