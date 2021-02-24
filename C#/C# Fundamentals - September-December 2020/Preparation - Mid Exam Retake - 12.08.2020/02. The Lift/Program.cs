122c160aa4eba56d6a202bcbe0b1efc084d5d067
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-24 22:18:11 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Preparation - Mid Exam Retake - 12.08.2020/02. The Lift/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._The_Lift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            List<int> lift = Console.ReadLine().Split().Select(int.Parse).ToList();
+            for (int i = 0; i < lift.Count; i++)
+            {
+                if(lift[i] < 4)
+                {
+                    int space = 4 - lift[i];
+                    if(people != 0)
+                    {
+                        if(people < space) { space = people; }
+                        people -= space;                        
+                        lift[i] += space;
+                    }                    
+                }
+            }
+            bool isFull = true;
+            foreach (var wagon in lift)
+            {
+                if(wagon < 4) { isFull = false; break; }
+            }
+            if(isFull && people > 0) 
+            { Console.WriteLine($"There isn't enough space! {people} people in a queue!"); }
+            else if (!isFull && people == 0)
+            { Console.WriteLine("The lift has empty spots!"); }
+            Console.WriteLine(string.Join(" ", lift));
+        }
+    }
+}
