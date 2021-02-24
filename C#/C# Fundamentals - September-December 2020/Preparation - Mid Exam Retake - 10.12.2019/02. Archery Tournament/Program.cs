80d4d597af8d54e07d2d7185215191c9e0f03d64
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-24 22:13:59 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Preparation - Mid Exam Retake - 10.12.2019/02. Archery Tournament/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Archery_Tournament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> targets = Console.ReadLine().Split("|").Select(int.Parse).ToList();
+            string[] command = Console.ReadLine().Split("@").ToArray();
+            int points = 0;
+            while (command[0] != "Game over")
+            {
+                if(command[0] == "Reverse") { targets.Reverse(); }
+                else
+                {
+                    int index = int.Parse(command[1]);
+                    int length = int.Parse(command[2]);
+                    if(index >= 0 && index < targets.Count)
+                    {
+                        if(command[0] == "Shoot Left")
+                        {
+                            index -= length;
+                            while(index < 0) { index += targets.Count; }
+                        }
+                        else
+                        {
+                            index += length;
+                            while(index > targets.Count) { index -= targets.Count; }
+                        }
+                        if (targets[index] >= 5) { points += 5; targets[index] -= 5; }
+                        else { points += targets[index]; targets[index] = 0; }
+                    }                   
+                }
+                command = Console.ReadLine().Split("@").ToArray();
+            }
+            Console.WriteLine(string.Join(" - ", targets));
+            Console.WriteLine($"Iskren finished the archery tournament with {points} points!");
+        }
+    }
+}
