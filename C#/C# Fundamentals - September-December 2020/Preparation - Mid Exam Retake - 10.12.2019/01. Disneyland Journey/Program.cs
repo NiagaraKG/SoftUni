5735b7d3b6a8e3d3db47e41729ca3965c5cfbe6e
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-24 22:12:32 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Preparation - Mid Exam Retake - 10.12.2019/01. Disneyland Journey/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01._Disneyland_Journey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cost = double.Parse(Console.ReadLine());
+            int months = int.Parse(Console.ReadLine());
+            double sum = 0.00;
+            for (int i = 1; i <= months; i++)
+            {
+                if(i%2==1 && i != 1)
+                { sum = sum * 0.84; }
+                if(i % 4 == 0) { sum = sum * 1.25; }
+                sum += cost * 0.25;
+            }
+            double diff = Math.Abs(sum - cost);
+            if(sum >= cost)
+            { Console.WriteLine($"Bravo! You can go to Disneyland and you will have {diff:F2}lv. for souvenirs.");}
+            else { Console.WriteLine($"Sorry. You need {diff:F2}lv. more."); }
+        }
+    }
+}
