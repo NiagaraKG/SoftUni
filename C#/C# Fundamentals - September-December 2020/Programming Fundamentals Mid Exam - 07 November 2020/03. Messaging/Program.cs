907aaa02c54d20810403efa70ff6fdd123fa002a
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-25 22:13:39 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Programming Fundamentals Mid Exam - 07 November 2020/03. Messaging/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> chat = new List<string>();
+            string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            while (command[0] != "end")
+            {
+                if (command[0] == "Chat")
+                {
+                    chat.Add(command[1]);
+                }
+                else if (command[0] == "Delete")
+                {
+                    if (chat.Contains(command[1])) { chat.Remove(command[1]); }
+                }
+                else if (command[0] == "Edit")
+                {
+                    int i = chat.IndexOf(command[1]);
+                    chat[i] = command[2];
+                }
+                else if (command[0] == "Pin")
+                {
+                    chat.Remove(command[1]);
+                    chat.Add(command[1]);
+                }
+                else if (command[0] == "Spam")
+                {
+                    for (int i = 1; i < command.Length; i++)
+                    {
+                        chat.Add(command[i]);
+                    }
+                }
+                command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, chat));
+        }
+    }
+}
