6b06b1f1be1b01a4c697a0cc053bdb234152b4ea
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-25 22:13:12 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Programming Fundamentals Mid Exam - 07 November 2020/03. Deck of Cards/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Deck_of_Cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cards = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                if(command[0] == "Add")
+                {
+                    if(cards.Contains(command[1])) { Console.WriteLine("Card is already bought"); }
+                    else 
+                    {
+                        cards.Add(command[1]);
+                        Console.WriteLine("Card successfully bought");
+                    }
+                }
+                else if(command[0] == "Insert")
+                {
+                    int index = int.Parse(command[1]);
+                    if(index < 0 || index >= cards.Count) { Console.WriteLine("Index out of range"); }
+                    else
+                    {
+                        if(cards.Contains(command[2])) { Console.WriteLine("Card is already bought"); }
+                        else 
+                        {
+                            cards.Insert(index, command[2]);
+                            Console.WriteLine("Card successfully bought");
+                        }
+                    }
+                    
+                }
+                else if(command[0] == "Remove")
+                {
+                    if (cards.Contains(command[1]))
+                    {
+                        cards.Remove(command[1]);
+                        Console.WriteLine("Card successfully sold");
+                    }
+                    else { Console.WriteLine("Card not found"); }
+                }
+                else if(command[0] == "Remove At")
+                {
+                    int index = int.Parse(command[1]);
+                    if (index < 0 || index >= cards.Count) { Console.WriteLine("Index out of range"); }
+                    else
+                    {
+                        cards.RemoveAt(index);
+                        Console.WriteLine("Card successfully sold");
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(", ", cards));
+        }
+    }
+}
