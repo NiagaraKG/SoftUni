ced4c607f6f9b0f1d5f6d5010df1d6b6f7297721
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-26 23:15:34 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Associative Arrays - Lab/04. Largest 3 Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(n=>n).ToArray();
+            if(numbers.Length < 3) { Console.WriteLine(String.Join(" ", numbers)); }
+            else { Console.WriteLine(numbers[0] + " " + numbers[1] + " " + numbers[2]); }
+        }
+    }
+}
