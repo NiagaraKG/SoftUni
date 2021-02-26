b4c8e42c23338a3b44fa077013f59f911d903025
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-26 23:13:22 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Associative Arrays - Lab/01. Count Real Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            SortedDictionary<double, int> numbers = new SortedDictionary<double, int>();
+            foreach (var item in input)
+            {
+                if (numbers.ContainsKey(item)) { numbers[item]++; }
+                else { numbers.Add(item, 1); }
+            }
+            foreach (var item in numbers)
+            { Console.WriteLine(item.Key + " -> " + item.Value); }
+        }
+    }
+}
