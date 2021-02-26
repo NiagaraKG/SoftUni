069f67764de9339b03f0a18461eaeb04b0b1c920
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-26 23:14:01 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Associative Arrays - Lab/02. Odd Occurrences/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().Select(x => x.ToLower()).ToArray();
+            Dictionary<string, int> words = new Dictionary<string, int>();
+            foreach (var item in input)
+            {
+                if (words.ContainsKey(item)) { words[item]++; }
+                else { words.Add(item, 1); }
+            }
+            foreach (var item in words)
+            {
+                if (item.Value % 2 == 1) { Console.Write(item.Key + " "); }
+            }
+        }
+    }
+}
