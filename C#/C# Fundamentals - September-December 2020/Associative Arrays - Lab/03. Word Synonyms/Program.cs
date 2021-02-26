8f4d9679796d0e35ae05ea66c695cb8d189bc70e
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-26 23:14:54 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Associative Arrays - Lab/03. Word Synonyms/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> synonyms = new Dictionary<string, List<string>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+                if (!synonyms.ContainsKey(word)) { synonyms.Add(word, new List<string>()); }
+                synonyms[word].Add(synonym);
+            }
+            foreach (var item in synonyms)
+            {
+                Console.Write(item.Key + " - ");
+                Console.WriteLine(string.Join(", ", item.Value));
+            }
+        }
+    }
+}
