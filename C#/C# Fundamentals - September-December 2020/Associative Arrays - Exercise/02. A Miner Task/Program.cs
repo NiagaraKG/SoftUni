8f0c4df98948c0fd6f9bd4043162a324ceb0bb5b
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-02-27 22:00:27 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> resources = new Dictionary<string, double>();
+            string command = Console.ReadLine();
+            while(command != "stop")
+            {
+                double quantity = double.Parse(Console.ReadLine());
+                if (!resources.ContainsKey(command))
+                { resources.Add(command, quantity); }
+                else { resources[command] += quantity; }
+                command = Console.ReadLine();
+            }
+            foreach (var item in resources)
+            {
+                Console.WriteLine(item.Key + " -> " + item.Value);
+            }
+        }
+    }
+}
