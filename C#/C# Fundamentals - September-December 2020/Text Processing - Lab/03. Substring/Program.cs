284c42fff2e46cf71829e7ec626466b912109364
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-01 22:07:23 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Text Processing - Lab/03. Substring/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _03._Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string toRemove = Console.ReadLine();
+            string removeFrom = Console.ReadLine();
+            while (removeFrom.Contains(toRemove))
+            { removeFrom = removeFrom.Replace(toRemove, ""); }
+            Console.WriteLine(removeFrom);
+        }
+    }
+}
