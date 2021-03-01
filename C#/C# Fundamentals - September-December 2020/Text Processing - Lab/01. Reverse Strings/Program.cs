2ace97b6d73a6fb665bc80536e75dee5f315c817
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-01 22:06:08 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Text Processing - Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            while (word != "end")
+            {
+                Console.WriteLine($"{word} = {new string(word.Reverse().ToArray())}");
+                word = Console.ReadLine();
+            }
+        }
+    }
+}
