8c0581e51d2d11c570993b133b8371619a277096
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-01 22:06:51 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace _02._Repeat_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            StringBuilder s = new StringBuilder();
+            foreach (var item in input)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    s.Append(item);
+                }
+            }
+            Console.WriteLine(s.ToString());
+        }
+    }
+}
