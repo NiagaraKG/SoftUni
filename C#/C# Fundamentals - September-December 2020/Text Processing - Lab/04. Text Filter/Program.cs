bd8fc1e930a6f88197e07d0cd608fa13ffafcffc
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-02 21:38:24 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Text Processing - Lab/04. Text Filter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04._Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banned = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+            foreach (var word in banned)
+            {
+                string replacement = "";
+                foreach (var letter in word) { replacement += "*"; }
+                text = text.Replace(word, replacement);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
