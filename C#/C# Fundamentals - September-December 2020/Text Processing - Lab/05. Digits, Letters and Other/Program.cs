e5123512261f54d53486f02eb26c78e4d51d8aa3
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-02 21:38:50 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Text Processing - Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Digits__Letters_and_Other
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+            List<char> digits = new List<char>();
+            List<char> letters = new List<char>();
+            List<char> others = new List<char>();
+            foreach (var c in input)
+            {
+                if(char.IsDigit(c)) { digits.Add(c); }
+                else if(char.IsLetter(c)) { letters.Add(c); }
+                else { others.Add(c); }
+            }
+            Console.WriteLine(string.Join("", digits));
+            Console.WriteLine(string.Join("", letters));
+            Console.WriteLine(string.Join("", others));
+        }
+    }
+}
