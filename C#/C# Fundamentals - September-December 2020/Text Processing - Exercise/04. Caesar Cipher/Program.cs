d894c316136339176bfb4325ec4692bbe012c9b2
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-02 21:41:05 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Text Processing - Exercise/04. Caesar Cipher/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04._Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] text = Console.ReadLine().ToCharArray();
+            for (int i = 0; i < text.Length; i++)
+            {
+                text[i] = (char)((int)text[i] + 3);
+            }
+            Console.WriteLine(string.Join("", text));
+        }
+    }
+}
