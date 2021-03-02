de8b112a974df3b7532c73fa5a72b06387c9740d
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-02 21:40:45 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Text Processing - Exercise/03. Extract File/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03._Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] command = Console.ReadLine().Split("\\");
+            string[] result = command[command.Length - 1].Split(".");
+            Console.WriteLine($"File name: {result[0]}");
+            Console.WriteLine($"File extension: {result[1]}");
+        }
+    }
+}
