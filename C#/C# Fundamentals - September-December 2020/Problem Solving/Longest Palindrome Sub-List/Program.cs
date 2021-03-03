a3782365cc5d4c5535d5b80bd1bddcc0444e844b
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-03 20:52:51 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Problem Solving/Longest Palindrome Sub-List/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Longest_Palindrome_Sub_List
+{
+    class Program
+    {
+        static int PalindromeLenght(string letters, int left, int right)
+        {
+            while(left>=0 && right < letters.Length && letters[left] == letters[right])
+            { left--; right++; }
+            return right - left - 1;
+        }
+        static void Main(string[] args)
+        {
+            string letters = Console.ReadLine();
+            int maxLen = 0;
+            for (int c = 0; c < letters.Length; c++)
+            {
+                maxLen = Math.Max(maxLen, PalindromeLenght(letters, c, c));
+            }
+            for (int c = 0; c < letters.Length-1; c++)
+            {
+                maxLen = Math.Max(maxLen, PalindromeLenght(letters, c, c+1));
+            }
+            Console.WriteLine(maxLen);
+        }
+    }
+}
