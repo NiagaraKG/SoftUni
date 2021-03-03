10b5a3bece6a5df3cccb66a2356e3b6e95b32f58
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-03 20:50:06 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Text Processing - Exercise/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Replace_Repeating_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> letters = Console.ReadLine().ToCharArray().ToList();
+            for (int i = 1; i < letters.Count; i++)
+            {
+                if (letters[i] == letters[i - 1]) { letters.RemoveAt(i); i--; }
+            }
+            Console.WriteLine(string.Join("", letters));
+        }
+    }
+}
