8aa8fd5bf7d92127a8d0db7be72e40dfbc870a4e
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-04 22:07:07 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Regular Expressions - Lab/03. Match Dates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(?<day>\d{2})([./-])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+            Regex r = new Regex(pattern);
+            string input = Console.ReadLine();
+            MatchCollection matches = r.Matches(input);
+            foreach (Match m in matches)
+            {
+                Console.WriteLine($"Day: {m.Groups["day"].Value}, Month: {m.Groups["month"].Value}, Year: {m.Groups["year"].Value}");
+            }
+        }
+    }
+}
