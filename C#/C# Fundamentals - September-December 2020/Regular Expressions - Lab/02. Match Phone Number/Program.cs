20816f90d065e27870acd5ed545dd10e8fdf9d0f
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-04 22:06:02 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(^| )\+359([ -])2\2\d{3}\2\d{4}\b";
+            Regex r = new Regex(pattern);
+            string[] input = Console.ReadLine().Split(", ");
+            MatchCollection m = r.Matches(string.Join(" ", input));
+            Console.WriteLine(string.Join(",", m));
+        }
+    }
+}
