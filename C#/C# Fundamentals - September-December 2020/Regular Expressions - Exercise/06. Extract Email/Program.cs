dbdaf65f8ec970a52049233e183e15a31124f4af
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-05 22:14:25 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Regular Expressions - Exercise/06. Extract Email/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06._Extract_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(^| )([A-Za-z0-9]+[._-]*[A-Za-z0-9]+)@((([A-Za-z]+\-*[A-Za-z]+)\.)+([A-Za-z]+\-*[A-Za-z]+))";
+            Regex r = new Regex(pattern);
+            string[] input = Console.ReadLine().Split();
+            foreach (string part in input)
+            {
+                Match m = r.Match(part);
+                if (m.Success)
+                { Console.WriteLine(m.Value); }
+            }
+        }
+    }
+}
