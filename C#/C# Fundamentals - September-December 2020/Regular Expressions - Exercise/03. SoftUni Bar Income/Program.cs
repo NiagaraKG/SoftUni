da5ff9b2265307d6110cb24ecff0f7e3938c2d42
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-05 22:13:05 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Regular Expressions - Exercise/03. SoftUni Bar Income/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._SoftUni_Bar_Income
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"%([A-Z][a-z]+)%[^\|\$%\.]*?<(\w+)>[^\|\$%\.]*?\|(\d+)\|[^\|\$%\.]*?(\d+\.?\d+)\$";
+            Regex r = new Regex(pattern);
+            string input = Console.ReadLine();
+            double total = 0;
+            while (input != "end of shift")
+            {
+                Match match = r.Match(input);
+                if (match.Success)
+                {
+                    string customer = match.Groups[1].Value;
+                    string product = match.Groups[2].Value;
+                    double sum = double.Parse(match.Groups[3].Value) * double.Parse(match.Groups[4].Value);
+                    total += sum;
+                    Console.WriteLine($"{customer}: {product} - {sum:f2}");
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total income: {total:f2}");
+        }
+    }
+}
