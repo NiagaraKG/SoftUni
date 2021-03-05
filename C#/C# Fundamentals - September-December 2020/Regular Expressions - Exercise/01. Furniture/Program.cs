68855e228e3666f3c53916c31a56fd5190370aa9
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-05 22:11:53 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Regular Expressions - Exercise/01. Furniture/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _01._Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @">>([A-Za-z]+)<<(\d+\.?\d+)!(\d+)";
+            Regex r = new Regex(pattern);
+            string input = Console.ReadLine();
+            double total = 0;
+            Console.WriteLine("Bought furniture:");
+            while (input != "Purchase")
+            {                
+                var match = r.Match(input);
+                if (match.Success)
+                {
+                    string item = match.Groups[1].Value;
+                    total += double.Parse(match.Groups[2].Value) * double.Parse(match.Groups[3].Value);
+                    Console.WriteLine(item);
+                }                
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total money spend: {total:f2}");
+        }
+    }
+}
