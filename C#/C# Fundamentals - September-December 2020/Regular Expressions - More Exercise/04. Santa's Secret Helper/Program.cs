f6425d0cd92462d6001ae8dcefd07840107fe5dc
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-06 22:07:54 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Regular Expressions - More Exercise/04. Santa's Secret Helper/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _4._Santa_s_Secret_Helper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            while (input != "end")
+            {
+                char[] encripted = input.ToCharArray();
+                for (int i = 0; i < encripted.Length; i++) { encripted[i] = (char)(encripted[i] - key); }
+                string decripted = string.Join("", encripted);
+                string pattern = @"@([A-Za-z]+)[^@!:>\-]*!([G])!";
+                Regex r = new Regex(pattern);
+                Match m = r.Match(decripted);
+                if (m.Success) { Console.WriteLine(m.Groups[1].Value); }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
