2339a5ccda4122aba796973f38d23d9409841328
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-06 22:07:07 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Regular Expressions - More Exercise/02. Rage Quit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _2._Rage_Quit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToUpper();
+            string pattern = @"(\D+)(\d+)";
+            Regex r = new Regex(pattern);
+            MatchCollection matches = r.Matches(input);
+            StringBuilder result = new StringBuilder();
+            foreach (Match item in matches)
+            {
+                string text = item.Groups[1].Value;
+                int times = int.Parse(item.Groups[2].Value);
+                for (int i = 0; i < times; i++) { result.Append(text); }
+            }
+            List<char> cleared = result.ToString().ToCharArray().Where(x => !char.IsDigit(x)).Distinct().ToList();
+            Console.WriteLine("Unique symbols used: " + cleared.Count());
+            Console.WriteLine(result);
+        }
+    }
+}
