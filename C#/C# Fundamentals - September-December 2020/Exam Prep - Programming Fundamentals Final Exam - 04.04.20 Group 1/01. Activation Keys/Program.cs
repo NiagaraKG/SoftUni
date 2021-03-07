ab211750213e7636b0d11d4fb82761466691ff12
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-07 22:15:16 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exam Prep - Programming Fundamentals Final Exam - 04.04.20 Group 1/01. Activation Keys/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01._Activation_Keys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = Console.ReadLine();
+            string[] command = Console.ReadLine().Split(">>>");
+            while (command[0] != "Generate")
+            {
+                if (command[0] == "Contains")
+                {
+                    if (key.Contains(command[1])) { Console.WriteLine($"{key} contains {command[1]}"); }
+                    else { Console.WriteLine("Substring not found!"); }
+                }
+                if (command[0] == "Flip")
+                {
+                    char[] chars = key.ToCharArray();
+                    int start = int.Parse(command[2]), end = int.Parse(command[3]);
+                    if (command[1] == "Upper")
+                    { for (int i = start; i < end; i++) { chars[i] = char.ToUpper(chars[i]); } }
+                    else { for (int i = start; i < end; i++) { chars[i] = char.ToLower(chars[i]); } }
+                    key = string.Join("", chars); Console.WriteLine(key);
+                }
+                if (command[0] == "Slice")
+                {
+                    int start = int.Parse(command[1]), end = int.Parse(command[2]);
+                    key = key.Remove(start, end - start);
+                    Console.WriteLine(key);
+                }
+                command = Console.ReadLine().Split(">>>");
+            }
+            Console.WriteLine($"Your activation key is: {key}");
+        }
+    }
+}
