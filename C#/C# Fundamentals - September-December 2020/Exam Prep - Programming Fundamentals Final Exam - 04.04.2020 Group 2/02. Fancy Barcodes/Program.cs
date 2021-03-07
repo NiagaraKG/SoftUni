c4516528ce6717bd6643d64740d4fa228f530c5f
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-07 22:18:27 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exam Prep - Programming Fundamentals Final Exam - 04.04.2020 Group 2/02. Fancy Barcodes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02._Fancy_Barcodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(@[#]+)([A-Z][A-Za-z0-9]{4,}[A-Z])(@[#]+)";
+            Regex r = new Regex(pattern);
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string barcode = Console.ReadLine();
+                Match m = r.Match(barcode);
+                if (m.Value == barcode)
+                {
+                    StringBuilder productGroup = new StringBuilder();
+                    foreach (char c in barcode) { if (char.IsDigit(c)) { productGroup.Append(c); } }
+                    if(productGroup.Length == 0) { productGroup.Append("00"); }
+                    Console.WriteLine("Product group: " + productGroup.ToString());
+                }
+                else { Console.WriteLine("Ïnvalid barcode"); }
+            }
+        }
+    }
+}
