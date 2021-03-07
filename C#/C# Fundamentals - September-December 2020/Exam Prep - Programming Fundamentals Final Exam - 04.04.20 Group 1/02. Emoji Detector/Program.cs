0f11704a312db8b36a2e54cf809c7be1d79797b3
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-07 22:16:27 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exam Prep - Programming Fundamentals Final Exam - 04.04.20 Group 1/02. Emoji Detector/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Emoji_Detector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            long coolthreshold = 1;
+            foreach (char c in input) { if (char.IsDigit(c)) { coolthreshold *= (c - '0'); } }
+            Console.WriteLine("Cool threshold: " + coolthreshold);
+            string pattern = @"(::|\*\*)([A-Z][a-z]{2,})\1";
+            Regex r = new Regex(pattern);
+            MatchCollection found = r.Matches(input);
+            Console.WriteLine(found.Count + " emojis found in the text. The cool ones are:");
+            foreach (Match m in found)
+            {
+                int coolness = 0;
+                foreach (char c in m.Groups[2].Value) { coolness += c; }
+                if(coolness > coolthreshold) { Console.WriteLine(m.Value); }
+            }
+        }
+    }
+}
