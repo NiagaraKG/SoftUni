d32318592b53aac64b0021ebd5a5f39b38ac05f0
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-08 22:51:30 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exam Prep - Programming Fundamentals Final Exam Retake - 10.04.2020/01. Secret Chat/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Secret_Chat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            string[] command = Console.ReadLine().Split(":|:");
+            while (command[0] != "Reveal")
+            {
+                if (command[0] == "InsertSpace") 
+                { message = message.Insert(int.Parse(command[1]), " "); Console.WriteLine(message); }
+                else if (command[0] == "Reverse")
+                {
+                    if (message.Contains(command[1]))
+                    {
+                        message = message.Remove(message.IndexOf(command[1]), command[1].Length);
+                        char[] reverse = command[1].ToCharArray();
+                        Array.Reverse(reverse);
+                        message += string.Join("", reverse);
+                        Console.WriteLine(message);
+                    }
+                    else { Console.WriteLine("error"); }
+                }
+                else if (command[0] == "ChangeAll") 
+                { message = message.Replace(command[1], command[2]); Console.WriteLine(message); }
+                command = Console.ReadLine().Split(":|:");
+            }
+            Console.WriteLine("You have a new text message: " + message);
+        }
+    }
+}
