30f75d5a3a0ca81befcf537e8d7d4c088a7fcdd1
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-08 22:56:22 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exam Prep - Programming Fundamentals Final Exam - 09.08.2020/02. Destination Mapper/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._Destination_Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(=|\/)([A-Z][A-Za-z]{2,})\1";
+            string input = Console.ReadLine();
+            Regex r = new Regex(pattern);
+            MatchCollection matches = r.Matches(input);
+            List<string> destinations = new List<string>();
+            foreach (Match m in matches) { destinations.Add(m.Groups[2].Value); }
+            Console.WriteLine("Destinations: " + string.Join(", ", destinations));
+            int points = 0;
+            foreach (var d in destinations) { points += d.Length; }
+            Console.WriteLine("Travel Points: " + points);
+        }
+    }
+}
