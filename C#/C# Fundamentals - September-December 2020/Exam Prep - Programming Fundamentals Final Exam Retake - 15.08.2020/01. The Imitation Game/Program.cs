f570c89d3ee93c53d47c5ba1263cdb33c50f2ecc
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-09 22:10:22 +0200

    Add files via upload

---
+++ C#/C# Fundamentals - September-December 2020/Exam Prep - Programming Fundamentals Final Exam Retake - 15.08.2020/01. The Imitation Game/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01._The_Imitation_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            string[] command = Console.ReadLine().Split("|");
+            while (command[0] != "Decode")
+            {
+                if(command[0] == "Move")
+                {
+                    int n = int.Parse(command[1]); string m = message.Substring(0, n);
+                    message = message.Remove(0, n); message += m;
+                }
+                else if(command[0] == "Insert") 
+                { message = message.Insert(int.Parse(command[1]), command[2]); }
+                else if(command[0] == "ChangeAll")
+                { message = message.Replace(command[1], command[2]); }
+                command = Console.ReadLine().Split("|");
+            }
+            Console.WriteLine("The decrypted message is: " + message);
+        }
+    }
+}
