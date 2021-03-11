d58d390926d93a242b6e25814507b512ec4f4eeb
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-11 22:34:27 +0200

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Inheritance/Inheritance - Lab/04.RandomList/RandomList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random r = new Random();
+            int index = r.Next(this.Count);
+            string toRemove = this[index];
+            this.RemoveAt(index);
+            return toRemove;
+        }
+    }
+}
