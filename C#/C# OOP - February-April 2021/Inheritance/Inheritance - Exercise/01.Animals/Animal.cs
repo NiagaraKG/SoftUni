5697e52f7c4a16816d3662b7a5b9ad8b3fe30407
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-12 20:27:20 +0200

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Inheritance/Inheritance - Exercise/01.Animals/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+        public virtual string ProduceSound()
+        {
+            return string.Empty;
+        }
+    }
+}
