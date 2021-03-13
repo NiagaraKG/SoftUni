cbde2393bfc6febd1622d7c82cfb51e4b6d88b15
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-13 22:09:16 +0200

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Inheritance/Inheritance - Exercise/02.NeedForSpeed/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+        }
+        private double DefaultFuelConsumption = 1.25;
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+        public virtual double FuelConsumption => this.DefaultFuelConsumption;
+        public virtual void Drive(double kilometers)
+        {
+            Fuel -= FuelConsumption * kilometers;
+        }
+    }
+}
