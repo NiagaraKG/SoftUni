04fbf0e30bae676bd09bb4d7f75a5e084e618ad7
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-13 22:05:53 +0200

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Inheritance/Inheritance - Exercise/02.NeedForSpeed/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Car : Vehicle
+    {
+        public override double FuelConsumption => 3;
+        public Car(int horsePower, double fuel) : base(horsePower, fuel) { }
+    }
+}
