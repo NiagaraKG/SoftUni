2300ba8ce6420c3ed8392949214cb5c856454165
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-13 22:07:34 +0200

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Inheritance/Inheritance - Exercise/02.NeedForSpeed/RaceMotorcycle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        public override double FuelConsumption => 8;
+        public RaceMotorcycle(int horsePower, double fuel) : base(horsePower, fuel) { }
+
+    }
+}
