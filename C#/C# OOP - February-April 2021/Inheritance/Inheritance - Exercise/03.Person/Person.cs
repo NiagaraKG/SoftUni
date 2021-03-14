58892872c78b8b1ceb3c6c4dfe68943af5c972f4
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-14 22:10:55 +0200

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Inheritance/Inheritance - Exercise/03.Person/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        public string Name { get { return this.name; } set { this.name = value; } }
+        public int Age { get { return this.age; } set { this.age = value; } }
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            if (age >= 0) { this.Age = age; }
+        }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}", this.Name, this.Age));
+            return stringBuilder.ToString();
+        }
+    }
+}
