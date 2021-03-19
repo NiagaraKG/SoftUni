b40652222647032a8ebc3ee3b7b7107947774628
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-19 21:18:34 +0200

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Inheritance/Inheritance - Exercise/05.Restaurant/Coffee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        public const double CoffeeMilliliters = 50;
+        public const decimal CoffeePrice = (decimal)3.50;
+        public double Caffeine { get; set; }       
+        public Coffee(string name, double caffeine) : base(name, CoffeePrice, CoffeeMilliliters) { this.Caffeine = caffeine; }
+
+    }
+}
