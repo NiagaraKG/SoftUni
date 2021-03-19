ff883852eee9a94fda42b225a791987a3937b3ae
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-19 21:18:56 +0200

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Inheritance/Inheritance - Exercise/05.Restaurant/Cake.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        public const double Grams = 250;
+        public const double Calories = 1000;
+        public const decimal CakePrice = 5;
+        public Cake(string name) : base(name, CakePrice, Grams, Calories) { }
+    }
+}
