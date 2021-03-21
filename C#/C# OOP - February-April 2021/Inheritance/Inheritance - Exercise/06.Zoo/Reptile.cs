8f08ed405bc77f6d4f910e1d22e5502b20729ca3
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-21 22:13:39 +0200

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Inheritance/Inheritance - Exercise/06.Zoo/Reptile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Reptile : Animal
+    {
+        public string Name { get; set; }
+        public Reptile(string name) : base(name) { }
+    }
+}
