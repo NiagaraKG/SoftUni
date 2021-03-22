0f4a38d6219f5578d0ce942fcda0e8c40fbd21f7
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-22 22:19:33 +0200

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Inheritance/Inheritance - Exercise/06.Zoo/Gorilla.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Gorilla : Mammal
+    {
+        public string Name { get; set; }
+        public Gorilla(string name) : base(name) { }
+    }
+}
