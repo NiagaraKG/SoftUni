8af147f7d635f5d3c4a24f19fe036719b03a8b74
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-27 22:06:17 +0200

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Encapsulation/Encapsulation - Exercise/01.ClassBoxData/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.ClassBoxData
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            try
+            {
+                Box b = new Box(length, width, height);
+                Console.WriteLine($"Surface Area - {b.SurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {b.LateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {b.Volume():f2}");
+            }
+            catch (ArgumentException ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
