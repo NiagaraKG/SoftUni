eec4fd619a2157afc0b890be699bd898bd0a90d3
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-03-30 20:36:17 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Encapsulation/Encapsulation - Exercise/04.PizzaCalories/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _04.PizzaCalories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            try
+            {
+                Pizza p = new Pizza(input[1]);
+                input = Console.ReadLine().Split();
+                p.Dough = new Dough(input[1], input[2], int.Parse(input[3]));
+                input = Console.ReadLine().Split();
+                while (input[0] != "END")
+                {
+                    p.Add(new Topping(input[1], int.Parse(input[2])));
+                    input = Console.ReadLine().Split();
+                }
+                Console.WriteLine(p);
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+        }
+    }
+}
