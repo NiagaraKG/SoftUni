7c8171cfdc57b055ad4bb87f26e8ff2a734be2fe
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-02 22:03:44 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Lab/02.Cars/Seat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public string Model { get; private set; }
+        public string Color { get; private set; }
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+        public string Start() { return "Engine start"; }
+        public string Stop() { return "Breaaak!"; }
+        public override string ToString() { return $"{this.Color} Seat {this.Model}"; }
+    }
+}
