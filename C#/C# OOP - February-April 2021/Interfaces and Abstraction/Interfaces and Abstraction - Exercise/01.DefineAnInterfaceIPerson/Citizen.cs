4040b610ff9faf2117cb37de7eb888fc8033c5e2
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-03 22:22:45 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/01.DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Citizen(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+}
