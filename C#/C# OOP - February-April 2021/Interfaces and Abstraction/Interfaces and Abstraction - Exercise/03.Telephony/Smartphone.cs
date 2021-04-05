ff5c525e0b2cc09ee3e9b7fb61a4cdf7c18183c1
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-05 22:20:00 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/03.Telephony/Smartphone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Telephony
+{
+    public class Smartphone : ICallable, IBrowsable
+    {
+        public void Call(string number)
+        {
+            foreach (var digit in number)
+            {
+                if (!char.IsDigit(digit)) 
+                { throw new InvalidOperationException("Invalid number!"); }
+            }
+            Console.WriteLine($"Calling... {number}");
+        }
+        public void Browse(string site)
+        {
+            foreach (var c in site)
+            {
+                if (char.IsDigit(c)) 
+                { throw new InvalidOperationException("Invalid URL!"); }
+            }
+            Console.WriteLine($"Browsing: {site}!");
+        }
+    }
+}
