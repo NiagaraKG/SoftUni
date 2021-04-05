9c226650b0848e0ce13c4fa5319cf0487824174a
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-05 22:20:55 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/03.Telephony/StationaryPhone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Telephony
+{
+    class StationaryPhone : ICallable
+    {
+        public void Call(string number)
+        {
+            foreach (var digit in number)
+            {
+                if (!char.IsDigit(digit)) { throw new InvalidOperationException("Invalid number!"); }
+            }
+            Console.WriteLine($"Dialing... {number}");
+        }
+    }
+}
