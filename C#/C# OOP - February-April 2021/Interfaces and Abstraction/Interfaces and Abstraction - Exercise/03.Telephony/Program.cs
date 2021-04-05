b2220160b9588ab66ce506e1a7b6040ed02267d5
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-05 22:19:29 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/03.Telephony/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _03.Telephony
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Smartphone smart = new Smartphone();
+            StationaryPhone stationary = new StationaryPhone();
+            string[] numbers = Console.ReadLine().Split();
+            string[] sites = Console.ReadLine().Split();
+            foreach (var num in numbers)
+            {
+                try
+                {
+                    if (num.Length == 10) { smart.Call(num); }
+                    else { stationary.Call(num); }
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+            }
+            foreach (var site in sites)
+            {
+                try { smart.Browse(site); }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+            }
+        }
+    }
+}
