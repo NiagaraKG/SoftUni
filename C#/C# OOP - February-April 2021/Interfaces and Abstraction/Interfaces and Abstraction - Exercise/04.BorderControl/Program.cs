eec7b7a32099ab9b830e8e2c25a099a3412f73d8
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-06 23:06:12 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/04.BorderControl/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.BorderControl
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            List<IID> travelers = new List<IID>();
+            while (input[0] != "End")
+            {
+                if (input.Length == 3) { travelers.Add(new Citizen(input[0], int.Parse(input[1]), input[2])); }
+                else { travelers.Add(new Robot(input[0], input[1])); }
+                input = Console.ReadLine().Split();
+            }
+            string fake = Console.ReadLine();
+            foreach (var t in travelers)
+            {
+                string end = t.ID.Substring(t.ID.Length - fake.Length);
+                if (end == fake) { Console.WriteLine(t.ID); }
+            }
+        }
+    }
+}
