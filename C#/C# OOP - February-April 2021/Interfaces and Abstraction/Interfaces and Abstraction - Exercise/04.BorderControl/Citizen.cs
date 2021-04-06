1ca14ef16861e737372ec4440a545583cf599bc6
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-06 23:05:19 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/04.BorderControl/Citizen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.BorderControl
+{
+    class Citizen : IID
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string ID { get; set; }
+        public Citizen(string name, int age, string ID)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.ID = ID;
+        }
+    }
+}
