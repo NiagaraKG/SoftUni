5f478b3c16b45dd4ba484f9365a5068bac2194d5
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-07 22:23:34 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/05.BirthdayCelebrations/IBirthable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.BirthdayCelebrations
+{
+    public interface IBirthable
+    {
+        public string Birthday { get; set; }
+    }
+}
