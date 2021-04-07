8089c054343c435c3a276e33e0f0732447ea766a
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-07 22:24:30 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/05.BirthdayCelebrations/Pet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.BirthdayCelebrations
+{
+    public class Pet : IBirthable
+    {
+        public string Name { get; set; }
+        public string Birthday { get; set; }
+        public Pet(string name, string birthday)
+        {
+            this.Name = name;
+            this.Birthday = birthday;
+        }
+    }
+}
