f11f3d50d3a45984a072dfb3f465df4127fdcab7
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-07 22:22:52 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/05.BirthdayCelebrations/Citizen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.BirthdayCelebrations
+{
+    public class Citizen : IID,  IBirthable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string ID { get; set; }
+        public string Birthday { get; set; }
+        public Citizen(string name, int age, string ID, string birthday)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.ID = ID;
+            this.Birthday = birthday;
+        }
+    }
+}
