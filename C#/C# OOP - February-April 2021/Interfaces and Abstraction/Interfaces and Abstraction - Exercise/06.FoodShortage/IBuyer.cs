a51b48c9f12e1375a064a5c2e6be814fbfdd404a
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-08 22:05:14 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/06.FoodShortage/IBuyer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.FoodShortage
+{
+    public interface IBuyer
+    {
+        public string Name { get; set; }
+        public int Food { get; set; }
+        public void BuyFood();
+    }
+}
