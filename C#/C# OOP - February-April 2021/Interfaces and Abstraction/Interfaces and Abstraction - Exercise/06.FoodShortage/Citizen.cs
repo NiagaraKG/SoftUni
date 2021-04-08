9e2cf6d6590358b1a8e8564239cbd23786c5a49a
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-08 22:04:49 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/06.FoodShortage/Citizen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.FoodShortage
+{
+    public class Citizen : IBuyer
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string ID { get; set; }
+        public string Birthday { get; set; }
+        public int Food { get; set; }
+        public Citizen(string name, int age, string ID, string birthday)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.ID = ID;
+            this.Birthday = birthday;
+            this.Food = 0;
+        }
+        public void BuyFood() { this.Food += 10; }
+    }
+}
