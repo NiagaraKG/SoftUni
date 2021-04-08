6daccc1076adc2c191edcf2682eb400a3070b7f7
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-08 22:05:41 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/06.FoodShortage/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.FoodShortage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<IBuyer> buyers = new List<IBuyer>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                if (input.Length == 4) { buyers.Add(new Citizen(input[0], int.Parse(input[1]), input[2], input[3])); }
+                else { buyers.Add(new Rebel(input[0], int.Parse(input[1]), input[2])); }
+            }
+            string name = Console.ReadLine();
+            while (name != "End")
+            {
+                if (buyers.Any(b => b.Name == name))
+                {
+                    buyers.FirstOrDefault(b => b.Name == name).BuyFood();
+                }
+                name = Console.ReadLine();
+            }
+            int food = 0;
+            foreach (var b in buyers) { food += b.Food; }
+            Console.WriteLine(food);
+        }
+    }
+}
