796c6cc811a18de3374bf8373ccd8bcced727250
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-10 22:10:05 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/07.MilitaryElite/Models/Private.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.MilitaryElite
+{
+    public class Private : Soldier, IPrivate
+    {
+        public decimal Salary { get; private set; }
+        public Private(string id, string firstName, string lastName, decimal salary)
+            : base(id, firstName, lastName) { this.Salary = salary; }
+        public override string ToString() { return $"Name: {this.FirstName} {this.LastName} Id: {this.ID} Salary: {this.Salary:f2}"; }
+    }
+}
