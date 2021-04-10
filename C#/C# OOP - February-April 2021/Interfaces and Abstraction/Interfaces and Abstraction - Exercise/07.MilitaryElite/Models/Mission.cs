bb0789b378f1fd2cc955e4c8864237c81678f056
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-10 22:08:50 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/07.MilitaryElite/Models/Mission.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.MilitaryElite
+{
+    public class Mission : IMission
+    {
+        public string CodeName { get; private set; }
+        public State State { get; private set; }
+        public Mission(string codeName, State state)
+        {
+            this.CodeName = codeName;
+            this.State = state;
+        }
+        public void CompleteMission() { this.State = State.Finished; }
+        public override string ToString() { return $"Code Name: {this.CodeName} State: {this.State}"; }
+    }
+}
