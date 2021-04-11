abc9ca8a73cd76d14ebf7c81f2430acdfa7866f1
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-11 22:05:01 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/07.MilitaryElite/Models/Soldier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.MilitaryElite
+{
+    public abstract class Soldier : ISoldier
+    {
+        public string ID { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private  set; }
+        public Soldier(string id, string firstName, string lastName)
+        {
+            this.ID = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
