81cf335a19b0ce431d2bb9fe0cbd1fe64a5ad565
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-11 22:06:11 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/07.MilitaryElite/Models/Spy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.MilitaryElite
+{
+    public class Spy : Soldier, ISpy
+    {
+        public int CodeNumber { get; private set; }
+        public Spy(string id, string firstName, string lastName, int codeNumber)
+                        : base(id, firstName, lastName) { this.CodeNumber = codeNumber; }
+        public override string ToString() 
+        { return $"Name: {this.FirstName} {this.LastName} Id: {this.ID}\nCode Number: {this.CodeNumber}"; }
+    }
+}
