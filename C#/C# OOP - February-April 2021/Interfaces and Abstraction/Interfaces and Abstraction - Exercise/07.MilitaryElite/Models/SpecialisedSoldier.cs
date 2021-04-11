bab62a81586cb9568cb30e2af56f0d11c4618ba5
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-11 22:05:45 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/07.MilitaryElite/Models/SpecialisedSoldier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.MilitaryElite
+{
+    public abstract class SpecialisedSoldier : Private, ISpecialisedSoldier
+    {
+        public Corps Corps { get; private set; }
+        protected SpecialisedSoldier(string id, string firstName, string lastName, decimal salary, Corps corps)
+            : base(id, firstName, lastName, salary) { this.Corps = corps; }
+    }
+}
