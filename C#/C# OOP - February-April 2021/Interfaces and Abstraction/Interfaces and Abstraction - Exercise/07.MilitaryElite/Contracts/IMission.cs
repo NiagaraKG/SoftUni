b6384a9fb84d66674b027df75942ba9c64f6b55f
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-12 22:27:06 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/07.MilitaryElite/Contracts/IMission.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.MilitaryElite
+{
+    public interface IMission
+    {
+        string CodeName { get; }
+        State State { get; }
+        void CompleteMission();
+    }
+}
