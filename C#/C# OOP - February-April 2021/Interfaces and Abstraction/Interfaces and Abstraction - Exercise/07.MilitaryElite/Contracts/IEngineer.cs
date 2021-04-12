2e74c0566d64c6365ff405cd537805224c708c65
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-12 22:25:53 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/07.MilitaryElite/Contracts/IEngineer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.MilitaryElite
+{
+    public interface IEngineer : ISpecialisedSoldier
+    {
+        IReadOnlyCollection<IRepair> Repairs { get; }
+        void AddRepair(IRepair repair);
+    }
+}
