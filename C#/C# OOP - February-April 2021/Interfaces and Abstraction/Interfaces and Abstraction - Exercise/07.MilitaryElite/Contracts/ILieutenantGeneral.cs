2d3d70fe2ad31206b02e3f298ef831971de9af41
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-12 22:26:23 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/07.MilitaryElite/Contracts/ILieutenantGeneral.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.MilitaryElite
+{
+    public interface ILieutenantGeneral : IPrivate
+    {
+        IReadOnlyCollection<IPrivate> Privates { get; }
+        void AddPrivate(IPrivate @private);
+    }
+}
