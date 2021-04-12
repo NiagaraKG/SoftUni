7b222e8b8002500361fc69a8544f4e411392b4f4
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-12 22:25:08 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/07.MilitaryElite/Contracts/ICommando.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.MilitaryElite
+{
+    public interface ICommando : ISpecialisedSoldier
+    {
+        IReadOnlyCollection<IMission> Missions { get; }
+        void AddMission(IMission mission);
+    }
+}
