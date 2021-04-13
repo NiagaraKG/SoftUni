95128a4891c9f314509926297b9650d865b133cb
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-13 22:19:58 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/07.MilitaryElite/Contracts/ISoldier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.MilitaryElite
+{
+    public interface ISoldier
+    {
+        string ID { get; }
+        string FirstName { get; }
+        string LastName { get; }
+    }
+}
