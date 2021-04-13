c487f9f29e4572197034eb07ada2d15aa074a162
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-13 22:20:32 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/07.MilitaryElite/Contracts/ISpecialisedSoldier.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.MilitaryElite
+{
+    public interface ISpecialisedSoldier : IPrivate
+    {
+        Corps Corps { get; }
+    }
+}
