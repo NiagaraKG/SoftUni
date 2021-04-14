0ad35ab68a5ca29cd7f43caf97367ba12c7734e4
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-14 22:05:43 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism - Exercise/01.Vehicles/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _01.Vehicles
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Car c = new Car(double.Parse(input[1]), double.Parse(input[2]));
+            input = Console.ReadLine().Split();
+            Truck t = new Truck(double.Parse(input[1]), double.Parse(input[2]));
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                input = Console.ReadLine().Split();
+                double value = double.Parse(input[2]);
+                if (input[0] == "Drive")
+                {
+                    switch (input[1])
+                    {
+                        case "Car": c.Drive(value); break;
+                        case "Truck": t.Drive(value); break;
+                    }
+                }
+                else if(input[0] == "Refuel")
+                {
+                    switch (input[1])
+                    {
+                        case "Car": c.Refuel(value); break;
+                        case "Truck": t.Refuel(value); break;
+                    }
+                }
+            }
+            Console.WriteLine($"Car: {c.fuelQuantity:f2}");
+            Console.WriteLine($"Truck: {t.fuelQuantity:f2}");
+        }
+    }
+}
