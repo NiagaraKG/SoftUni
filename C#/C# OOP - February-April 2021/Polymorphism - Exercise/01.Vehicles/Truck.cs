1c109a2b37019f404693960bc08092ca75e783a8
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-14 22:06:11 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism - Exercise/01.Vehicles/Truck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.Vehicles
+{
+    public class Truck : Vehicle
+    {
+        public Truck(double quantity, double consumption) : base(quantity, consumption + 1.6) { }
+        public override void Refuel(double liters) { this.fuelQuantity += liters*0.95; }
+        public override void Drive(double distance)
+        {
+            double fuelNeeded = distance * this.fuelConsumption;
+            if (fuelNeeded > fuelQuantity) { Console.WriteLine("Truck needs refueling"); }
+            else
+            {
+                Console.WriteLine($"Truck travelled {distance} km");
+                this.fuelQuantity -= fuelNeeded;
+            }
+        }
+    }
+}
