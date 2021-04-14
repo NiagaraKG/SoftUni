c28167470cfd980aacbb0a0ff4b25b40bb906a0f
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-14 22:06:53 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism - Exercise/01.Vehicles/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.Vehicles
+{
+    public class Vehicle
+    {
+        public double fuelQuantity, fuelConsumption;
+        public Vehicle(double quantity, double consumption)
+        {
+            this.fuelQuantity = quantity;
+            this.fuelConsumption = consumption;
+        }
+        public virtual void Drive(double distance) { }
+        public virtual void Refuel(double liters) { }
+    }
+}
