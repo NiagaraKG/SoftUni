6e2c8f92f7d11a46434d78e26f66a612609b116c
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-17 22:50:12 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism - Exercise/03.Raiding/BaseHero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Raiding
+{
+    public abstract class BaseHero
+    {
+        public string Name { get; set; }
+        public int Power { get; set; }
+        public BaseHero(string name, int power)
+        {
+            this.Name = name;
+            this.Power = power;
+        }
+        public virtual string CastAbility() { return ""; }
+    }
+}
