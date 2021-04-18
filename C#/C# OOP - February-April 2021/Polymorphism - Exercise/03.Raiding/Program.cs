36fedd1a1f16fd95b859f7602e3e4400cc7d492b
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-18 22:02:27 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism - Exercise/03.Raiding/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.Raiding
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<BaseHero> heroes = new List<BaseHero>();
+            while (heroes.Count < n)
+            {
+                string name = Console.ReadLine();
+                string type = Console.ReadLine();
+                switch (type)
+                {
+                    case "Druid": heroes.Add(new Druid(name)); break;
+                    case "Paladin": heroes.Add(new Paladin(name)); break;
+                    case "Rogue": heroes.Add(new Rogue(name)); break;
+                    case "Warrior": heroes.Add(new Warrior(name)); break;
+                    default: Console.WriteLine("Invalid hero!"); break;
+                }
+            }
+            int bossPower = int.Parse(Console.ReadLine()), groupPower = 0;
+            foreach (var h in heroes)
+            {
+                Console.WriteLine(h.CastAbility());
+                groupPower += h.Power;
+            }
+            if (groupPower >= bossPower) { Console.WriteLine("Victory!"); }
+            else { Console.WriteLine("Defeat..."); }
+        }
+    }
+}
