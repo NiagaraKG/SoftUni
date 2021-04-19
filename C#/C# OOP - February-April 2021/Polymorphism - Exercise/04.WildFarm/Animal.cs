9a5c15b634ef606b93405551cc27112ead585e76
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-19 22:04:52 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism - Exercise/04.WildFarm/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.WildFarm
+{
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public int FoodEaten { get; set; }
+        public Animal(string name, double weight)
+        {
+            this.Name = name;
+            this.Weight = weight;
+        }
+        public virtual void ProduceSound() { }
+    }
+}
