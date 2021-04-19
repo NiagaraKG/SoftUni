6d613558136b4a20d13df5522f4fd0fab1b06747
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-19 22:05:14 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism - Exercise/04.WildFarm/Bird.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.WildFarm
+{
+    public abstract class Bird : Animal
+    {
+        public double WingSize { get; set; }
+        protected Bird(string name, double weight, double wingSize) : base(name, weight)
+        { this.WingSize = wingSize; }
+    }
+}
