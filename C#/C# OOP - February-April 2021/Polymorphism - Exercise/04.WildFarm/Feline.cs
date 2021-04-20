008ec8cb539ad5d467eb583cbe37c2c149de50f0
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-20 22:31:51 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism - Exercise/04.WildFarm/Feline.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.WildFarm
+{
+    public abstract class Feline : Mammal
+    {
+        public string Breed { get; set; }
+        protected Feline(string name, double weight, string livingRegion, string breed) : base(name, weight, livingRegion)
+        { this.Breed = breed; }
+    }
+}
