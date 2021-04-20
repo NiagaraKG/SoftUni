5e127832fce0855f414ddbf5f2ba471e4c376660
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-20 22:31:15 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism - Exercise/04.WildFarm/Cat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.WildFarm
+{
+    public class Cat : Feline
+    {
+        public Cat(string name, double weight, string livingRegion, string breed) : base(name, weight, livingRegion, breed) { }
+        public override void ProduceSound()
+        { Console.WriteLine("Meow"); }
+        public override string ToString()
+        { return $"Cat [{this.Name}, {this.Breed}, {this.Weight}, {this.LivingRegion}, {this.FoodEaten}]"; }
+    }
+}
