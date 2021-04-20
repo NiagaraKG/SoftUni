8abb1ce729962995816a9fe907deb9c84a46d4fb
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-20 22:31:35 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism - Exercise/04.WildFarm/Dog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.WildFarm
+{
+    public class Dog : Mammal
+    {
+        public Dog(string name, double weight, string livingRegion) : base(name, weight, livingRegion) { }
+        public override void ProduceSound()
+        { Console.WriteLine("Woof!"); }
+        public override string ToString()
+        { return $"Dog [{this.Name}, {this.Weight}, {this.LivingRegion}, {this.FoodEaten}]"; }
+    }
+}
