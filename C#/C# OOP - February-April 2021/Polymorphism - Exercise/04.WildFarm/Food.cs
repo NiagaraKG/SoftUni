875acfdb56dd789f17e05981f2a94a7eeee1db0e
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-21 22:03:57 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism - Exercise/04.WildFarm/Food.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.WildFarm
+{
+    public abstract class Food
+    {
+        public int Quantity { get; set; }
+        public Food(int quantity)
+        { this.Quantity = quantity; }
+    }
+}
