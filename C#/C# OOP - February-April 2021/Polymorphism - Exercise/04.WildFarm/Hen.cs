0a377f4c0a751ada837a3cb2fc95c8d45251f31f
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-21 22:04:46 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism - Exercise/04.WildFarm/Hen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.WildFarm
+{
+    public class Hen : Bird
+    {
+        public Hen(string name, double weight, double wingSize) : base(name, weight, wingSize) { }
+        public override void ProduceSound()
+        { Console.WriteLine("Cluck"); }
+        public override string ToString()
+        { return $"Hen [{this.Name}, {this.WingSize}, {this.Weight}, {this.FoodEaten}]"; }
+    }
+}
