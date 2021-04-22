b885f38e02c05d81a8fce6cefc0082fa3e6b3518
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-22 22:19:35 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism - Exercise/04.WildFarm/Mouse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.WildFarm
+{
+    public class Mouse : Mammal
+    {
+        public Mouse(string name, double weight, string livingRegion) : base(name, weight, livingRegion) { }
+        public override void ProduceSound()
+        { Console.WriteLine("Squeak"); }
+        public override string ToString()
+        { return $"Mouse [{this.Name}, {this.Weight}, {this.LivingRegion}, {this.FoodEaten}]"; }
+    }
+}
