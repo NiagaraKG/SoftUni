0f20380117960336aed19f30c3c7f471d7b1c15a
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-22 22:18:55 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism - Exercise/04.WildFarm/Mammal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.WildFarm
+{
+    public abstract class Mammal : Animal
+    {
+        public string LivingRegion { get; set; }
+        protected Mammal(string name, double weight, string livingRegion) : base(name, weight)
+        { this.LivingRegion = livingRegion; }
+    }
+}
