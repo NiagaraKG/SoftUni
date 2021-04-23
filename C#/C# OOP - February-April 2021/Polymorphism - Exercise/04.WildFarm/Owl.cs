155b26d432aedb252c2d53240594285b96ec4fbd
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-23 21:31:57 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism - Exercise/04.WildFarm/Owl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.WildFarm
+{
+    public class Owl : Bird
+    {
+        public Owl(string name, double weight, double wingSize) : base(name, weight, wingSize) { }
+        public override void ProduceSound()
+        { Console.WriteLine("Hoot Hoot"); }
+        public override string ToString()
+        { return $"Owl [{this.Name}, {this.WingSize}, {this.Weight}, {this.FoodEaten}]"; }
+    }
+}
