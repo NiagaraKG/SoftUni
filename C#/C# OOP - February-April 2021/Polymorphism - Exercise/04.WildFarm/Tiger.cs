75bbaed7eff0e70e00a614a7a83ab6cf406de65f
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-24 22:04:24 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism - Exercise/04.WildFarm/Tiger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.WildFarm
+{
+    public class Tiger : Feline
+    {
+        public Tiger(string name, double weight, string livingRegion, string breed) : base(name, weight, livingRegion, breed) { }
+        public override void ProduceSound()
+        { Console.WriteLine("ROAR!!!"); }
+        public override string ToString()
+        { return $"Tiger [{this.Name}, {this.Breed}, {this.Weight}, {this.LivingRegion}, {this.FoodEaten}]"; }
+    }
+}
