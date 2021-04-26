17f0849e84c175309327c5d7f265f5239f851b60
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-26 22:23:51 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism/Polymorphism - Lab/02.Animals/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favouriteFood;
+        public Animal(string animalName, string food)
+        {
+            this.name = animalName;
+            this.favouriteFood = food;
+        }
+        public virtual string ExplainSelf() { return $"I am {this.name} and my favourite food is {this.favouriteFood}"; }
+    }
+}
