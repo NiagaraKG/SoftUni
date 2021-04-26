f26a201354c956d3c8d983a4363d2a9a5f058485
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-26 22:24:17 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism/Polymorphism - Lab/02.Animals/Cat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Cat : Animal
+    {
+        public Cat(string catName, string food) : base(catName, food) { }
+        public override string ExplainSelf()
+        {
+            return base.ExplainSelf() + Environment.NewLine + "MEEOW";
+        }
+    }
+}
