139ac528d8a4eba9a1e6cef9e6db9741b3121518
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-27 22:17:58 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism/Polymorphism - Lab/02.Animals/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Dog : Animal
+    {
+        public Dog(string dogName, string food) : base(dogName, food) { }
+        public override string ExplainSelf()
+        {
+            return base.ExplainSelf() + Environment.NewLine + "DJAAF";
+        }
+
+    }
+}
