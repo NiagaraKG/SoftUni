67c51b22d60f543e4bf5462efeac1186f57c02f7
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-27 22:18:20 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism/Polymorphism - Lab/02.Animals/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Animal cat = new Cat("Maria", "Whiskas");
+            Animal dog = new Dog("Rex", "Meat");
+            Console.WriteLine(cat.ExplainSelf());
+            Console.WriteLine(dog.ExplainSelf());
+        }
+    }
+}
