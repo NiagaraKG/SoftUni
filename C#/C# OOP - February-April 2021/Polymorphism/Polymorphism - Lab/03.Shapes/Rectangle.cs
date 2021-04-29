b44bf0e09d3d65fc8ceba3491e982d156a46fdc6
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-04-29 22:28:02 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Polymorphism/Polymorphism - Lab/03.Shapes/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height, width;
+        public Rectangle(double h, double w) { this.height = h; this.width = w; }
+        public override double CalculateArea() { return this.height * this.width; }
+        public override double CalculatePerimeter() { return 2 * (this.height + this.width); }
+        public override string Draw() { return base.Draw() + this.GetType().Name; }
+
+    }
+}
