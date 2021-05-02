4d5695b97bb205ac23b4f9eab5b8ae5aa4360343
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-05-02 22:07:53 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Reflection and Attributes/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,24 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] commandArgs = args.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string commandName = commandArgs[0] + "Command";
+            commandArgs = commandArgs.Skip(1).ToArray();
+            Assembly assembly = Assembly.GetCallingAssembly();
+            Type commandType = assembly.GetTypes().FirstOrDefault(t => t.Name.ToLower() == commandName.ToLower());
+            if(commandType == null) { throw new ArgumentException("Invalid command type!"); }
+            ICommand commandInstance = (ICommand)Activator.CreateInstance(commandType);
+            return commandInstance.Execute(commandArgs);
+        }
+    }
+}
