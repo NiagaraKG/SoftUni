3156f4fa4691094af5be591458783105262191c3
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-05-05 21:59:09 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Reflection and Attributes/ValidationAttributes/Utilities/Validator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Utilities
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            if (obj == null) { return false; }
+            Type objType = obj.GetType();
+            PropertyInfo[] properties = objType.GetProperties();
+            foreach (var p in properties)
+            {
+                MyValidationAttribute[] attributes = p.GetCustomAttributes().Where(x => x is MyValidationAttribute)
+                                                                            .Cast<MyValidationAttribute>().ToArray();
+                foreach (var att in attributes)
+                {
+                    if (!att.IsValid(p.GetValue(obj))) { return false; }
+                }
+            }
+            return true;
+        }
+    }
+}
