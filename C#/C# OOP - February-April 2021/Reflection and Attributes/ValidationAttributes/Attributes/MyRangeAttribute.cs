b4498e49bbb0f2e4a0594e2349889733c3af02f8
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-05-06 22:15:34 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Reflection and Attributes/ValidationAttributes/Attributes/MyRangeAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes.Attributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private readonly int minValue;
+        private readonly int maxValue;
+        public MyRangeAttribute(int min, int max)
+        {
+            this.ValidateRange(min, max);
+            this.minValue = min;
+            this.maxValue = max;
+        }
+        public override bool IsValid(object obj)
+        {
+            if (obj is Int32)
+            {
+                int value = (int)obj;
+                if (value < this.minValue || value > this.maxValue) { return false; }
+                return true;
+            }
+            else { throw new InvalidOperationException("Cannot validate given data type!"); }
+        }
+        private void ValidateRange(int min, int max)
+        {
+            if (min > max) { throw new ArgumentException("Ïnvalid range!"); }
+        }
+    }
+}
