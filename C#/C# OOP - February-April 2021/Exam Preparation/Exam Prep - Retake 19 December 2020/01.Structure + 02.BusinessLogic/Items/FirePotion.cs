7b4609bbcbdc82ceb5ab0d92925bdd6cd3d467f7
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-05-30 22:17:49 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Exam Preparation/Exam Prep - Retake 19 December 2020/01.Structure + 02.BusinessLogic/Items/FirePotion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WarCroft.Constants;
+
+namespace WarCroft.Entities.Items
+{
+    public class FirePotion : Item
+    {
+        public FirePotion() : base(5) { }
+        public override void AffectCharacter(Characters.Contracts.Character character)
+        {
+            if (!character.IsAlive) { throw new InvalidOperationException(ExceptionMessages.AffectedCharacterDead); }
+            character.Health -= 20;
+            if(character.Health <= 0) { character.IsAlive = false; }
+        }
+    }
+}
