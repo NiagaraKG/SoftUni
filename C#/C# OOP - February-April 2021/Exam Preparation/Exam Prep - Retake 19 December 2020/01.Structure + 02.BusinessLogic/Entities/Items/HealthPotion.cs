319d4fca13d8224c103314e7571f34b12e04d23f
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-05-30 22:19:01 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Exam Preparation/Exam Prep - Retake 19 December 2020/01.Structure + 02.BusinessLogic/Entities/Items/HealthPotion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WarCroft.Constants;
+
+namespace WarCroft.Entities.Items
+{
+    public class HealthPotion : Item
+    {
+        public HealthPotion() : base(5) { }
+        public override void AffectCharacter(Characters.Contracts.Character character)
+        {
+            if (!character.IsAlive) { throw new InvalidOperationException(ExceptionMessages.AffectedCharacterDead); }
+            character.Health += 20;
+        }
+    }
+}
