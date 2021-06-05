f40e51831c58c81b91e412c9bf70b01dee0aab82
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-06-05 20:55:09 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Exam Preparation/Exam Prep - Retake 19 December 2020/03.UnitTests/FestivalManager/Entities/Song.cs
@@ -0,0 +1,17 @@
+﻿namespace FestivalManager.Entities
+{
+    using System;
+
+    public class Song
+    {
+        public Song(string name, TimeSpan duration)
+        {
+            this.Name = name;
+            this.Duration = duration;
+        }
+        public string Name { get; }
+        public TimeSpan Duration { get; }
+        public override string ToString()
+        { return $"{this.Name} ({this.Duration:mm\\:ss})"; }
+    }
+}
