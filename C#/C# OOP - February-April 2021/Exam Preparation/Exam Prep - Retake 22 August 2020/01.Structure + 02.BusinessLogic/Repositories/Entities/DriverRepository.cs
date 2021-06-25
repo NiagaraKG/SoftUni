20c247c199ceb063c85d8e1f4711019819de73ee
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-06-25 22:23:18 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Exam Preparation/Exam Prep - Retake 22 August 2020/01.Structure + 02.BusinessLogic/Repositories/Entities/DriverRepository.cs
@@ -0,0 +1,31 @@
+﻿using EasterRaces.Models.Drivers.Contracts;
+using EasterRaces.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasterRaces.Repositories.Entities
+{
+    public class DriverRepository : IRepository<IDriver>
+    {
+        private readonly IDictionary<string, IDriver> driversByName;
+        public DriverRepository() { this.driversByName = new Dictionary<string, IDriver>(); }
+
+        public void Add(IDriver model)
+        {
+            if (this.driversByName.ContainsKey(model.Name))
+            { throw new ArgumentException($"Driver {model.Name} is already created."); }
+            this.driversByName.Add(model.Name, model);
+        }
+        public IDriver GetByName(string name)
+        {
+            return driversByName.FirstOrDefault(d => d.Key == name).Value;
+            /*IDriver d = null;
+            if (this.driversByName.ContainsKey(name)) { d = this.driversByName[name]; }
+            return d;*/
+        }
+        public IReadOnlyCollection<IDriver> GetAll() { return this.driversByName.Values.ToList(); }
+        public bool Remove(IDriver model) { return this.driversByName.Remove(model.Name); }
+    }
+}
