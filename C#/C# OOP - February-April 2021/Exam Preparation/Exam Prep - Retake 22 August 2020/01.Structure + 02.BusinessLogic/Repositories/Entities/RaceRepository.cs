dfd07bd968270b5107ff5d72c59161a914dbe18b
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-06-25 22:23:44 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Exam Preparation/Exam Prep - Retake 22 August 2020/01.Structure + 02.BusinessLogic/Repositories/Entities/RaceRepository.cs
@@ -0,0 +1,31 @@
+﻿using EasterRaces.Models.Races.Contracts;
+using EasterRaces.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasterRaces.Repositories.Entities
+{
+    public class RaceRepository : IRepository<IRace>
+    {
+        private readonly IDictionary<string, IRace> racesByName;
+        public RaceRepository() { this.racesByName = new Dictionary<string, IRace>(); }
+
+        public void Add(IRace model)
+        {
+            if (this.racesByName.ContainsKey(model.Name))
+            { throw new ArgumentException($"Race {model.Name} is already created."); }
+            this.racesByName.Add(model.Name, model);
+        }
+        public IRace GetByName(string name)
+        {
+            return racesByName.FirstOrDefault(r => r.Key == name).Value;
+            /*IRace r = null;
+            if (this.racesByName.ContainsKey(name)) { r = this.racesByName[name]; }
+            return r;*/
+        }
+        public IReadOnlyCollection<IRace> GetAll() { return this.racesByName.Values.ToList(); }
+        public bool Remove(IRace model) { return this.racesByName.Remove(model.Name); }
+    }
+}
