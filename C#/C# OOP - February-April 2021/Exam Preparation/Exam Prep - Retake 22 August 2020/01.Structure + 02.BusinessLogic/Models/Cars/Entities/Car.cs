2e5dd1c2972afdc544a6b76f85f0ae5f35c69bab
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-06-26 22:03:16 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Exam Preparation/Exam Prep - Retake 22 August 2020/01.Structure + 02.BusinessLogic/Models/Cars/Entities/Car.cs
@@ -0,0 +1,43 @@
+﻿using EasterRaces.Models.Cars.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Models.Cars.Entities
+{
+    public abstract class Car : ICar
+    {
+        private string model;
+        private int maxHP, minHP, horsePower;
+        public string Model
+        {
+            get => this.model;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 4)
+                { throw new ArgumentException($"Model {value} cannot be less than 4 symbols."); }
+                this.model = value;
+            }
+        }
+
+        public int HorsePower
+        {
+            get => this.horsePower;
+            private set
+            {
+                if (value > maxHP || value < minHP) { throw new ArgumentException($"Invalid horse power: {value}."); }
+                this.horsePower = value;
+            }
+        }
+
+        public double CubicCentimeters { get; private set; }
+
+        public double CalculateRacePoints(int laps) { return this.CubicCentimeters / this.HorsePower * laps; }
+
+        protected Car(string model, int horsePower, double cubicCentimeters, int minHorsePower, int maxHorsePower)
+        {
+            this.minHP = minHorsePower; this.maxHP = maxHorsePower;
+            this.Model = model; this.HorsePower = horsePower; this.CubicCentimeters = cubicCentimeters;            
+        }
+    }
+}
