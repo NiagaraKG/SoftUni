ad62f7e7b7e6c0d850778f8e6af2202d218099cc
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-07-06 22:30:13 +0300

    Add files via upload

---
+++ C#/C# OOP - February-April 2021/Exam/01.Structure + 02.BusinessLogic/AquaShop/Repositories/DecorationRepository.cs
@@ -0,0 +1,34 @@
+﻿using AquaShop.Models.Decorations.Contracts;
+using AquaShop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AquaShop.Repositories
+{
+    public class DecorationRepository : IRepository<IDecoration>
+    {
+        public DecorationRepository()
+        {
+            this.decorations = new List<IDecoration>();
+        }
+        private readonly List<IDecoration> decorations;
+        public IReadOnlyCollection<IDecoration> Models => this.decorations;
+
+        public void Add(IDecoration model)
+        {
+            this.decorations.Add(model);
+        }
+
+        public IDecoration FindByType(string type)
+        {
+            return this.decorations.FirstOrDefault(d => d.GetType().Name == type);
+        }
+
+        public bool Remove(IDecoration model)
+        {
+            return this.decorations.Remove(model);
+        }
+    }
+}
