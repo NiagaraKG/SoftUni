53f1be1e16c133d56e2b88bade0ef3b311dfc4a3
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-12-06 23:09:34 +0200

    Add files via upload

---
+++ C#/MS SQL - September-October 2021/CRUD/Problem22-25.sql
@@ -0,0 +1,13 @@
+/*SELECT PeakName FROM Peaks ORDER BY PeakName
+
+SELECT TOP 30 CountryName, [Population] FROM Countries WHERE ContinentCode = 'EU'
+ORDER BY [Population] DESC, CountryName
+
+SELECT CountryName, CountryCode,
+CASE
+	WHEN CurrencyCode = 'EUR' THEN 'Euro'
+	ELSE 'Not Euro'
+END 
+AS Currency FROM Countries ORDER BY CountryName*/
+
+SELECT [Name] FROM Characters ORDER BY [Name]
