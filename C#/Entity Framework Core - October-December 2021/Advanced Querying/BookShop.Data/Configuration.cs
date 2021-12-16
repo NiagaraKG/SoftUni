3b8d40b2d2e662c6c93ff4d04bac8860142ae987
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-12-16 22:11:37 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/Advanced Querying/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString 
+            => "Server=DESKTOP-5HMNE89\\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
+    }
+}
