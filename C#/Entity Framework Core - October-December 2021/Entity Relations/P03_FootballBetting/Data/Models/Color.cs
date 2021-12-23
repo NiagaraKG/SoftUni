940936bb80e81bc90f64ff2bbff648e89c364630
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-12-23 22:59:07 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/Entity Relations/P03_FootballBetting/Data/Models/Color.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class Color
+    {
+        public int ColorId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Team> PrimaryKitTeams { get; set; }
+        public ICollection<Team> SecondaryKitTeams { get; set; }
+    }
+}
