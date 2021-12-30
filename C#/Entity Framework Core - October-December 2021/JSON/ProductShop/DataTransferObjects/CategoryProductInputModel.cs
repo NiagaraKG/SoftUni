f2bfc797db44184ae89d4d72ea42ee5c9db93440
Niagara Georgieva <niagara.georgieva@abv.bg>
2021-12-30 23:05:51 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/JSON/ProductShop/DataTransferObjects/CategoryProductInputModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductShop.DataTransferObjects
+{
+    public class CategoryProductInputModel
+    {
+        public int CategoryId { get; set; }
+        public int ProductId { get; set; }
+    }
+}
