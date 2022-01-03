1f2a418e8b26401b6b034c5467df6bea9729b9dd
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-01-03 22:49:47 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/XML/CarDealer/DataTransferObjects/Input/PartsDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.DataTransferObjects.Input
+{
+    [XmlType("partId")]
+    public class PartsDto
+    {
+        [XmlAttribute("id")]
+        public int PartId { get; set; }
+    }
+}
