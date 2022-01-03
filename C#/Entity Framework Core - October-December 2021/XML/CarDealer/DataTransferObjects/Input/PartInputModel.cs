076274b0486a92637383e142c798ec9c5b907c0c
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-01-03 22:50:11 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/XML/CarDealer/DataTransferObjects/Input/PartInputModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.DataTransferObjects.Input
+{
+    [XmlType("Part")]
+    public class PartInputModel
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("price")]
+        public decimal Price { get; set; }
+
+        [XmlElement("quantity")]
+        public int Quantity { get; set; }
+
+        [XmlElement("supplierId")]
+        public int SupplierId { get; set; }
+    }
+}
