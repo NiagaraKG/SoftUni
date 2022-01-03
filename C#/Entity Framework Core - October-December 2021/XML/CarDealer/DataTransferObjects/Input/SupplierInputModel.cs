8e4d504e044e140fc2cbd47ddcc2bc4ebca9a9e1
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-01-03 22:48:35 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/XML/CarDealer/DataTransferObjects/Input/SupplierInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.DataTransferObjects.Input
+{
+    [XmlType("Supplier")]
+    public class SupplierInputModel
+    {
+        [XmlElement("name")]
+
+        public string Name { get; set; }
+
+        [XmlElement("isImporter")]
+        public bool IsImporter { get; set; }
+    }
+}
