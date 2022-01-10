945a6f3bb120c4e0ab66be2c75d0c0b1b0c69e3b
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-01-10 23:12:48 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/XML/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ProductShop.Dtos.Import;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            this.CreateMap<UserInputModel, User>();
+
+            this.CreateMap<ProductInputModel, Product>();
+
+            this.CreateMap<CategoryInputModel, Category>();
+
+            this.CreateMap<CategoryProductInputModel, CategoryProduct>();
+
+        }
+    }
+}
