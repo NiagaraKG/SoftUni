be1380c2f5f2aac3109409fada5fdb64c230c978
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-01-13 22:29:38 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/XML/ProductShop/Dtos/Export/ProductItemSold.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ProductShop.Dtos.Export
+{
+    [XmlType("Product")]
+    public class ProductItemSold
+    {
+
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("price")]
+        public decimal Price { get; set; }
+
+    }
+}
