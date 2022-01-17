e31a0e97d3621b9299de0465525f4cd8e417a5cf
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-01-17 23:35:54 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/Exam 04.12.2021/Theatre/Data/Models/Play.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Theatre.Data.Models.Enums;
+
+namespace Theatre.Data.Models
+{
+    public class Play
+    {
+        public Play()
+        {
+            this.Tickets = new HashSet<Ticket>();
+            this.Casts = new HashSet<Cast>();
+        }
+
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        public TimeSpan Duration { get; set; }
+
+        [Required]
+        [Range(0.00,10.00)]
+        public float Rating { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [Required]
+        [MaxLength(700)]
+        public string Description { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Screenwriter { get; set; }
+
+        public virtual ICollection<Cast> Casts { get; set; }
+
+        public virtual ICollection<Ticket> Tickets { get; set; }
+
+    }
+}
