f5c3f54162cd5bb9a040260daf311478fa1804c2
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-01-18 22:15:37 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/Exam 04.12.2021/Theatre/DataProcessor/ImportDto/ImportPlayDto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Theatre.DataProcessor.ImportDto
+{
+    [XmlType("Play")]
+    public class ImportPlayDto
+    {
+        [XmlElement("Title")]
+        [Required]
+        [MinLength(4)]
+        [MaxLength(30)]
+        public string Title { get; set; }
+
+        [XmlElement("Duration")]
+        [Required]
+        public string Duration { get; set; }
+
+        [XmlElement("Rating")]
+        [Required]
+        [Range(0.00, 10.00)]
+        public float Rating { get; set; }
+
+        [XmlElement("Genre")]
+        [Required]
+        public string Genre { get; set; }
+
+        [XmlElement("Description")]
+        [Required]
+        [MaxLength(700)]
+        public string Description { get; set; }
+
+        [XmlElement("Screenwriter")]
+        [Required]
+        [MinLength(4)]
+        [MaxLength(30)]
+        public string Screenwriter { get; set; }
+    }
+}
