2561453fcca21f6868f854716a4d6d689b09c6b2
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-01-18 22:15:08 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/Exam 04.12.2021/Theatre/DataProcessor/ImportDto/ImportTheatreDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Theatre.DataProcessor.ImportDto
+{
+    public class ImportTheatreDto
+    {
+        [Required]
+        [MaxLength(30)]
+        [MinLength(4)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 10)]
+        public sbyte NumberOfHalls { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [MinLength(4)]
+        public string Director { get; set; }
+
+        public IEnumerable<ImportTicketDto> Tickets { get; set; }
+    }
+}
