5254a31ebdab3a5b8afc4e3f3016fce2c8cfec8a
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-01-22 23:51:41 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/Exam Preparation/04.04.2021/TeisterMask/DataProcessor/ImportDto/ImportTaskDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    [XmlType("Task")]
+    public class ImportTaskDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Name { get; set; }
+
+        [XmlElement("OpenDate")]
+        [Required]
+        public string OpenDate { get; set; }
+
+        [XmlElement("DueDate")]
+        [Required]
+        public string DueDate { get; set; }
+
+        [XmlElement("ExecutionType")]
+        [Range(0, 3)]
+        public int ExecutionType { get; set; }
+
+        [XmlElement("LabelType")]
+        [Range(0, 4)]
+        public int LabelType { get; set; }
+    }
+}
