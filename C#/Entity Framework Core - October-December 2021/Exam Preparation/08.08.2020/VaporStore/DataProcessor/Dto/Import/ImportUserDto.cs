a4b779276034499c258873235afff922c7317238
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-01-24 23:33:22 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/Exam Preparation/08.08.2020/VaporStore/DataProcessor/Dto/Import/ImportUserDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace VaporStore.DataProcessor.Dto.Import
+{
+    public class ImportUserDto
+    {
+        [Required]
+        [MaxLength(20)]
+        [MinLength(3)]
+        public string Username { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z][a-z]+\s[A-Z][a-z]+$")]
+        public string FullName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Range(3, 103)]
+        public int Age { get; set; }
+
+        [MinLength(1)]
+        public ImportCardDto[] Cards { get; set; }
+    }
+}
