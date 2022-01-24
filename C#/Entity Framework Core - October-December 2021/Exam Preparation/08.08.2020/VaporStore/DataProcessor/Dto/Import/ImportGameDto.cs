82f9f7591323427aa466d2d9db8bd68eaddefd7b
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-01-24 23:34:42 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/Exam Preparation/08.08.2020/VaporStore/DataProcessor/Dto/Import/ImportGameDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace VaporStore.DataProcessor.Dto.Import
+{
+    public class ImportGameDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0, (double)decimal.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string ReleaseDate { get; set; }
+
+        [Required]
+        public string Developer { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [MinLength(1)]
+        public string[] Tags { get; set; }
+    }
+}
