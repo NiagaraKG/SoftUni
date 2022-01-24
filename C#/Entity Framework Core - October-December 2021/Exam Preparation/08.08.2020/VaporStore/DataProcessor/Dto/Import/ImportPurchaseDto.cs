1342aefb1e1c7fcf86fc1de7464584f2b124bdb3
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-01-24 23:34:02 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/Exam Preparation/08.08.2020/VaporStore/DataProcessor/Dto/Import/ImportPurchaseDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace VaporStore.DataProcessor.Dto.Import
+{
+    [XmlType(TypeName = "Purchase")]
+    public class ImportPurchaseDto
+    {
+        [Required]
+        [XmlElement]
+        public string Type { get; set; }
+
+        [Required]
+        [XmlElement]
+        [RegularExpression(@"^[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}$")]
+        public string Key { get; set; }
+
+        [Required]
+        [XmlElement]
+        public string Date { get; set; }
+
+        [Required]
+        [XmlElement]
+        public string Card { get; set; }
+
+        [Required]
+        [XmlAttribute(AttributeName = "title")]
+        public string Title { get; set; }
+    }
+}
