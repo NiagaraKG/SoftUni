db92248601db5b0851fc99a875942b5cfab4e629
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-01-25 22:18:12 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/Exam Preparation/08.08.2020/VaporStore/DataProcessor/Dto/Export/ExportGameDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace VaporStore.DataProcessor.Dto.Export
+{
+    [XmlType("Game")]
+    public class ExportGameDto
+    {
+        [XmlAttribute("title")]
+        public string Title { get; set; }
+
+        [XmlElement("Genre")]
+        public string Genre { get; set; }
+
+        [XmlElement("Price")]
+        public decimal Price { get; set; }
+    }
+}
