9676585981902dd3df49acb30633b764da3a501a
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-02-06 22:51:03 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/Exam Preparation/14.08.2020/SoftJail/DataProcessor/ImportDto/ImportOfficerDto.cs
@@ -0,0 +1,32 @@
+﻿namespace SoftJail.DataProcessor.ImportDto
+{
+    using System.Xml.Serialization;
+
+    using System.ComponentModel.DataAnnotations;
+
+    [XmlType("Officer")]
+    public class ImportOfficerDto
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        [XmlElement("Name")]
+        public string FullName { get; set; }
+
+        [Range(typeof(decimal), "0.0", "79228162514264337593543950335")]
+        [XmlElement("Money")]
+        public decimal Salary { get; set; }
+
+        [XmlElement("Position")]
+        public string Position { get; set; }
+
+        [XmlElement("Weapon")]
+        public string Weapon { get; set; }
+
+        [XmlElement("DepartmentId")]
+        public int DepartmentId { get; set; }
+
+        [XmlArray("Prisoners")]
+        public ImportOfficerPrisonerDto[] Prisoners { get; set; }
+    }
+}
