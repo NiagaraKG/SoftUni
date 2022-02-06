ad53bd3c1b88919501b533cabd79455b3e9109c2
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-02-06 22:49:50 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/Exam Preparation/14.08.2020/SoftJail/DataProcessor/ImportDto/ImportDepartmentDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SoftJail.DataProcessor.ImportDto
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ImportDepartmentDto
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(25)]
+        public string Name { get; set; }
+
+        public ImportCellDto[] Cells { get; set; }
+    }
+}
