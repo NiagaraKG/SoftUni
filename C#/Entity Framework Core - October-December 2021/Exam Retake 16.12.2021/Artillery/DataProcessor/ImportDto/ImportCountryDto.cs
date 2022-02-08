e75c9ecde130416226f2e5c6f6c7d4d16e567f92
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-02-08 23:44:06 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/Exam Retake 16.12.2021/Artillery/DataProcessor/ImportDto/ImportCountryDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Artillery.DataProcessor.ImportDto
+{
+    [XmlType("Country")]
+    public class ImportCountryDto
+    {
+        [XmlElement("CountryName")]
+        [Required]
+        [MaxLength(60)]
+        [MinLength(4)]
+        public string CountryName { get; set; }
+
+        [XmlElement("ArmySize")]
+        [Required]
+        [Range(50_000, 10_000_000)]
+        public int ArmySize { get; set; }
+    }
+}
