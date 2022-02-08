965032cec9ae19b0524c2d6c16c3e7c4a62a1f5b
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-02-08 23:45:58 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/Exam Retake 16.12.2021/Artillery/DataProcessor/ImportDto/ImportShellDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Artillery.DataProcessor.ImportDto
+{
+    [XmlType("Shell")]
+    public class ImportShellDto
+    {
+        [XmlElement("ShellWeight")]
+        [Required]
+        [Range(2.0, 1_680.0)]
+        public double ShellWeight { get; set; }
+
+        [XmlElement("Caliber")]
+        [Required]
+        [MaxLength(30)]
+        [MinLength(4)]
+        public string Caliber { get; set; }
+    }
+}
