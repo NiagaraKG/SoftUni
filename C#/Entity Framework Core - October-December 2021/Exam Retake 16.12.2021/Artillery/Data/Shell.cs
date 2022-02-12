bf594eabfc7b627a74e70bd370e3da40006fa0f1
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-02-12 23:10:30 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/Exam Retake 16.12.2021/Artillery/Data/Shell.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Artillery.Data.Models
+{
+    public class Shell
+    {
+        public Shell()
+        {
+            this.Guns = new HashSet<Gun>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Range(2.0, 1_680.0)]
+        public double ShellWeight { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Caliber { get; set; }
+
+        public ICollection<Gun> Guns { get; set; }
+    }
+}
