339399725b751cb88a155db4fe7f097e280e29c3
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-02-12 23:08:55 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/Exam Retake 16.12.2021/Artillery/Data/Models/CountryGun.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Artillery.Data.Models
+{
+    public class CountryGun
+    {
+        [Required]
+        [ForeignKey(nameof(Country))]
+        public int CountryId { get; set; }
+        public virtual Country Country { get; set; }
+
+
+        [Required]
+        [ForeignKey(nameof(Gun))]
+        public int GunId { get; set; }
+        public virtual Gun Gun { get; set; }
+    }
+}
