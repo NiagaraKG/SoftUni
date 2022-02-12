d1bdbe84b433ed6dcc3798a28fcc2b48ce1699ba
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-02-12 23:08:11 +0200

    Add files via upload

---
+++ C#/Entity Framework Core - October-December 2021/Exam Retake 16.12.2021/Artillery/Data/Models/Country.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Artillery.Data.Models
+{
+    public class Country
+    {
+        public Country()
+        {
+            this.CountriesGuns = new HashSet<CountryGun>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(60)]
+        public string CountryName { get; set; }
+
+        [Required]
+        [Range(50_000, 10_000_000)]
+        public int ArmySize { get; set; }
+
+        public ICollection<CountryGun> CountriesGuns { get; set; }
+    }
+}
