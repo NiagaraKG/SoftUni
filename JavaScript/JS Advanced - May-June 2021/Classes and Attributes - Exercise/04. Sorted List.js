2d3652d7a9638abac8c81144e3716454bdc37e3b
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-03-04 22:10:21 +0200

    Add files via upload

---
+++ JavaScript/JS Advanced - May-June 2021/Classes and Attributes - Exercise/04. Sorted List.js
@@ -0,0 +1,27 @@
+class List {
+    constructor() { this.numbers = [];  this.size = this.numbers.length;}
+    add(el) {
+        this.numbers[this.numbers.length] = Number(el);
+        this.size = this.numbers.length;
+        this.numbers.sort((a,b)=>a-b);
+    }
+    remove(index) {
+        if (index >= 0 && index < this.numbers.length) {
+            this.numbers.splice(index,1);
+            this.size = this.numbers.length;            
+        }
+    }
+    get(index){
+        if (index >= 0 && index < this.numbers.length) {
+            return this.numbers[index];
+        }
+    }    
+}
+
+let list = new List();
+list.add(5);
+list.add(6);
+list.add(7);
+console.log(list.get(1));
+list.remove(1);
+console.log(list.get(1));
