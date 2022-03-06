a2fe626e20aa520098eea93e44815e0df2b969fd
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-03-06 23:29:26 +0200

    Add files via upload

---
+++ JavaScript/JS Advanced - May-June 2021/Prototypes and Inheritance - Exercise/04. Balloons.js
@@ -0,0 +1,26 @@
+function createBalloons() {
+    class Balloon {
+        constructor(color, gasWeight) {
+            this.color = color;
+            this.gasWeight = Number(gasWeight);
+        }
+    }
+
+    class PartyBalloon extends Balloon {
+        constructor(color, gasWeight, ribbonColor, ribbonLength) {
+            super(color, gasWeight);
+            this._ribbon = { color: ribbonColor, length: Number(ribbonLength) };
+        }
+        get ribbon() { return this._ribbon; }
+    }
+
+    class BirthdayBalloon extends PartyBalloon {
+        constructor(color, gasWeight, ribbonColor, ribbonLength, text) {
+            super(color, gasWeight, ribbonColor, ribbonLength);
+            this._text = text;
+        }
+        get text() { return this._text; }
+    }
+
+    return { Balloon, PartyBalloon, BirthdayBalloon };
+}
