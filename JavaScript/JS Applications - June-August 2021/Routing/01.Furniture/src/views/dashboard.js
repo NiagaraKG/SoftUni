1abe86f15cf7d29ca6b6ccced89aa8eb037949cc
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-04-02 23:36:45 +0300

    Add files via upload

---
+++ JavaScript/JS Applications - June-August 2021/Routing/01.Furniture/src/views/dashboard.js
@@ -0,0 +1,34 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+import { getFurniture } from "../api/data.js";
+
+let templateDashboard = (data) => html`
+<div class="row space-top">
+    <div class="col-md-12">
+        <h1>Welcome to Furniture System</h1>
+        <p>Select furniture from the catalog to view details.</p>
+    </div>
+</div>
+<div class="row space-top">
+    ${data.map(templateItem)}
+</div>`;
+
+let templateItem = (item) => html`
+<div class="col-md-4">
+    <div class="card text-white bg-primary">
+        <div class="card-body">
+            <img src=${item.img} />
+            <p>${item.description}</p>
+            <footer>
+                <p>Price: <span>${item.price} $</span></p>
+            </footer>
+            <div>
+                <a href=${`/details/${item._id}`} class="btn btn-info">Details</a>
+            </div>
+        </div>
+    </div>
+</div>`;
+
+export async function pageDashboard(ctx) {
+    let data = await getFurniture();
+    ctx.render(templateDashboard(data));
+}
