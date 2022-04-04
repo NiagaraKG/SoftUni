3156cbe186b5b87251bbafc0cab52ad3997f3204
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-04-04 22:53:05 +0300

    Add files via upload

---
+++ JavaScript/JS Applications - June-August 2021/Routing/01.Furniture/src/views/login.js
@@ -0,0 +1,39 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+import { login } from "../api/data.js";
+
+let templateLogin = (Submit) => html`
+<div class="row space-top">
+            <div class="col-md-12">
+                <h1>Login User</h1>
+                <p>Please fill all fields.</p>
+            </div>
+        </div>
+        <form @submit=${Submit}>
+            <div class="row space-top">
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label class="form-control-label" for="email">Email</label>
+                        <input class="form-control" id="email" type="text" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label class="form-control-label" for="password">Password</label>
+                        <input class="form-control" id="password" type="password" name="password">
+                    </div>
+                    <input type="submit" class="btn btn-primary" value="Login" />
+                </div>
+            </div>
+        </form>`;
+
+export async function pageLogin(ctx){
+    ctx.render(templateLogin(Submit));
+
+    async function Submit(e) {
+        e.preventDefault();
+        let formData = new FormData(e.target);
+        let email = formData.get('email');
+        let password = formData.get('password');
+        await login(email, password);
+        ctx.setNavigation();
+        ctx.page.redirect('/');
+    }
+}
