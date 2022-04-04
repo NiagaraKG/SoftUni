fd0447d65f3b7ecf53c186e49246f40c78ef481c
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-04-04 22:54:00 +0300

    Add files via upload

---
+++ JavaScript/JS Applications - June-August 2021/Routing/01.Furniture/src/views/edit.js
@@ -0,0 +1,105 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+import { getItem, Edit } from "../api/data.js";
+
+let templateEdit = (item, Submit, invalidMake, invalidModel, invalidYear,
+    invalidDescription, invalidPrice, invalidImage) => html`
+<div class="row space-top">
+    <div class="col-md-12">
+        <h1>Edit Furniture</h1>
+        <p>Please fill all fields.</p>
+    </div>
+</div>
+<form @submit=${Submit}>
+    <div class="row space-top">
+        <div class="col-md-4">
+            <div class="form-group">
+                <label class="form-control-label" for="new-make">Make</label>
+                <input class=${'form-control' + (invalidMake ? ' is-invalid' : 'is-valid' )} id="new-make" type="text" name="make" .value=${item.make}>
+            </div>
+            <div class="form-group has-success">
+                <label class="form-control-label" for="new-model">Model</label>
+                <input class=${'form-control' + (invalidModel ? ' is-invalid' : 'is-valid' )} id="new-model" type="text" name="model" .value=${item.model}>
+            </div>
+            <div class="form-group has-danger">
+                <label class="form-control-label" for="new-year">Year</label>
+                <input class=${'form-control' + (invalidYear ? ' is-invalid' : 'is-valid' )} id="new-year" type="number" name="year" .value=${item.year}>
+            </div>
+            <div class="form-group">
+                <label class="form-control-label" for="new-description">Description</label>
+                <input class=${'form-control' + (invalidDescription ? ' is-invalid' : 'is-valid' )} id="new-description" type="text" name="description" .value=${item.description}>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="form-group">
+                <label class="form-control-label" for="new-price">Price</label>
+                <input class=${'form-control' + (invalidPrice ? ' is-invalid' : 'is-valid' )} id="new-price" type="number" name="price" .value=${item.price}>
+            </div>
+            <div class="form-group">
+                <label class="form-control-label" for="new-image">Image</label>
+                <input class=${'form-control' + (invalidImage ? ' is-invalid' : 'is-valid' )} id="new-image" type="text" name="img" .value=${item.img}>
+            </div>
+            <div class="form-group">
+                <label class="form-control-label" for="new-material">Material (optional)</label>
+                <input class="form-control" id="new-material" type="text" name="material" .value=${item.material}>
+            </div>
+            <input type="submit" class="btn btn-info" value="Edit" />
+        </div>
+    </div>
+</form>`;
+
+export async function pageEdit(ctx) {
+    let id = ctx.params.id;
+    let item = await getItem(id);
+    ctx.render(templateEdit(item, Submit));
+
+    async function Submit(e) {
+        e.preventDefault();
+        let formData = new FormData(e.target);
+        let make = formData.get('make');
+        let model = formData.get('model');
+        let year = Number(formData.get('year'));
+        let description = formData.get('description');
+        let price = Number(formData.get('price'));
+        let img = formData.get('img');
+        let material = formData.get('material');
+        let newItem = {make, model, year, description, price, img, material};
+
+        let invalidMake = false, invalidModel = false;
+        let invalidYear = false, invalidDescription = false;
+        let invalidPrice = false, invalidImage = false;
+        let errorMessage = '';
+
+        if (make.length < 4) {
+            invalidMake = true;
+            errorMessage += 'Make must be at least 4 symbols.\n';
+        }
+        if (model.length < 4) {
+            invalidModel = true;
+            errorMessage += 'Model must be at least 4 symbols.\n';
+        }
+        if (year == '' || year < 1950 || year > 2050) {
+            invalidYear = true;
+            errorMessage += 'Year must be between 1950 and 2050.\n';
+        }
+        if (description.length <= 10) {
+            invalidDescription = true;
+            errorMessage += 'Description must be more than 10 symbols.\n';
+        }
+        if (price == '' || price <= 0) {
+            invalidPrice = true;
+            errorMessage += 'Price must be positive number.\n';
+        }
+        if (img == '') {
+            invalidImage = true;
+            errorMessage += 'Image URL is required.\n';
+        }
+        if (invalidMake || invalidModel || invalidYear
+            || invalidDescription || invalidPrice || invalidImage) {
+            ctx.render(templateRegister(item, Submit, invalidMake, invalidModel,
+                invalidYear, invalidDescription, invalidPrice, invalidImage));
+            return alert(errorMessage);
+        }
+        await Edit(item._id, newItem);
+        ctx.page.redirect('/');
+    }
+}
