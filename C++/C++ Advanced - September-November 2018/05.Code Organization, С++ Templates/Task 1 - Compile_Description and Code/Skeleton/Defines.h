4241a8bac885b4c4a27b75e19dcf5d0d1d47b8fc
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-06-21 23:36:18 +0300

    Add files via upload

---
+++ C++/C++ Advanced - September-November 2018/05.Code Organization, С++ Templates/Task 1 - Compile_Description and Code/Skeleton/Defines.h
@@ -0,0 +1,10 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#include <string>
+#include <iostream>
+
+#define DONT_COMPILE_THIS 1;
+#define STANDARD_TEMPLATE_LIBRARY namespace std;
+
+#endif // DEFINES_H
