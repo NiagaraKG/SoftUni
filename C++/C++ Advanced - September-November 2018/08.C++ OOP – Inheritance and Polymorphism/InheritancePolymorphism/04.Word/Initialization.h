ee5c11c71674a57ad3c39d92a4c9a8cea2d62c70
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-07-11 21:06:56 +0300

    Add files via upload

---
+++ C++/C++ Advanced - September-November 2018/08.C++ OOP – Inheritance and Polymorphism/InheritancePolymorphism/04.Word/Initialization.h
@@ -0,0 +1,17 @@
+#ifndef INITIALIZATION_H
+#define INITIALIZATION_H
+
+#include "CommandInterface.h"
+#include "AdvancedCommandInterface.h"
+
+#include <memory>
+#include <string>
+
+std::shared_ptr<CommandInterface> buildCommandInterface(std::string& text) {
+	auto interface = std::make_shared<AdvancedCommandInterface>(text);
+	interface->init();
+	return interface;
+}
+
+#endif // INITIALIZATION_H
+
