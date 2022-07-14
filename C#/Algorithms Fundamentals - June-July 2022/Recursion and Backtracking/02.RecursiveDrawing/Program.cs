5f76aee350aba126cb98588031cd8ff1019c1e51
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-07-14 22:24:24 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Recursion and Backtracking/02.RecursiveDrawing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RecursionAndBacktracking
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Draw(n);
+        }
+
+        public static void Draw(int n)
+        {
+            if (n == 0) return;
+            Console.WriteLine(new string('*', n));
+            Draw(n - 1);
+            Console.WriteLine(new string('#', n));
+        }
+
+    }
+}
