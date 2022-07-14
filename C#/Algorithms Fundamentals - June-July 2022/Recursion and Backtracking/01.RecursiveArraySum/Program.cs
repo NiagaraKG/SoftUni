ae614ec69cdc5946b671b92486d1b3857c469237
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-07-14 22:23:44 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Recursion and Backtracking/01.RecursiveArraySum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace RecursionAndBacktracking
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(Sum(arr, 0));
+        }
+
+        public static int Sum(int[] arr, int i)
+        {
+            if (i == arr.Length - 1) { return arr[i]; }
+            return arr[i] + Sum(arr, i + 1);
+        }
+
+    }
+}
