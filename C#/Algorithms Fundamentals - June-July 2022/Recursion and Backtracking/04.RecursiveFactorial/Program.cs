db1be0c0f24952311eea5a92e3a8d605e3c430b0
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-07-15 21:40:50 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Recursion and Backtracking/04.RecursiveFactorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RecursionAndBacktracking
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(n));
+        }
+
+        public static int Factorial(int n)
+        {
+            if (n == 0) return 1;
+            return n * Factorial(n - 1);
+        }
+
+    }
+}
