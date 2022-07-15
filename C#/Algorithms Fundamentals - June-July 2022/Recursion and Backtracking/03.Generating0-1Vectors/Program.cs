1eab633481d52edc1d84f1c65028022578155050
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-07-15 21:39:06 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Recursion and Backtracking/03.Generating0-1Vectors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RecursionAndBacktracking
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            GenerateVectors(new int[n], 0);
+        }
+
+        public static void GenerateVectors(int[] arr, int index)
+        {
+            if (index == arr.Length)
+            {
+                Console.WriteLine(string.Join(string.Empty, arr));
+                return;
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                arr[index] = i;
+                GenerateVectors(arr, index + 1);
+            }
+        }
+
+    }
+}
