a1f4c420bd60e437cd7f1d44380a0745b293f983
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-07-16 23:48:14 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Recursion and Backtracking/07.RecursiveFibonacci/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RecursionAndBacktracking
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fibonacci(n+1));
+        }
+
+        public static int Fibonacci(int n)
+        {
+            if (n <= 2) return 1;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+    }
+}
