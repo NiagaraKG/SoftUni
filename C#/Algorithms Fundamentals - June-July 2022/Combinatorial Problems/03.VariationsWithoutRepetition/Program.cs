0273f22e86259ab320b022b16f4d6c1625d10df5
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-07-18 23:06:28 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Combinatorial Problems/03.VariationsWithoutRepetition/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CombinatorialProblems
+{
+    public class Program
+    {
+        private static int k;
+        private static string[] elements;
+        private static string[] variations;
+        private static bool[] used;
+        public static void Main()
+        {
+            elements = Console.ReadLine().Split();
+            k = int.Parse(Console.ReadLine());
+            variations = new string[k];
+            used = new bool[elements.Length];
+            Variate(0);
+        }
+
+        private static void Variate(int index)
+        {
+            if (index >= variations.Length)
+            {
+                Console.WriteLine(String.Join(" ", variations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    variations[index] = elements[i];
+                    Variate(index + 1);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
