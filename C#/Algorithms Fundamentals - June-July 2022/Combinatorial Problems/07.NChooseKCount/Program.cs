f09b5081854cfe8724792cc43152fa954cd47ee9
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-07-19 23:49:44 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Combinatorial Problems/07.NChooseKCount/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CombinatorialProblems
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            Console.WriteLine(Binom(n, k));
+        }
+
+        private static int Binom(int row, int col)
+        {
+            if (row <= 1 || col == 0 || col == row) { return 1; }
+            return Binom(row - 1, col - 1) + Binom(row - 1, col);
+        }
+    }
+}
