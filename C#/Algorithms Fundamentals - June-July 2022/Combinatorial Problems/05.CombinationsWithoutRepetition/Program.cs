0dbe42b6f7618a1d6f16f4a617f19b86c22883c5
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-07-19 23:47:50 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Combinatorial Problems/05.CombinationsWithoutRepetition/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CombinatorialProblems
+{
+    public class Program
+    {
+        private static int k;
+        private static string[] elements;
+        private static string[] combinations;
+        public static void Main()
+        {
+            elements = Console.ReadLine().Split();
+            k = int.Parse(Console.ReadLine());
+            combinations = new string[k];
+            Combine(0, 0);
+        }
+
+        private static void Combine(int index, int start)
+        {
+            if (index >= combinations.Length)
+            {
+                Console.WriteLine(String.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = start; i < elements.Length; i++)
+            {
+                combinations[index] = elements[i];
+                Combine(index + 1, i + 1);
+            }
+        }
+    }
+}
