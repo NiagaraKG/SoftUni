03b3aceb884b2ce7f60241a3133a31f0d023e5a6
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-07-20 23:20:49 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Recursion and Combinatorial Problems - Exercise/01.ReverseArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RecursionExercise
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var arr = Console.ReadLine().Split();
+            Reverse(arr, 0);
+            Console.WriteLine(String.Join(" ", arr));
+        }
+
+        private static void Reverse(string[] arr, int i)
+        {
+            if (i == arr.Length / 2) { return; }
+            var temp = arr[i];
+            arr[i] = arr[arr.Length - 1 - i];
+            arr[arr.Length - 1 - i] = temp;
+            Reverse(arr, i + 1);
+        }
+    }
+}
