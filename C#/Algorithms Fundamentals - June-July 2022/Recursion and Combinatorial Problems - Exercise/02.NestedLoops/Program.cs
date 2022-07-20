face04bf4e520445314f48b4893380cd2f6bd9ce
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-07-20 23:21:31 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Recursion and Combinatorial Problems - Exercise/02.NestedLoops/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RecursionExercise
+{
+    public class Program
+    {
+        private static int[] numbers;
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            numbers = new int[n];
+            Generate(0);
+        }
+
+        private static void Generate(int index)
+        {
+            if (index >= numbers.Length)
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+                return;
+            }
+            for (int i = 1; i <= numbers.Length; i++)
+            {
+                numbers[index] = i;
+                Generate(index + 1);
+            }
+        }
+    }
+}
