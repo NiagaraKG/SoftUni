dc903507e2dc2dc1a2d4fb612c7f63ab9fc23338
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-07-23 23:44:03 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Searching, Sorting and Greedy Algorithms/01.BinarySearch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace SearchingSortingAndGreedyAlgorithms
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(BinarySearch(n, numbers));
+        }
+
+        private static int BinarySearch(int n, int[] numbers)
+        {
+            int left = 0, right = numbers.Length-1;
+            while (left <= right)
+            {
+                var mid = (left + right) / 2;
+                if (numbers[mid] == n)
+                {
+                    return mid;
+                }
+                if(numbers[mid] < n)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
