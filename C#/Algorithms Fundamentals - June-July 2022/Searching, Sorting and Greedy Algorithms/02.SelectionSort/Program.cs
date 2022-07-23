fbc5821d8783f870f9c4b1307aaf804c4fc23730
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-07-23 23:45:42 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Searching, Sorting and Greedy Algorithms/02.SelectionSort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace SearchingSortingAndGreedyAlgorithms
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            SelectionSort(numbers);
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+
+        private static void SelectionSort(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int min = i;
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[min])
+                    {
+                        min = j;
+                    }
+                }
+                Swap(numbers, i, min);
+            }
+        }
+
+        private static void Swap(int[] numbers, int first, int second)
+        {
+            int temp = numbers[first];
+            numbers[first] = numbers[second];
+            numbers[second] = temp;
+        }
+    }
+}
