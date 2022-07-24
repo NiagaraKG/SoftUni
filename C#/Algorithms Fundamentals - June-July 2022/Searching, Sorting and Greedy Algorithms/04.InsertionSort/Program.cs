35f01702e450ac2576ac0c70477c3d824671ff7a
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-07-24 23:50:17 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Searching, Sorting and Greedy Algorithms/04.InsertionSort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace SearchingSortingAndGreedyAlgorithms
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            InsertionSort(numbers);
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+
+        private static void InsertionSort(int[] numbers)
+        {
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                int j = i;
+                while (j > 0 && numbers[j-1] > numbers[j])
+                {
+                    Swap(numbers, j, j - 1);
+                    j--;
+                }
+            }
+        }
+
+        private static void Swap(int[] numbers, int first, int second)
+        {
+            int temp = numbers[first];
+            numbers[first] = numbers[second];
+            numbers[second] = temp;
+        }
+    }
+}
