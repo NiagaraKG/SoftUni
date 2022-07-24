87abba72c06a8893bf784fbac2637b34d7deec5f
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-07-24 23:48:54 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Searching, Sorting and Greedy Algorithms/03.BubbleSort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace SearchingSortingAndGreedyAlgorithms
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            BubbleSort(numbers);
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+
+        private static void BubbleSort(int[] numbers)
+        {
+            int sortedCount = 0;
+            var isSorted = false;
+            while(!isSorted)
+            {
+                isSorted = true;
+                for (int i = 1; i < numbers.Length - sortedCount; i++)
+                {
+                    int j = i - 1;
+                    if(numbers[j] > numbers[i])
+                    {
+                        Swap(numbers, i, j);
+                        isSorted = false;
+                    }
+                }
+                sortedCount++;
+            }
+        }
+
+        private static void Swap(int[] numbers, int first, int second)
+        {
+            int temp = numbers[first];
+            numbers[first] = numbers[second];
+            numbers[second] = temp;
+        }
+    }
+}
