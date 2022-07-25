24b978ce132e55a22db4bbc552fd8a023220c6f4
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-07-25 23:50:58 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Searching, Sorting and Greedy Algorithms/05.Quicksort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace SearchingSortingAndGreedyAlgorithms
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            QuickSort(numbers, 0, numbers.Length-1);
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+
+        private static void QuickSort(int[] numbers, int start, int end)
+        {
+            if(start > end) { return; }
+            int pivot = start;
+            int left = start + 1;
+            int right = end;
+            while (left <= right)
+            {
+                if (numbers[left] > numbers[pivot] && numbers[pivot] > numbers[right])
+                {
+                    Swap(numbers, left, right);
+                }
+                if(numbers[left] <= numbers[pivot]) { left++; }
+                if(numbers[right] >= numbers[pivot]) { right--; }
+            }
+            Swap(numbers, pivot, right);
+            QuickSort(numbers, start, right - 1);
+            QuickSort(numbers, right + 1, end);
+        }
+
+        private static void Swap(int[] numbers, int first, int second)
+        {
+            int temp = numbers[first];
+            numbers[first] = numbers[second];
+            numbers[second] = temp;
+        }
+    }
+}
