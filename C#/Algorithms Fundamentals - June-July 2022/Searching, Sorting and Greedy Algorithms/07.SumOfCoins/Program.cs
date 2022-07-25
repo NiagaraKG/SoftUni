cce9a7d7fdd2d71f3051f8f6a372122eb5879516
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-07-25 23:55:25 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Searching, Sorting and Greedy Algorithms/07.SumOfCoins/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SearchingSortingAndGreedyAlgorithms
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var coins = new Queue<int>(Console.ReadLine().Split(", ").Select(int.Parse).OrderByDescending(x => x));
+            int target = int.Parse(Console.ReadLine());
+            Dictionary<int, int> used = new Dictionary<int, int>();
+            int total = 0;
+            while (target > 0 && coins.Count > 0)
+            {
+                var current = coins.Dequeue();
+                var count = target / current;
+                if (count != 0)
+                {
+                    target %= current;
+                    used.Add(current, count);
+                    total += count;
+                }
+            }
+            if(target > 0) { Console.WriteLine("Error"); }
+            else
+            {
+                Console.WriteLine($"Number of coins to take: {total}");
+                foreach (var kvp in used)
+                {
+                    Console.WriteLine($"{kvp.Value} coin(s) with value {kvp.Key}");
+                }
+            }
+        }        
+    }
+}
