0ee2bf2b4e3dde5e7c3f053e147a8d0dac89c3f7
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-08-02 23:34:51 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Graph - Exercise/04.Salaries/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphExercise
+{
+    public class Program
+    {
+        private static List<int>[] graph;
+        private static Dictionary<int, int> visited;
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            graph = new List<int>[n];
+            visited = new Dictionary<int, int>();
+            for (int node = 0; node < n; node++)
+            {
+                graph[node] = new List<int>();
+                var children = Console.ReadLine();
+                for (int child = 0; child < children.Length; child++)
+                {
+                    if (children[child] == 'Y')
+                    {
+                        graph[node].Add(child);
+                    }
+                }
+            }
+            var total = 0;
+            for (int node = 0; node < graph.Length; node++)
+            {
+                total += DFS(node);
+            }
+            Console.WriteLine(total);
+        }
+
+        private static int DFS(int node)
+        {
+            if (visited.ContainsKey(node)) { return visited[node]; }
+            var salary = 0;
+            if (graph[node].Count == 0) { salary = 1; }
+            else
+            {
+                foreach (var child in graph[node])
+                {
+                    salary += DFS(child);
+                }
+            }
+            visited[node] = salary;
+            return salary;
+        }
+    }
+}
