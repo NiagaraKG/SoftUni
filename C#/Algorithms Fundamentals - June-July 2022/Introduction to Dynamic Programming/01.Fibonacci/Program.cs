edf5ee12e8244d024a18ed3be349c3580e3ace92
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-08-04 23:51:30 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Introduction to Dynamic Programming/01.Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DynamicProgramming
+{
+    public class Program
+    {
+        private static Dictionary<int, long> calculated = new Dictionary<int, long>();
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fibonacci(n));
+        }
+
+        private static long Fibonacci(int n)
+        {
+            if(calculated.ContainsKey(n)) { return calculated[n]; }
+            if (n < 2) { return n; }
+            var current = Fibonacci(n - 1) + Fibonacci(n - 2);
+            calculated[n] = current;
+            return current;
+        }
+    }
+}
