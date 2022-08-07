68183b7816a6fe3d265d6a8f81f3da4036c44c42
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-08-07 22:44:05 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/DynamicProgramming/Exercise/03.SumWithUnlimitedCoins/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DynamicProgrammingExercise
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var target = int.Parse(Console.ReadLine());
+            Console.WriteLine(CountSums(numbers, target));
+        }
+
+        private static int CountSums(int[] numbers, int target)
+        {
+            var sums = new int[target + 1];
+            sums[0] = 1;
+            foreach (var number in numbers)
+            {
+                for (int sum = number; sum <= target; sum++)
+                {
+                    sums[sum] += sums[sum - number];
+                }
+            }
+            return sums[target];
+        }
+    }
+}
