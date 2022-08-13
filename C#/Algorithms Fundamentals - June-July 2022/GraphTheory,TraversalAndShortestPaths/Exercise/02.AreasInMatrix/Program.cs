89d71bc08e84fc0daabd4f94a991cb722577de79
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-08-13 23:17:18 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/GraphTheory,TraversalAndShortestPaths/Exercise/02.AreasInMatrix/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphExercise
+{
+    public class Program
+    {
+        private static char[,] graph;
+        private static bool[,] visited;
+        private static SortedDictionary<char, int> areas;
+        public static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            graph = new char[rows, cols];
+            visited = new bool[rows, cols];
+            areas = new SortedDictionary<char, int>();
+            for (int r = 0; r < rows; r++)
+            {
+                var line = Console.ReadLine();
+                for (int c = 0; c < cols; c++)
+                {
+                    graph[r, c] = line[c];
+                }
+            }
+            int count = 0;
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    if (!visited[r, c])
+                    {
+                        var node = graph[r, c];
+                        DFS(r, c, node);
+                        count++;
+                        if (areas.ContainsKey(node)) { areas[node]++; }
+                        else { areas[node] = 1; }
+                    }
+                }
+            }
+            Console.WriteLine($"Areas: {count}");
+            foreach (var a in areas)
+            {
+                Console.WriteLine($"Letter '{a.Key}' -> {a.Value}");
+            }
+        }
+
+        private static void DFS(int row, int col, char node)
+        {
+            if (row < 0 || row >= graph.GetLength(0) || col < 0 || col >= graph.GetLength(1)) { return; }
+            if (visited[row, col]) { return; }
+            if (graph[row, col] != node) { return; }
+            visited[row, col] = true;
+            DFS(row, col - 1, node);
+            DFS(row, col + 1, node);
+            DFS(row - 1, col, node);
+            DFS(row + 1, col, node);
+        }
+    }
+}
---
+++ C#/Algorithms Fundamentals - June-July 2022/GraphTheory,TraversalAndShortestPaths/Exercise/03.CyclesInGraph/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphExercise
+{
+    public class Program
+    {
+        private static Dictionary<string, List<string>> graph;
+        private static HashSet<string> visited;
+        private static HashSet<string> cycles;
+        public static void Main()
+        {
+            graph = new Dictionary<string, List<string>>();
+            visited = new HashSet<string>();
+            cycles = new HashSet<string>();
+            string line = Console.ReadLine();
+            while (line != "End")
+            {
+                var parts = line.Split('-');
+                if(!graph.ContainsKey(parts[0])) 
+                {
+                    graph.Add(parts[0], new List<string>());
+                }
+                graph[parts[0]].Add(parts[1]);
+                if (!graph.ContainsKey(parts[1]))
+                {
+                    graph.Add(parts[1], new List<string>());
+                }
+                line = Console.ReadLine();
+            }
+            try
+            {
+                foreach (var node in graph.Keys)
+                {
+                    DFS(node);
+                }
+                Console.WriteLine("Acyclic: Yes");
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Acyclic: No");
+            }            
+        }
+
+        private static void DFS(string node)
+        {
+            if(cycles.Contains(node)) { throw new InvalidOperationException(); }
+            if(visited.Contains(node)) { return; }
+            visited.Add(node);
+            cycles.Add(node);
+            foreach (var child in graph[node])
+            {
+                DFS(child);
+            }
+            cycles.Remove(node);
+        }
+    }
+}
