d907fd7b3be9daa89fc8e59aad0654baed64c4bb
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-08-14 22:45:52 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Exam Preparation/Exam Preparation/01.TwoMinutesToMidnight/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExamPreparation
+{
+    public class Program
+    {
+        public static Dictionary<string, ulong> pascal = new Dictionary<string, ulong>();
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetPascal(n, k));
+        }
+
+        private static ulong GetPascal(int n, int k)
+        {
+            if (n <= 1 || k == n || k == 0) { return 1; }
+            string id = $"{n}-{k}";
+            if(pascal.ContainsKey(id)) { return pascal[id]; }
+            ulong result = GetPascal(n - 1, k - 1) + GetPascal(n - 1, k);
+            pascal[id] = result;
+            return result;
+        }
+    }
+}
