9ded5fb49700eac3e3bcad272c8dc200f1383900
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-08-15 23:49:17 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Exam Preparation/Exam Preparation/03.TheStoryTelling/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExamPreparation
+{
+    public class Program
+    {
+        private static Dictionary<string, List<string>> graph = new Dictionary<string, List<string>>();
+        private static HashSet<string> visited = new HashSet<string>();
+        public static void Main()
+        {
+            ReadGraph();
+            foreach (var node in graph.Keys)
+            {
+                DFS(node);
+            }
+            Console.WriteLine(String.Join(" ", visited.Reverse()));
+        }
+
+        private static void ReadGraph()
+        {
+            string line;
+            while ((line = Console.ReadLine()) != "End")
+            {
+                string[] elements = line.Split("->", StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
+                var preStory = elements[0];
+                if (!graph.ContainsKey(preStory))
+                {
+                    graph[preStory] = new List<string>();
+                }
+                if (elements.Length < 2) { continue; }
+                string[] postStories = elements[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
+                graph[preStory].AddRange(postStories);
+            }
+        }
+
+        private static void DFS(string parent)
+        {
+            if (visited.Contains(parent)) { return; }
+            foreach (var child in graph[parent]) { DFS(child); }
+            visited.Add(parent);
+        }
+    }
+}
