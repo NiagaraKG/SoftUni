5d662c910e450aa7c77b0b5195147badfcabc6b4
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-08-19 23:55:22 +0300

    Add files via upload

---
+++ C#/Algorithms Fundamentals - June-July 2022/Exam Preparation/Prep - Exam 09.01.21/01.SuperSet/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09_01_21
+{
+    public class Program
+    {
+        private static string[] elements;
+        private static string[] combinations;
+        public static void Main()
+        {
+            elements = Console.ReadLine().Split(", ");
+            for (int i = 0; i <= elements.Length; i++)
+            {
+                combinations = new string[i];
+                Combine(0,0);
+            }            
+        }
+
+        private static void Combine(int index, int start)
+        {
+            if (index >= combinations.Length)
+            {
+                Console.WriteLine(String.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = start; i < elements.Length; i++)
+            {
+                combinations[index] = elements[i];
+                Combine(index + 1, i + 1);
+            }
+        }
+    }
+}
